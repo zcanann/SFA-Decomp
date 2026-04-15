95f8e670944ba2e102f0b86b87c96c8b546c0ec2
Zachary Canann <zacharycanann@gmail.com>
2026-04-14 20:43:47 -0700

    Match TRK notify donor

--- src/dolphin/TRK_MINNOW_DOLPHIN/notify.c
+++ src/dolphin/TRK_MINNOW_DOLPHIN/notify.c
@@ -1,31 +1,47 @@
 /* TODO: restore stripped imported address metadata if needed. */
 
 #include "TRK_MINNOW_DOLPHIN/MetroTRK/Portable/notify.h"
-
 #include "PowerPC_EABI_Support/MetroTRK/trk.h"
 
+inline DSError TRKDoNotifyStopped_Inline(TRKBuffer* msg, MessageCommandID cmd) {
+    DSError err;
+
+    if (msg->position >= 0x880) {
+        err = DS_MessageBufferOverflow;
+    } else {
+        msg->data[msg->position++] = cmd;
+        msg->length += 1;
+        err = DS_NoError;
+    }
+
+    return err;
+}
+
 DSError TRKDoNotifyStopped(MessageCommandID cmd) {
+    DSError err;
     int reqIdx;
     int bufIdx;
     TRKBuffer* msg;
-    DSError err;
-    DSError bufError;
 
-    bufError = TRKGetFreeBuffer(&bufIdx, &msg);
-    if ((err = bufError) == FALSE) {
+    err = TRKGetFreeBuffer(&bufIdx, &msg);
+    if (err == DS_NoError) {
+        err = TRKDoNotifyStopped_Inline(msg, cmd);
+
         if (err == DS_NoError) {
-            if (cmd == DSMSG_NotifyStopped) {
+            if ((u8)cmd == DSMSG_NotifyStopped) {
                 TRKTargetAddStopInfo(msg);
             } else {
                 TRKTargetAddExceptionInfo(msg);
             }
         }
-        bufError = TRKRequestSend(msg, &reqIdx, 2, 3, 1);
-        err = bufError;
+
+        err = TRKRequestSend(msg, &reqIdx, 2, 3, 1);
         if (err == DS_NoError) {
             TRKReleaseBuffer(reqIdx);
         }
+
         TRKReleaseBuffer(bufIdx);
     }
+
     return err;
 }
