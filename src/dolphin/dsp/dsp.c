84f66554bbc75cf18b1babedcc308fd2ef29121e
Zachary Canann <zacharycanann@gmail.com>
2026-04-15 21:20:14 -0700

    Tighten retail DSP init subset

--- src/dolphin/dsp/dsp.c
+++ src/dolphin/dsp/dsp.c
@@ -4,27 +4,16 @@
 
 #include "dolphin/dsp/__dsp.h"
 
-#if DEBUG
-#define BUILD_DATE "Apr  5 2004"
-#define BUILD_TIME "03:56:49"
-const char* __DSPVersion = "<< Dolphin SDK - DSP\tdebug build: Apr  5 2004 03:56:49 (0x2301) >>";
-#define DSP_INIT_BUILD_DATE_MSG "DSPInit(): Build Date: %s %s\n"
-#else
 typedef struct DSPStrings {
-    char version[0x45];
-    char _pad0[3];
-    char initMsg[0x1E];
-    char _pad1[2];
+    char initMsg[0x20];
     char buildDate[0xC];
     char buildTime[0x9];
 } DSPStrings;
 
 extern const DSPStrings sDSPStrings;
-const char* __DSPVersion = sDSPStrings.version;
+#define DSP_INIT_BUILD_DATE_MSG sDSPStrings.initMsg
 #define BUILD_DATE sDSPStrings.buildDate
 #define BUILD_TIME sDSPStrings.buildTime
-#define DSP_INIT_BUILD_DATE_MSG sDSPStrings.initMsg
-#endif
 
 extern DSPTaskInfo* __DSP_rude_task;
 extern int __DSP_rude_task_pending;
@@ -48,6 +37,32 @@ void DSPSendMailToDSP(u32 mail) {
     __DSPRegs[1] = mail & 0xFFFF;
 }
 
+void DSPInit(void) {
+    BOOL old;
+    u16 tmp;
+
+    __DSP_debug_printf(DSP_INIT_BUILD_DATE_MSG, BUILD_DATE, BUILD_TIME);
+
+    if (__DSP_init_flag == 1)
+        return;
+
+    old = OSDisableInterrupts();
+    __OSSetInterruptHandler(7, __DSPHandler);
+    __OSUnmaskInterrupts(OS_INTERRUPTMASK_DSP_DSP);
+
+    tmp = __DSPRegs[5];
+    tmp = (tmp & ~0xA8) | 0x800;
+    __DSPRegs[5] = tmp;
+
+    tmp = __DSPRegs[5];
+    __DSPRegs[5] = tmp = tmp & ~0xAC;
+
+    __DSP_first_task = __DSP_last_task = __DSP_curr_task = __DSP_tmp_task = NULL;
+    __DSP_init_flag = 1;
+
+    OSRestoreInterrupts(old);
+}
+
 void DSPAssertInt(void) {
     BOOL old;
     u16 tmp;
@@ -67,34 +82,6 @@ void DSPHalt(void) {
     tmp = __DSPRegs[5];
     tmp = (tmp & ~0xA8) | 4;
     __DSPRegs[5] = tmp;
-    OSRestoreInterrupts(old);
-}
-
-void DSPInit(void) {
-    BOOL old;
-    u16 tmp;
-
-    __DSP_debug_printf(DSP_INIT_BUILD_DATE_MSG, BUILD_DATE, BUILD_TIME);
-
-    if (__DSP_init_flag == 1)
-        return;
-
-    OSRegisterVersion(__DSPVersion);
-
-    old = OSDisableInterrupts();
-    __OSSetInterruptHandler(7, __DSPHandler);
-    __OSUnmaskInterrupts(OS_INTERRUPTMASK_DSP_DSP);
-
-    tmp = __DSPRegs[5];
-    tmp = (tmp & ~0xA8) | 0x800;
-    __DSPRegs[5] = tmp;
-
-    tmp = __DSPRegs[5];
-    __DSPRegs[5] = tmp = tmp & ~0xAC;
-
-    __DSP_first_task = __DSP_last_task = __DSP_curr_task = __DSP_tmp_task = NULL;
-    __DSP_init_flag = 1;
-
     OSRestoreInterrupts(old);
 }
 
