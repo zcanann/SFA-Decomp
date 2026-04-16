be48a31190cc47e5339f52bc7f2ec816a663cd65
Zachary Canann <zacharycanann@gmail.com>
2026-04-15 23:07:25 -0700

    Split OSMutex unlock-all subset

--- configure.py
+++ configure.py
@@ -337,6 +337,7 @@ config.libs = [
             Object(NonMatching, "dolphin/os/OSLink.c"),
             Object(NonMatching, "dolphin/os/OSMessage.c"),
             Object(NonMatching, "dolphin/os/OSMemory.c"),
+            Object(NonMatching, "dolphin/os/OSMutex.c"),
             Object(NonMatching, "dolphin/os/OSReboot.c"),
             Object(NonMatching, "dolphin/os/OSReset.c"),
             Object(NonMatching, "dolphin/os/OSResetSW.c"),
