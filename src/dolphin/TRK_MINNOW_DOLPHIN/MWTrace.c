a177512e04d03af1113cbf0d5cf06f81912a1319
Zachary Canann <zacharycanann@gmail.com>
2026-04-19 00:47:15 -0700

    Recover MetroTRK MWTRACE body

--- src/dolphin/TRK_MINNOW_DOLPHIN/MWTrace.c
+++ src/dolphin/TRK_MINNOW_DOLPHIN/MWTrace.c
@@ -1,12 +1,24 @@
-#include "TRK_MINNOW_DOLPHIN/MetroTRK/Portable/MWTrace.h"
+#include "dolphin/types.h"
 
-/*
- * --INFO--
- * JP Address: TODO
- * PAL Address: TODO
- * EN Address: TODO
- */
-void MWTRACE(u8, char*, ...)
+extern u32 lbl_803DD648;
+extern char* lbl_803DD64C;
+extern u32 lbl_8033B1A0[];
+extern char lbl_802C7B80[];
+
+void MWTRACE(level, format)
+int level;
+char* format;
 {
-	
+    u32* entry;
+    char* current;
+
+    entry = (u32*)((u8*)lbl_8033B1A0 + (lbl_803DD648++ * 0x14));
+    if (level == 0xFF) {
+        current = 0;
+    } else {
+        current = &lbl_802C7B80[level << 5];
+    }
+    lbl_803DD64C = current;
+    entry[0] = 8;
+    entry[1] = level;
 }
