52b23b2df69c8f501d438a11205cda5f589c4240
Zachary Canann <zacharycanann@gmail.com>
2026-04-18 20:28:18 -0700

    Match OSArena to EN split shape

--- src/dolphin/os/OSArena.c
+++ src/dolphin/os/OSArena.c
@@ -1,52 +1,20 @@
-#include <dolphin.h>
-#include <dolphin/os.h>
+#include "dolphin/os/OSArena.h"
 
-#define ROUND(n, a) (((u32)(n) + (a)-1) & ~((a)-1))
-#define TRUNC(n, a) (((u32)(n)) & ~((a)-1))
-
-static void* __OSArenaHi;
 static void* __OSArenaLo = (void*)-1;
+static void* __OSArenaHi = NULL;
 
 void* OSGetArenaHi(void) {
-    ASSERTMSGLINE(55, (u32)__OSArenaLo != -1, "OSGetArenaHi(): OSInit() must be called in advance.");
-    ASSERTMSGLINE(57, (u32)__OSArenaLo <= (u32)__OSArenaHi, "OSGetArenaHi(): invalid arena (hi < lo).");
     return __OSArenaHi;
 }
 
 void* OSGetArenaLo(void) {
-    ASSERTMSGLINE(73, (u32)__OSArenaLo != -1, "OSGetArenaLo(): OSInit() must be called in advance.");
-    ASSERTMSGLINE(75, (u32)__OSArenaLo <= (u32)__OSArenaHi, "OSGetArenaLo(): invalid arena (hi < lo).");
     return __OSArenaLo;
 }
 
-void OSSetArenaHi(void* newHi) {
-    __OSArenaHi = newHi;
+void OSSetArenaHi(void* hi) {
+    __OSArenaHi = hi;
 }
 
-void OSSetArenaLo(void* newLo) {
-    __OSArenaLo = newLo;
-}
-
-void* OSAllocFromArenaLo(u32 size, u32 align) {
-  void* ptr;
-  u8* arenaLo;
-
-  ptr = OSGetArenaLo();
-  arenaLo = ptr = (void*)ROUND(ptr, align);
-  arenaLo += size;
-  arenaLo = (u8*)ROUND(arenaLo, align);
-  OSSetArenaLo(arenaLo);
-  return ptr;
-}
-
-void* OSAllocFromArenaHi(u32 size, u32 align) {
-  void* ptr;
-  u8* arenaHi;
-
-  arenaHi = OSGetArenaHi();
-  arenaHi = (u8*)TRUNC(arenaHi, align);
-  arenaHi -= size;
-  arenaHi = ptr = (void*)TRUNC(arenaHi, align);
-  OSSetArenaHi(arenaHi);
-  return ptr;
+void OSSetArenaLo(void* lo) {
+    __OSArenaLo = lo;
 }
