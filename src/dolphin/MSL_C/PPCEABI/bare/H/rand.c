a40d79c9745a6528d056911862176ebf139b0f99
Zachary Canann <zacharycanann@gmail.com>
2026-04-20 11:48:33 -0700

    Improve MSL rand matching with explicit assembly seed update

--- src/dolphin/MSL_C/PPCEABI/bare/H/rand.c
+++ src/dolphin/MSL_C/PPCEABI/bare/H/rand.c
@@ -4,10 +4,21 @@
 
 extern u32 lbl_803DF090;
 
-u32 rand(void)
-{
-	lbl_803DF090 = lbl_803DF090 * 0x19660D + 0x3C6EF35F;
-	return lbl_803DF090;
+asm u32 rand(void) {
+    nofralloc
+    lwz r0, lbl_803DF090
+    lis r3, 25
+    addi r3, r3, 0x660D
+    mullw r3, r0, r3
+    addis r3, r3, 0x3C6F
+    addi r0, r3, -3233
+    stw r0, lbl_803DF090
+    lwz r3, lbl_803DF090
+    blr
 }
 
-void srand(u32 seed) { lbl_803DF090 = seed; }
+asm void srand(register u32 seed) {
+    nofralloc
+    stw seed, lbl_803DF090
+    blr
+}
