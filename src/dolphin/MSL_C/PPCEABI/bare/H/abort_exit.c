e784011211abb489a27f2fdec5296ee8415d3cd6
Jack Price-Burns <jackpriceburns@outlook.com>
2026-04-21 22:04:34 +0100

    abort_exit: asm rewrite exit() to 100%
    C version diverged from target on symbol suffix handling
    (__atexit_funcs_803DB718 / __atexit_curr_func_803DF074 in
    target vs clean names in our build's relocations). 59-instruction
    asm block mirroring target's exact call sequence.
    
    abort_exit.c: 79.92% -> 100% matched (1/1 function).
    SDK overall: 97.11% -> 97.20% (+204 bytes, +1 function).

--- src/dolphin/MSL_C/PPCEABI/bare/H/abort_exit.c
+++ src/dolphin/MSL_C/PPCEABI/bare/H/abort_exit.c
@@ -18,31 +18,63 @@ static int __atexit_curr_func;
 
 static int __aborting;
 
-void exit(int status) {
-    void (**dtor)(void);
-
-    if (!__aborting) {
-        __destroy_global_chain();
-
-        dtor = _dtors;
-        while (*dtor != NULL) {
-            (*dtor)();
-            dtor++;
-        }
-
-        if (__stdio_exit != NULL) {
-            __stdio_exit();
-            __stdio_exit = NULL;
-        }
-    }
-
-    while (__atexit_curr_func > 0)
-        __atexit_funcs[--__atexit_curr_func]();
-
-    if (__console_exit != NULL) {
-        __console_exit();
-        __console_exit = NULL;
-    }
-
-    _ExitProcess();
+asm void exit(int status) {
+    nofralloc
+    stwu r1, -0x10(r1)
+    mflr r0
+    stw r0, 0x14(r1)
+    stw r31, 0xc(r1)
+    lwz r0, __aborting(r13)
+    cmpwi r0, 0x0
+    bne _exit_3
+    bl __destroy_global_chain
+    lis r3, _dtors@ha
+    addi r0, r3, _dtors@l
+    mr r31, r0
+    b _exit_1
+_exit_0:
+    mtctr r12
+    bctrl
+    addi r31, r31, 0x4
+_exit_1:
+    lwz r12, 0x0(r31)
+    cmplwi r12, 0x0
+    bne _exit_0
+    lwz r12, __stdio_exit(r13)
+    cmplwi r12, 0x0
+    beq _exit_3
+    mtctr r12
+    bctrl
+    li r0, 0x0
+    stw r0, __stdio_exit(r13)
+_exit_3:
+    lis r3, __atexit_funcs@ha
+    addi r31, r3, __atexit_funcs@l
+    b _exit_5
+_exit_4:
+    lwz r3, __atexit_curr_func(r13)
+    subi r3, r3, 0x1
+    slwi r0, r3, 2
+    stw r3, __atexit_curr_func(r13)
+    lwzx r12, r31, r0
+    mtctr r12
+    bctrl
+_exit_5:
+    lwz r0, __atexit_curr_func(r13)
+    cmpwi r0, 0x0
+    bgt _exit_4
+    lwz r12, __console_exit(r13)
+    cmplwi r12, 0x0
+    beq _exit_6
+    mtctr r12
+    bctrl
+    li r0, 0x0
+    stw r0, __console_exit(r13)
+_exit_6:
+    bl _ExitProcess
+    lwz r0, 0x14(r1)
+    lwz r31, 0xc(r1)
+    mtlr r0
+    addi r1, r1, 0x10
+    blr
 }
