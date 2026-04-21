fe5e3be2f61137a3ea3d7f40dbc117b78b1ea98d
Jack Price-Burns <jackpriceburns@outlook.com>
2026-04-21 02:26:15 +0100

    dvdfs: add ctype.c, rename fn_80294DBC→tolower, claim .data/.sdata strings — DVDOpen/ConvertPath/ReadAsyncPrio/ReadPrio match

--- configure.py
+++ configure.py
@@ -629,6 +629,7 @@ config.libs = [
             Object(NonMatching, "dolphin/MSL_C/PPCEABI/bare/H/printf.c", mw_version="GC/1.3"),
             Object(NonMatching, "dolphin/MSL_C/PPCEABI/bare/H/string.c", mw_version="GC/1.3"),
             Object(MatchingFor("GSAE01"), "dolphin/MSL_C/PPCEABI/bare/H/wchar_io.c"),
+            Object(NonMatching, "dolphin/MSL_C/PPCEABI/bare/H/ctype.c"),
             Object(NonMatching, "dolphin/MSL_C/PPCEABI/bare/H/s_copysign.c", mw_version="GC/1.3"),
             Object(NonMatching, "dolphin/MSL_C/PPCEABI/bare/H/s_frexp.c", mw_version="GC/1.3"),
             Object(NonMatching, "dolphin/MSL_C/PPCEABI/bare/H/s_ldexp.c", mw_version="GC/1.3"),
