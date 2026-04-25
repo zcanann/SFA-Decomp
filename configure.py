9b9f35af1d41ec50466a4808721b7aded8b15afc
Jack Price-Burns <jackpriceburns@outlook.com>
2026-04-25 17:59:59 +0100

    mtx44: rename labels lbl_803E82XX → lbl_803E76XX (subtract 0xC98) (2/2 fns 100%)

--- configure.py
+++ configure.py
@@ -376,7 +376,7 @@ config.libs = [
             Object(MatchingFor("GSAE01"), "dolphin/mtx/mtx.c", source="dolphin/mtx/mtx.c", extra_cflags=["-DGEKKO"]),
             Object(MatchingFor("GSAE01"), "dolphin/mtx/mtxvec.c", source="dolphin/mtx/mtxvec.c"),
             Object(NonMatching, "dolphin/mtx/vec.c"),
-            Object(NonMatching, "dolphin/mtx/mtx44.c"),
+            Object(MatchingFor("GSAE01"), "dolphin/mtx/mtx44.c"),
             Object(NonMatching, "dolphin/mtx/mtx44vec.c"),
             Object(MatchingFor("GSAE01"), "dolphin/mtx/psmtx.c"),
         ],
--- src/dolphin/mtx/mtx44.c
+++ src/dolphin/mtx/mtx44.c
@@ -3,64 +3,64 @@
 
 extern f32 tanf(f32);
 
-extern const f32 lbl_803E82C8;
-extern const f32 lbl_803E82CC;
-extern const f32 lbl_803E82D0;
-extern const f32 lbl_803E82D4;
-extern const f32 lbl_803E82D8;
-extern const f32 lbl_803E82DC;
+extern const f32 lbl_803E7630;
+extern const f32 lbl_803E7634;
+extern const f32 lbl_803E7638;
+extern const f32 lbl_803E763C;
+extern const f32 lbl_803E7640;
+extern const f32 lbl_803E7644;
 
 void C_MTXPerspective(Mtx44 m, f32 fovY, f32 aspect, f32 n, f32 f) {
     f32 angle;
     f32 cot;
     f32 tmp;
 
-    angle = lbl_803E82D8 * fovY;
-    angle = lbl_803E82DC * angle;
-    cot = lbl_803E82C8 / tanf(angle);
+    angle = lbl_803E7640 * fovY;
+    angle = lbl_803E7644 * angle;
+    cot = lbl_803E7630 / tanf(angle);
 
     m[0][0] = cot / aspect;
-    m[0][1] = lbl_803E82D0;
-    m[0][2] = lbl_803E82D0;
-    m[0][3] = lbl_803E82D0;
-    m[1][0] = lbl_803E82D0;
+    m[0][1] = lbl_803E7638;
+    m[0][2] = lbl_803E7638;
+    m[0][3] = lbl_803E7638;
+    m[1][0] = lbl_803E7638;
     m[1][1] = cot;
-    m[1][2] = lbl_803E82D0;
-    m[1][3] = lbl_803E82D0;
-    m[2][0] = lbl_803E82D0;
-    m[2][1] = lbl_803E82D0;
+    m[1][2] = lbl_803E7638;
+    m[1][3] = lbl_803E7638;
+    m[2][0] = lbl_803E7638;
+    m[2][1] = lbl_803E7638;
 
-    tmp = lbl_803E82C8 / (f - n);
+    tmp = lbl_803E7630 / (f - n);
     m[2][2] = -n * tmp;
     m[2][3] = tmp * -(f * n);
-    m[3][0] = lbl_803E82D0;
-    m[3][1] = lbl_803E82D0;
-    m[3][2] = lbl_803E82D4;
-    m[3][3] = lbl_803E82D0;
+    m[3][0] = lbl_803E7638;
+    m[3][1] = lbl_803E7638;
+    m[3][2] = lbl_803E763C;
+    m[3][3] = lbl_803E7638;
 }
 
 void C_MTXOrtho(Mtx44 m, f32 t, f32 b, f32 l, f32 r, f32 n, f32 f) {
     f32 tmp;
 
-    tmp = lbl_803E82C8 / (r - l);
-    m[0][0] = lbl_803E82CC * tmp;
-    m[0][1] = lbl_803E82D0;
-    m[0][2] = lbl_803E82D0;
+    tmp = lbl_803E7630 / (r - l);
+    m[0][0] = lbl_803E7634 * tmp;
+    m[0][1] = lbl_803E7638;
+    m[0][2] = lbl_803E7638;
     m[0][3] = tmp * -(r + l);
 
-    tmp = lbl_803E82C8 / (t - b);
-    m[1][0] = lbl_803E82D0;
-    m[1][1] = lbl_803E82CC * tmp;
-    m[1][2] = lbl_803E82D0;
+    tmp = lbl_803E7630 / (t - b);
+    m[1][0] = lbl_803E7638;
+    m[1][1] = lbl_803E7634 * tmp;
+    m[1][2] = lbl_803E7638;
     m[1][3] = tmp * -(t + b);
 
-    tmp = lbl_803E82C8 / (f - n);
-    m[2][0] = lbl_803E82D0;
-    m[2][1] = lbl_803E82D0;
-    m[2][2] = lbl_803E82D4 * tmp;
+    tmp = lbl_803E7630 / (f - n);
+    m[2][0] = lbl_803E7638;
+    m[2][1] = lbl_803E7638;
+    m[2][2] = lbl_803E763C * tmp;
     m[2][3] = (-f) * tmp;
-    m[3][0] = lbl_803E82D0;
-    m[3][1] = lbl_803E82D0;
-    m[3][2] = lbl_803E82D0;
-    m[3][3] = lbl_803E82C8;
+    m[3][0] = lbl_803E7638;
+    m[3][1] = lbl_803E7638;
+    m[3][2] = lbl_803E7638;
+    m[3][3] = lbl_803E7630;
 }
