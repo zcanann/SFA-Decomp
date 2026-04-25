727dfdfadf4931cce7ed34f2a837f8a1b542f72d
Jack Price-Burns <jackpriceburns@outlook.com>
2026-04-25 18:11:13 +0100

    mbstring: reorder fns + rename lbl_803E85D0→7938 + remove static (2/2 fns 100%)

--- src/dolphin/MSL_C/PPCEABI/bare/H/mbstring.c
+++ src/dolphin/MSL_C/PPCEABI/bare/H/mbstring.c
@@ -1,65 +1,9 @@
 #include "PowerPC_EABI_Support/Msl/MSL_C/MSL_Common/ansi_files.h"
 #include "string.h"
 
-extern const int lbl_803E85D0;
+extern const int lbl_803E7938;
 
-static asm int unicode_to_UTF8(char* s, wchar_t wchar) {
-    nofralloc
-    stwu r1, -0x10(r1)
-    cmplwi r3, 0x0
-    lwz r0, lbl_803E85D0(r2)
-    stw r0, 0x8(r1)
-    bne _u8_0
-    li r3, 0x0
-    b _u8_end
-_u8_0:
-    clrlwi r0, r4, 16
-    cmplwi r0, 0x80
-    bge _u8_1
-    li r5, 0x1
-    b _u8_2
-_u8_1:
-    cmplwi r0, 0x800
-    bge _u8_3
-    li r5, 0x2
-    b _u8_2
-_u8_3:
-    li r5, 0x3
-_u8_2:
-    cmpwi r5, 0x2
-    add r6, r3, r5
-    beq _u8_4
-    bge _u8_5
-    cmpwi r5, 0x1
-    bge _u8_6
-    b _u8_7
-_u8_5:
-    cmpwi r5, 0x4
-    bge _u8_7
-    clrlwi r0, r4, 26
-    extrwi r4, r4, 10, 16
-    ori r0, r0, 0x80
-    stbu r0, -0x1(r6)
-_u8_4:
-    clrlwi r0, r4, 26
-    extrwi r4, r4, 10, 16
-    ori r0, r0, 0x80
-    stbu r0, -0x1(r6)
-_u8_6:
-    addi r3, r1, 0x8
-    lbzx r0, r3, r5
-    or r0, r4, r0
-    stb r0, -0x1(r6)
-_u8_7:
-    mr r3, r5
-_u8_end:
-    addi r1, r1, 0x10
-    blr
-}
-
-static inline int wctomb(char* s, wchar_t wchar) {
-    return (unicode_to_UTF8(s, wchar));
-}
+asm int unicode_to_UTF8(char* s, wchar_t wchar);
 
 asm size_t wcstombs(char* s, const wchar_t* pwcs, size_t n) {
     nofralloc
@@ -109,5 +53,59 @@ _wcs_end:
     lwz r0, 0x34(r1)
     mtlr r0
     addi r1, r1, 0x30
+    blr
+}
+
+asm int unicode_to_UTF8(char* s, wchar_t wchar) {
+    nofralloc
+    stwu r1, -0x10(r1)
+    cmplwi r3, 0x0
+    lwz r0, lbl_803E7938(r0)
+    stw r0, 0x8(r1)
+    bne _u8_0
+    li r3, 0x0
+    b _u8_end
+_u8_0:
+    clrlwi r0, r4, 16
+    cmplwi r0, 0x80
+    bge _u8_1
+    li r5, 0x1
+    b _u8_2
+_u8_1:
+    cmplwi r0, 0x800
+    bge _u8_3
+    li r5, 0x2
+    b _u8_2
+_u8_3:
+    li r5, 0x3
+_u8_2:
+    cmpwi r5, 0x2
+    add r6, r3, r5
+    beq _u8_4
+    bge _u8_5
+    cmpwi r5, 0x1
+    bge _u8_6
+    b _u8_7
+_u8_5:
+    cmpwi r5, 0x4
+    bge _u8_7
+    clrlwi r0, r4, 26
+    extrwi r4, r4, 10, 16
+    ori r0, r0, 0x80
+    stbu r0, -0x1(r6)
+_u8_4:
+    clrlwi r0, r4, 26
+    extrwi r4, r4, 10, 16
+    ori r0, r0, 0x80
+    stbu r0, -0x1(r6)
+_u8_6:
+    addi r3, r1, 0x8
+    lbzx r0, r3, r5
+    or r0, r4, r0
+    stb r0, -0x1(r6)
+_u8_7:
+    mr r3, r5
+_u8_end:
+    addi r1, r1, 0x10
     blr
 }
