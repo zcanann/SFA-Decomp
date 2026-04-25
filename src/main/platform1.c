e3bcfd0ef460c26b362655c0ce1d6a4c732c03f7
Zachary Canann <zacharycanann@gmail.com>
2026-04-25 10:22:42 -0700

    platform1: claim unsupported stub window

---
+++ src/main/platform1.c
@@ -0,0 +1,50 @@
+#include "ghidra_import.h"
+#include "dolphin/os.h"
+
+extern char sPlatform1DrawNoLongerSupported[];
+extern char sPlatform1ControlNoLongerSupported[];
+extern char sPlatform1InitNoLongerSupported[];
+
+int platform1_getExtraSize(void)
+{
+  return 0;
+}
+
+int platform1_func08(void)
+{
+  return 0;
+}
+
+void platform1_free(void)
+{
+}
+
+void platform1_renderUnsupported(void)
+{
+  OSReport(sPlatform1DrawNoLongerSupported);
+  return;
+}
+
+void platform1_hitDetect(void)
+{
+}
+
+void platform1_updateUnsupported(void)
+{
+  OSReport(sPlatform1ControlNoLongerSupported);
+  return;
+}
+
+void platform1_initUnsupported(void)
+{
+  OSReport(sPlatform1InitNoLongerSupported);
+  return;
+}
+
+void platform1_release(void)
+{
+}
+
+void platform1_initialise(void)
+{
+}
