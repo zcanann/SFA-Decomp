24786cf3a1dca4466a5c4a4a715cb2b45ed1f4c9
Zachary Canann <zacharycanann@gmail.com>
2026-04-25 20:08:12 -0700

    recover dfptargetblock callbacks

--- include/main/dll/door.h
+++ include/main/dll/door.h
@@ -4,5 +4,9 @@
 #include "ghidra_import.h"
 
 void dfptargetblock_resolveCollisionPoints(undefined2 *param_1,int param_2);
+int dfptargetblock_getExtraSize(void);
+int dfptargetblock_func08(void);
+void dfptargetblock_free(void);
+void dfptargetblock_render(int obj);
 
 #endif /* MAIN_DLL_DOOR_H_ */
--- src/main/dll/door.c
+++ src/main/dll/door.c
@@ -3,6 +3,7 @@
 
 extern undefined4 FUN_80006b4c();
 extern uint FUN_80017690();
+extern void fn_8003B8F4(int obj,float param_2);
 extern undefined4 sfxplayer_updateState();
 
 extern undefined4 DAT_803add98;
@@ -13,6 +14,7 @@ extern undefined4 DAT_803adda8;
 extern undefined4 DAT_803addac;
 extern undefined4 DAT_803addb0;
 extern undefined4 DAT_803addb4;
+extern f32 lbl_803E6490;
 
 /*
  * --INFO--
@@ -29,4 +31,81 @@ extern undefined4 DAT_803addb4;
  */
 void dfptargetblock_resolveCollisionPoints(undefined2 *param_1,int param_2)
 {
+}
+
+/*
+ * --INFO--
+ *
+ * Function: dfptargetblock_getExtraSize
+ * EN v1.0 Address: 0x80208660
+ * EN v1.0 Size: 8b
+ * EN v1.1 Address: 0x8020874C
+ * EN v1.1 Size: 8b
+ * JP Address: TODO
+ * JP Size: TODO
+ * PAL Address: TODO
+ * PAL Size: TODO
+ */
+int dfptargetblock_getExtraSize(void)
+{
+  return 0x6c;
+}
+
+/*
+ * --INFO--
+ *
+ * Function: dfptargetblock_func08
+ * EN v1.0 Address: 0x80208668
+ * EN v1.0 Size: 8b
+ * EN v1.1 Address: 0x80208754
+ * EN v1.1 Size: 8b
+ * JP Address: TODO
+ * JP Size: TODO
+ * PAL Address: TODO
+ * PAL Size: TODO
+ */
+int dfptargetblock_func08(void)
+{
+  return 0;
+}
+
+/*
+ * --INFO--
+ *
+ * Function: dfptargetblock_free
+ * EN v1.0 Address: 0x80208670
+ * EN v1.0 Size: 4b
+ * EN v1.1 Address: 0x8020875C
+ * EN v1.1 Size: 4b
+ * JP Address: TODO
+ * JP Size: TODO
+ * PAL Address: TODO
+ * PAL Size: TODO
+ */
+void dfptargetblock_free(void)
+{
+}
+
+/*
+ * --INFO--
+ *
+ * Function: dfptargetblock_render
+ * EN v1.0 Address: 0x80208674
+ * EN v1.0 Size: 80b
+ * EN v1.1 Address: 0x80208760
+ * EN v1.1 Size: 80b
+ * JP Address: TODO
+ * JP Size: TODO
+ * PAL Address: TODO
+ * PAL Size: TODO
+ */
+void dfptargetblock_render(int obj)
+{
+  int state;
+
+  state = *(int *)(obj + 0xb8);
+  if (((*(u8 *)(state + 0x6b) == 0) && (*(u8 *)(state + 0x6a) != 0)) &&
+      (*(u8 *)(state + 0x69) != 4)) {
+    fn_8003B8F4(obj,lbl_803E6490);
+  }
 }
