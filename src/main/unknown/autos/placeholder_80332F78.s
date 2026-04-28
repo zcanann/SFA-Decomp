992747594e34df8ccd67062f61032915e2351072
Zachary Canann <zacharycanann@gmail.com>
2026-04-27 21:21:56 -0700

    Link SDK data placeholder units

---
+++ src/main/unknown/autos/placeholder_80332F78.s
@@ -0,0 +1,29 @@
+.include "macros.inc"
+.file "placeholder_80332F78.s"
+
+.data
+.balign 8
+
+.obj lbl_80332F78, global
+	.4byte 0x00250026
+	.4byte 0x00270028
+	.4byte 0x0029002A
+	.4byte 0x002B002C
+.endobj lbl_80332F78
+
+.obj lbl_80332F88, global
+	.4byte 0x003A003B
+	.4byte 0x003C003D
+	.4byte 0x003E003F
+	.4byte 0x00400041
+	.4byte 0x3BA3D70A
+	.4byte 0x3E087FCB
+	.4byte 0x3DDA6612
+	.4byte 0x3EB84620
+	.4byte 0x3EB172EE
+	.4byte 0x3ED392E1
+	.4byte 0x3ECCBFB1
+	.4byte 0x3F502949
+	.4byte 0x3F4CBFB1
+	.4byte 0x3F998FC4
+.endobj lbl_80332F88
---
+++ src/main/unknown/autos/placeholder_803AEA38.s
@@ -0,0 +1,14 @@
+.include "macros.inc"
+.file "placeholder_803AEA38.s"
+
+.section .bss, "wa", @nobits
+.balign 8
+
+.obj lbl_803AEA38, global
+	.skip 0x48
+.endobj lbl_803AEA38
+
+.obj gap_08_803AEA80_bss, global
+.hidden gap_08_803AEA80_bss
+	.skip 0x240
+.endobj gap_08_803AEA80_bss
