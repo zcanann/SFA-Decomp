ad4bb437465f57140b59f6f7b3e375ef86cbb5b8
Zachary Canann <zacharycanann@gmail.com>
2026-04-27 17:27:58 -0700

    Link matched GXPixel SDK source

--- configure.py
+++ configure.py
@@ -489,7 +489,7 @@ config.libs = [
             Object(NonMatching, "dolphin/gx/GXDraw.c"),
             Object(NonMatching, "main/unknown/autos/placeholder_8032F618.c"),
             Object(MatchingFor("GSAE01"), "dolphin/gx/GXPerf.c"),
-            Object(NonMatching, "dolphin/gx/GXPixel.c"),
+            Object(MatchingFor("GSAE01"), "dolphin/gx/GXPixel.c"),
             Object(NonMatching, "dolphin/gx/GXSave.c"),
             Object(MatchingFor("GSAE01"), "dolphin/gx/GXStubs.c"),
             Object(MatchingFor("GSAE01"), "dolphin/gx/GXTev.c"),
--- src/dolphin/gx/GXPixel.c
+++ src/dolphin/gx/GXPixel.c
@@ -1,5 +1,3 @@
-#include <math.h>
-
 #include <dolphin/gx.h>
 #include <dolphin/os.h>
 
