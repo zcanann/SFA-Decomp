8566ba83b2d8e98c533bec02199edd5df7b6234a
Zachary Canann <zacharycanann@gmail.com>
2026-04-28 20:07:02 -0700

    Link abort_exit SDK unit

--- src/dolphin/MSL_C/PPCEABI/bare/H/abort_exit.c
+++ src/dolphin/MSL_C/PPCEABI/bare/H/abort_exit.c
@@ -1,80 +1,47 @@
 #include "PowerPC_EABI_Support/Msl/MSL_C/MSL_Common/abort_exit.h"
-#include "PowerPC_EABI_Support/Msl/MSL_C/MSL_Common/critical_regions.h"
-#include "PowerPC_EABI_Support/Msl/MSL_C/MSL_Common/signal.h"
-#include "Runtime.PPCEABI.H/NMWException.h"
 #include "stddef.h"
 
 void _ExitProcess(void);
 void __destroy_global_chain(void);
 
 extern void (*_dtors[])(void);
-extern void (*__atexit_funcs_803DAAB8[64])(void);
-extern void (*__console_exit)(void);
-extern void (*__stdio_exit)(void);
 
-extern int __atexit_curr_func_803DE3F4;
-extern int __aborting;
+int __aborting = 0;
 
-extern unsigned char lbl_803DE400;
-extern unsigned int lbl_803DABB8[13];
-extern void* memset(void*, int, unsigned int);
-extern void __sys_free(void*);
+static void (*__atexit_funcs[64])(void);
+static int __atexit_curr_func = 0;
 
-#define __atexit_curr_func __atexit_curr_func_803DE3F4
+void (*__stdio_exit)(void) = 0;
+void (*__console_exit)(void) = 0;
 
-void exit(int status) {
+void exit(int status)
+{
+    void (**dtor)(void);
+
     (void)status;
-
     if (!__aborting) {
-        int i;
-
         __destroy_global_chain();
-        for (i = 0; _dtors[i] != 0; i++) {
-            _dtors[i]();
+
+        dtor = _dtors;
+        while (*dtor != NULL) {
+            (*dtor)();
+            dtor++;
         }
 
-        if (__stdio_exit != 0) {
+        if (__stdio_exit != NULL) {
             __stdio_exit();
-            __stdio_exit = 0;
+            __stdio_exit = NULL;
         }
     }
 
     while (__atexit_curr_func > 0) {
-        __atexit_curr_func--;
-        __atexit_funcs_803DAAB8[__atexit_curr_func]();
+        __atexit_funcs[--__atexit_curr_func]();
     }
 
-    if (__console_exit != 0) {
+    if (__console_exit != NULL) {
         __console_exit();
-        __console_exit = 0;
+        __console_exit = NULL;
     }
 
     _ExitProcess();
-}
-
-void SubBlock_merge_next(void* subBlock, void** start) {
-    (void)subBlock;
-    (void)start;
-}
-
-void Block_link(void* block, void* subBlock) {
-    (void)block;
-    (void)subBlock;
-}
-
-void deallocate_from_fixed_pools(void* poolObj, void* ptr, unsigned long size) {
-    (void)poolObj;
-    (void)size;
-    __sys_free(ptr);
-}
-
-void fn_8028D574(void* p) {
-    if (!lbl_803DE400) {
-        memset(lbl_803DABB8, 0, sizeof(lbl_803DABB8));
-        lbl_803DE400 = 1;
-    }
-
-    if (p != 0) {
-        __sys_free(p);
-    }
 }
