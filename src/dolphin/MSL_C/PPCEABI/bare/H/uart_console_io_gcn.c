655a30e0c488ac3cc094846365edfe2c172338e5
Zachary Canann <zacharycanann@gmail.com>
2026-05-01 10:53:17 -0700

    Improve UART float floor helper match

--- src/dolphin/MSL_C/PPCEABI/bare/H/uart_console_io_gcn.c
+++ src/dolphin/MSL_C/PPCEABI/bare/H/uart_console_io_gcn.c
@@ -61,14 +61,19 @@ asm float fabsf__Ff(register float x) {
 
 float fn_80294724(float x) {
     int n = (int)x;
-    float truncated = (float)n;
+    float diff = (float)n - x;
+    u32 bits;
 
-    if (truncated != x && ((*(u32*)&x & 0x7F800000) < 0x4B800000)) {
-        if (*(u32*)&x & 0x80000000) {
-            n--;
+    if (*(s32*)&diff != 0) {
+        bits = *(u32*)&x;
+        if ((s32)(bits & 0x7F800000) < 0x4B800000) {
+            if ((s32)bits < 0) {
+                return (float)(n - 1);
+            }
+
+            return (float)n;
         }
-        truncated = (float)n;
     }
 
-    return truncated;
+    return x;
 }
