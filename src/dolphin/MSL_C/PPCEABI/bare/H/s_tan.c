6629fc2d0d66c89c0005c50da0de6d5e7e0af16a
Zachary Canann <zacharycanann@gmail.com>
2026-04-30 23:01:24 -0700

    Replace fake tan reducer

--- src/dolphin/MSL_C/PPCEABI/bare/H/s_tan.c
+++ src/dolphin/MSL_C/PPCEABI/bare/H/s_tan.c
@@ -1,6 +1,29 @@
-double tan(int* out_n, float x) {
-    if (out_n != 0) {
-        *out_n = 0;
-    }
-    return x;
+extern double lbl_803E7C00;
+extern double lbl_803E7C08;
+extern double lbl_803E7C10;
+extern unsigned int __cvt_fp2unsigned(double x);
+
+typedef union IntDouble {
+    double d;
+    struct {
+        unsigned int hi;
+        unsigned int lo;
+    } words;
+} IntDouble;
+
+double tan(int* out_n, float x)
+{
+    unsigned int n;
+    float ax;
+    double scaled;
+    IntDouble conv;
+
+    ax = x;
+    ax = __fabsf(ax);
+    scaled = lbl_803E7C00 * ax;
+    n = (__cvt_fp2unsigned(scaled) + 1) & ~1U;
+    *out_n = n;
+    conv.words.hi = 0x43300000;
+    conv.words.lo = n;
+    return ax - lbl_803E7C08 * (conv.d - lbl_803E7C10);
 }
