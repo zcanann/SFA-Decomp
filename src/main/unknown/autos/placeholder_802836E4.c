badd9b0b5dc54d4b724829cd2540a214cc61b8f5
Jack Price-Burns <jackpriceburns@outlook.com>
2026-05-01 09:13:13 +0100

    Recover MusyX hwStart logic

--- src/main/unknown/autos/placeholder_802836E4.c
+++ src/main/unknown/autos/placeholder_802836E4.c
@@ -1,6 +1,10 @@
 #include "ghidra_import.h"
 #include "main/unknown/autos/placeholder_802836E4.h"
 
+extern void fn_8027EF1C(void *entry);
+extern u8 *lbl_803DE344;
+extern u8 lbl_803DE370;
+
 /*
  * --INFO--
  *
@@ -14,6 +18,16 @@
  * PAL Address: TODO
  * PAL Size: TODO
  */
-void hwStart(int *param_1)
+void hwStart(int slot)
 {
+  u8 *entry;
+  u32 offset;
+
+  offset = slot * 0xf4;
+  entry = lbl_803DE344;
+  entry += offset;
+  entry[0xd4] = lbl_803DE370;
+  entry = lbl_803DE344;
+  entry += offset;
+  fn_8027EF1C(entry);
 }
