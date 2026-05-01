477851e4fdcb17fe5e05879be9ce95c76ce93c44
Zachary Canann <zacharycanann@gmail.com>
2026-04-30 21:47:34 -0700

    Replace fake vector normalize helper

--- src/dolphin/MSL_C/PPCEABI/bare/H/e_atan2.c
+++ src/dolphin/MSL_C/PPCEABI/bare/H/e_atan2.c
@@ -6,15 +6,19 @@ typedef struct Vec3f {
     float z;
 } Vec3f;
 
+extern float fn_80293954(float x);
+
+void fn_80292C74(void* v_in, void* v_out, float s);
+float fn_80292C9C(void* v);
+
 float fn_80292B44(float x, float y) {
     (void)y;
     return x;
 }
 
 void fn_80292C30(void* v_in, void* v_out) {
-    Vec3f* in = v_in;
-    Vec3f* out = v_out;
-    *out = *in;
+    float scale = fn_80293954(fn_80292C9C(v_in));
+    fn_80292C74(v_in, v_out, scale);
 }
 
 void fn_80292C74(void* v_in, void* v_out, float s) {
@@ -27,7 +31,7 @@ void fn_80292C74(void* v_in, void* v_out, float s) {
 
 float fn_80292C9C(void* v) {
     Vec3f* vec = v;
-    return vec->x * vec->x + vec->y * vec->y + vec->z * vec->z;
+    return vec->z * vec->z + (vec->x * vec->x + vec->y * vec->y);
 }
 
 float fn_80292CC4(short* p, float x) {
