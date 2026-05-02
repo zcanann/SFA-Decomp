683f8cfbc9f01c333111504a5d4e3491a9056105
Zachary Canann <zacharycanann@gmail.com>
2026-05-02 13:35:21 -0700

    Improve s_tan SDK source shape

--- src/dolphin/MSL_C/PPCEABI/bare/H/s_tan.c
+++ src/dolphin/MSL_C/PPCEABI/bare/H/s_tan.c
@@ -14,16 +14,15 @@ typedef union IntDouble {
 double tan(int* out_n, float x)
 {
     unsigned int n;
-    float ax;
+    double ax;
     double scaled;
     IntDouble conv;
 
-    ax = x;
-    ax = __fabsf(ax);
+    ax = __fabsf(x);
     scaled = lbl_803E7C00 * ax;
     n = (__cvt_fp2unsigned(scaled) + 1) & ~1U;
     *out_n = n;
-    conv.words.hi = 0x43300000;
     conv.words.lo = n;
+    conv.words.hi = 0x43300000;
     return ax - lbl_803E7C08 * (conv.d - lbl_803E7C10);
 }
