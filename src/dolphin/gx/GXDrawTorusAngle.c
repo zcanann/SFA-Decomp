89fb941138e3c332bac3b236648550e96c4abe05
Zachary Canann <zacharycanann@gmail.com>
2026-05-01 19:42:59 -0700

    Replace GXDraw torus asm constants with C

---
+++ src/dolphin/gx/GXDrawTorusAngle.c
@@ -0,0 +1,4 @@
+#include <dolphin/gx.h>
+
+const f32 lbl_803E7748 = 6.2831855f;
+const f32 lbl_803E774C = 0.0f;
---
+++ src/dolphin/gx/GXDrawTorusRadius.c
@@ -0,0 +1,4 @@
+#include <dolphin/gx.h>
+
+const f32 lbl_803E7730 = 1.0f;
+const f32 lbl_803E7734 = 0.0f;
