f03e8b728569d598041e80d02fb71154546b7cd2
Zachary Canann <zacharycanann@gmail.com>
2026-05-02 18:04:26 -0700

    Count TRK entry units as SDK

--- configure.py
+++ configure.py
@@ -635,8 +635,8 @@ config.libs = [
                 extra_cflags=["-sdata", "8", "-sdata2", "8"],
             ),
             Object(MatchingFor("GSAE01"), "dolphin/TRK_MINNOW_DOLPHIN/MWCriticalSection_gc.c"),
-            Object(NonMatching, "dolphin/TRK_MINNOW_DOLPHIN/main.c", progress_category="game"),
-            Object(NonMatching, "dolphin/TRK_MINNOW_DOLPHIN/main_gdev.c", progress_category="game"),
+            Object(NonMatching, "dolphin/TRK_MINNOW_DOLPHIN/main.c", progress_category="sdk"),
+            Object(NonMatching, "dolphin/TRK_MINNOW_DOLPHIN/main_gdev.c", progress_category="sdk"),
         ],
     },
     DolphinLib(
