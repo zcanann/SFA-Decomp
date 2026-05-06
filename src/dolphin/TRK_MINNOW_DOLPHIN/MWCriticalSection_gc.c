1eee02f948620f8192e08c695d818fcd910e0044
Zachary Canann <zacharycanann@gmail.com>
2026-05-05 18:11:34 -0700

    Match stripped MW critical section unit

--- src/dolphin/TRK_MINNOW_DOLPHIN/MWCriticalSection_gc.c
+++ src/dolphin/TRK_MINNOW_DOLPHIN/MWCriticalSection_gc.c
@@ -1,14 +1,1 @@
 #include "TRK_MINNOW_DOLPHIN/MetroTRK/Portable/MWCriticalSection_gc.h"
-#include <dolphin/os.h>
-
-void MWInitializeCriticalSection(u32* section) {
-    (void)section;
-}
-
-void MWEnterCriticalSection(u32* section) {
-    *section = OSDisableInterrupts();
-}
-
-void MWExitCriticalSection(u32* section) {
-    OSRestoreInterrupts(*section);
-}
