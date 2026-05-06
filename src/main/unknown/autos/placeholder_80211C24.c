ed971645ad455e70bb137379d11d03537ab7008c
Zachary Canann <zacharycanann@gmail.com>
2026-05-05 21:19:39 -0700

    Name DR Shackle object functions

--- src/main/unknown/autos/placeholder_80211C24.c
+++ src/main/unknown/autos/placeholder_80211C24.c
@@ -2,10 +2,10 @@
 
 /* Pattern wrappers. */
 int fn_802133E8(void) { return 0x0; }
-int fn_8021AFD4(void) { return 0x20; }
-int fn_8021AFDC(void) { return 0x0; }
-void fn_8021B350(void) {}
-void fn_8021B354(void) {}
+int drshackle_getExtraSize(void) { return 0x20; }
+int drshackle_func08(void) { return 0x0; }
+void drshackle_release(void) {}
+void drshackle_initialise(void) {}
 int fn_8021DF08(void) { return 0x0; }
 void fn_8021E81C(void) {}
 int fn_8021E820(void) { return 0x0; }
