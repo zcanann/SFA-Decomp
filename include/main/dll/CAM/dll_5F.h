91b0bc6c1b777751574f0e7bfcfdca3fe78acf84
Zachary Canann <zacharycanann@gmail.com>
2026-05-07 20:10:22 -0700

    Propagate test strength camera symbols

--- include/main/dll/CAM/dll_5F.h
+++ include/main/dll/CAM/dll_5F.h
@@ -3,9 +3,9 @@
 
 #include "ghidra_import.h"
 
-void FUN_8010b424(undefined8 param_1,undefined8 param_2,undefined8 param_3,undefined8 param_4,
+void fn_8010B424(short *param_1,undefined8 param_2,undefined8 param_3,undefined8 param_4,
                  undefined8 param_5,undefined8 param_6,undefined8 param_7,undefined8 param_8);
-void FUN_8010bd7c(undefined4 param_1,undefined4 param_2,undefined4 *param_3,undefined4 param_4,
+void fn_8010BA98(undefined4 param_1,undefined4 param_2,undefined4 *param_3,undefined4 param_4,
                  undefined4 param_5,undefined4 param_6,undefined4 param_7,undefined4 param_8);
 
 #endif /* MAIN_DLL_CAM_DLL_5F_H_ */
--- src/main/dll/CAM/dll_5F.c
+++ src/main/dll/CAM/dll_5F.c
@@ -2,34 +2,35 @@
 #include "main/dll/CAM/dll_5F.h"
 
 extern undefined4 FUN_800033a8();
-extern undefined4 FUN_800068f4();
-extern double FUN_80006a28();
-extern double FUN_80006a38();
-extern undefined4 FUN_80006c00();
+extern void Obj_TransformWorldPointToLocal(f32 x,f32 y,f32 z,float *outX,float *outY,float *outZ,int obj);
+extern double fn_80010C64();
+extern double fn_80010EE0();
+extern undefined4 getButtonsJustPressed();
 extern int FUN_80017730();
 extern undefined4 FUN_80017830();
 extern undefined4 FUN_8010910c();
 extern undefined4 pathcam_buildWindowSamples();
 extern undefined8 pathcam_findTaggedNodeWindow();
-extern double FUN_8010aea8();
+extern double fn_8010AC48();
+extern undefined4 fn_8010AEA8();
 extern undefined4 FUN_8010b218();
 extern undefined8 FUN_8028683c();
 extern undefined4 FUN_80286888();
 extern undefined4 FUN_80293900();
 
-extern undefined4 DAT_803dc070;
-extern undefined4* DAT_803dd6d0;
-extern undefined4* DAT_803dd71c;
-extern undefined4* DAT_803de1d8;
-extern f64 DOUBLE_803e2520;
-extern f32 lbl_803E2508;
-extern f32 lbl_803E250C;
-extern f32 lbl_803E253C;
+extern u8 framesThisStep;
+extern int *lbl_803DCA50;
+extern undefined4* lbl_803DCA9C;
+extern undefined4* lbl_803DD560;
+extern f64 lbl_803E18A0;
+extern f32 lbl_803E1888;
+extern f32 lbl_803E188C;
+extern f32 lbl_803E18BC;
 
 /*
  * --INFO--
  *
- * Function: FUN_8010b424
+ * Function: fn_8010B424
  * EN v1.0 Address: 0x8010B424
  * EN v1.0 Size: 2392b
  * EN v1.1 Address: 0x8010B6C0
@@ -39,7 +40,7 @@ extern f32 lbl_803E253C;
  * PAL Address: TODO
  * PAL Size: TODO
  */
-void FUN_8010b424(undefined8 param_1,undefined8 param_2,undefined8 param_3,undefined8 param_4,
+void fn_8010B424(short *param_1,undefined8 param_2,undefined8 param_3,undefined8 param_4,
                  undefined8 param_5,undefined8 param_6,undefined8 param_7,undefined8 param_8)
 {
   float fVar1;
@@ -112,17 +113,17 @@ void FUN_8010b424(undefined8 param_1,undefined8 param_2,undefined8 param_3,undef
   fStack_24 = (float)in_ps29_1;
   local_38 = (float)in_f28;
   fStack_34 = (float)in_ps28_1;
-  psVar3 = (short *)FUN_8028683c();
-  if (*(char *)((int)DAT_803de1d8 + 0x65) == '\0') {
+  psVar3 = param_1;
+  if (*(char *)((int)lbl_803DD560 + 0x65) == '\0') {
     iVar13 = *(int *)(psVar3 + 0x52);
-    FUN_80006c00(0);
-    uVar4 = (**(code **)(*DAT_803dd71c + 0x1c))(DAT_803de1d8[3]);
-    uVar5 = (**(code **)(*DAT_803dd71c + 0x1c))(DAT_803de1d8[2]);
+    getButtonsJustPressed(0);
+    uVar4 = (**(code **)(*lbl_803DCA9C + 0x1c))(lbl_803DD560[3]);
+    uVar5 = (**(code **)(*lbl_803DCA9C + 0x1c))(lbl_803DD560[2]);
     uVar15 = pathcam_findTaggedNodeWindow(extraout_f1,param_2,param_3,param_4,param_5,param_6,
-                                          param_7,param_8,uVar5,local_98,DAT_803de1d8[1],in_r6,
+                                          param_7,param_8,uVar5,local_98,lbl_803DD560[1],in_r6,
                                           in_r7,in_r8,in_r9,in_r10);
     pathcam_findTaggedNodeWindow(uVar15,param_2,param_3,param_4,param_5,param_6,param_7,param_8,
-                                 uVar4,local_88,DAT_803de1d8[1],in_r6,in_r7,in_r8,in_r9,in_r10);
+                                 uVar4,local_88,lbl_803DD560[1],in_r6,in_r7,in_r8,in_r9,in_r10);
     pfVar8 = afStack_c8;
     pfVar9 = afStack_d8;
     pfVar10 = afStack_e8;
@@ -132,94 +133,94 @@ void FUN_8010b424(undefined8 param_1,undefined8 param_2,undefined8 param_3,undef
                                pfVar12);
     dVar17 = (double)*(float *)(iVar13 + 0x1c);
     dVar18 = (double)*(float *)(iVar13 + 0x20);
-    dVar16 = FUN_8010aea8((double)*(float *)(iVar13 + 0x18),dVar17,dVar18,local_88);
-    dVar14 = (double)lbl_803E2508;
+    dVar16 = fn_8010AC48((double)*(float *)(iVar13 + 0x18),dVar17,dVar18,local_88);
+    dVar14 = (double)lbl_803E1888;
     if (dVar14 <= dVar16) {
       dVar14 = dVar16;
-      if ((double)lbl_803E250C < dVar16) {
+      if ((double)lbl_803E188C < dVar16) {
         if ((local_80 < 0) || (local_7c < 0)) {
-          dVar14 = (double)lbl_803E250C;
+          dVar14 = (double)lbl_803E188C;
         }
         else {
-          DAT_803de1d8[3] = local_80;
-          uVar4 = (**(code **)(*DAT_803dd71c + 0x1c))(DAT_803de1d8[3]);
+          lbl_803DD560[3] = local_80;
+          uVar4 = (**(code **)(*lbl_803DCA9C + 0x1c))(lbl_803DD560[3]);
           pathcam_findTaggedNodeWindow(extraout_f1_02,dVar17,dVar18,param_4,param_5,param_6,param_7,
-                                       param_8,uVar4,local_88,DAT_803de1d8[1],pfVar8,pfVar9,
+                                       param_8,uVar4,local_88,lbl_803DD560[1],pfVar8,pfVar9,
                                        pfVar10,pfVar11,pfVar12);
           if ((local_90 < 0) || (local_8c < 0)) {
-            dVar14 = (double)lbl_803E250C;
+            dVar14 = (double)lbl_803E188C;
           }
           else {
-            DAT_803de1d8[2] = local_90;
-            uVar4 = (**(code **)(*DAT_803dd71c + 0x1c))(DAT_803de1d8[2]);
+            lbl_803DD560[2] = local_90;
+            uVar4 = (**(code **)(*lbl_803DCA9C + 0x1c))(lbl_803DD560[2]);
             pathcam_findTaggedNodeWindow(extraout_f1_03,dVar17,dVar18,param_4,param_5,param_6,
-                                         param_7,param_8,uVar4,local_98,DAT_803de1d8[1],pfVar8,
+                                         param_7,param_8,uVar4,local_98,lbl_803DD560[1],pfVar8,
                                          pfVar9,pfVar10,pfVar11,pfVar12);
             pathcam_buildWindowSamples(local_98,afStack_a8,afStack_b8,afStack_c8,afStack_d8,
                                        afStack_e8,afStack_f8,afStack_108);
-            dVar14 = FUN_8010aea8((double)*(float *)(iVar13 + 0x18),
+            dVar14 = fn_8010AC48((double)*(float *)(iVar13 + 0x18),
                                   (double)*(float *)(iVar13 + 0x1c),
                                   (double)*(float *)(iVar13 + 0x20),local_88);
-            DAT_803de1d8[0x16] = (int)((float)DAT_803de1d8[0x16] - lbl_803E250C);
+            lbl_803DD560[0x16] = (int)((float)lbl_803DD560[0x16] - lbl_803E188C);
           }
         }
       }
     }
     else if (-1 < local_88[0]) {
-      DAT_803de1d8[3] = local_88[0];
-      uVar4 = (**(code **)(*DAT_803dd71c + 0x1c))(DAT_803de1d8[3]);
+      lbl_803DD560[3] = local_88[0];
+      uVar4 = (**(code **)(*lbl_803DCA9C + 0x1c))(lbl_803DD560[3]);
       pathcam_findTaggedNodeWindow(extraout_f1_00,dVar17,dVar18,param_4,param_5,param_6,param_7,
-                                   param_8,uVar4,local_88,DAT_803de1d8[1],pfVar8,pfVar9,pfVar10,
+                                   param_8,uVar4,local_88,lbl_803DD560[1],pfVar8,pfVar9,pfVar10,
                                    pfVar11,pfVar12);
       if (local_98[0] < 0) {
-        dVar14 = (double)lbl_803E2508;
+        dVar14 = (double)lbl_803E1888;
       }
       else {
-        DAT_803de1d8[2] = local_98[0];
-        uVar4 = (**(code **)(*DAT_803dd71c + 0x1c))(DAT_803de1d8[2]);
+        lbl_803DD560[2] = local_98[0];
+        uVar4 = (**(code **)(*lbl_803DCA9C + 0x1c))(lbl_803DD560[2]);
         pathcam_findTaggedNodeWindow(extraout_f1_01,dVar17,dVar18,param_4,param_5,param_6,param_7,
-                                     param_8,uVar4,local_98,DAT_803de1d8[1],pfVar8,pfVar9,
+                                     param_8,uVar4,local_98,lbl_803DD560[1],pfVar8,pfVar9,
                                      pfVar10,pfVar11,pfVar12);
         pathcam_buildWindowSamples(local_98,afStack_a8,afStack_b8,afStack_c8,afStack_d8,afStack_e8,
                                    afStack_f8,afStack_108);
-        dVar14 = FUN_8010aea8((double)*(float *)(iVar13 + 0x18),(double)*(float *)(iVar13 + 0x1c),
+        dVar14 = fn_8010AC48((double)*(float *)(iVar13 + 0x18),(double)*(float *)(iVar13 + 0x1c),
                               (double)*(float *)(iVar13 + 0x20),local_88);
-        DAT_803de1d8[0x16] = (int)((float)DAT_803de1d8[0x16] + lbl_803E250C);
+        lbl_803DD560[0x16] = (int)((float)lbl_803DD560[0x16] + lbl_803E188C);
       }
     }
-    fVar1 = (float)((double)lbl_803E253C *
-                    (double)(float)(dVar14 - (double)(float)DAT_803de1d8[0x16]) +
-                   (double)(float)DAT_803de1d8[0x16]);
+    fVar1 = (float)((double)lbl_803E18BC *
+                    (double)(float)(dVar14 - (double)(float)lbl_803DD560[0x16]) +
+                   (double)(float)lbl_803DD560[0x16]);
     dVar16 = (double)fVar1;
-    DAT_803de1d8[0x16] = (int)fVar1;
-    dVar14 = FUN_80006a38(dVar16,afStack_a8,(float *)0x0);
+    lbl_803DD560[0x16] = (int)fVar1;
+    dVar14 = fn_80010EE0(dVar16,afStack_a8,(float *)0x0);
     *(float *)(psVar3 + 0xc) = (float)dVar14;
-    dVar14 = FUN_80006a38(dVar16,afStack_b8,(float *)0x0);
+    dVar14 = fn_80010EE0(dVar16,afStack_b8,(float *)0x0);
     *(float *)(psVar3 + 0xe) = (float)dVar14;
-    dVar14 = FUN_80006a38(dVar16,afStack_c8,(float *)0x0);
+    dVar14 = fn_80010EE0(dVar16,afStack_c8,(float *)0x0);
     *(float *)(psVar3 + 0x10) = (float)dVar14;
-    iVar6 = (**(code **)(*DAT_803dd71c + 0x1c))(DAT_803de1d8[2]);
+    iVar6 = (**(code **)(*lbl_803DCA9C + 0x1c))(lbl_803DD560[2]);
     bVar2 = *(byte *)(iVar6 + 0x3b);
     if ((bVar2 & 1) == 0) {
-      dVar14 = FUN_80006a28(dVar16,afStack_d8,(float *)0x0);
+      dVar14 = fn_80010C64(dVar16,afStack_d8,(float *)0x0);
       local_78 = (double)(longlong)(int)dVar14;
       *psVar3 = (short)(int)dVar14 + -0x8000;
     }
     if ((bVar2 & 2) == 0) {
-      dVar14 = FUN_80006a28(dVar16,afStack_e8,(float *)0x0);
+      dVar14 = fn_80010C64(dVar16,afStack_e8,(float *)0x0);
       local_78 = (double)(longlong)(int)dVar14;
       psVar3[1] = (short)(int)dVar14;
     }
     if ((bVar2 & 4) == 0) {
-      dVar14 = FUN_80006a28(dVar16,afStack_f8,(float *)0x0);
+      dVar14 = fn_80010C64(dVar16,afStack_f8,(float *)0x0);
       local_78 = (double)(longlong)(int)dVar14;
       psVar3[2] = (short)(int)dVar14;
     }
-    dVar14 = FUN_80006a38(dVar16,afStack_108,(float *)0x0);
+    dVar14 = fn_80010EE0(dVar16,afStack_108,(float *)0x0);
     *(float *)(psVar3 + 0x5a) = (float)dVar14;
-    if ((*(char *)(DAT_803de1d8 + 0x19) == '\0') &&
-       (uVar7 = FUN_8010aea8(psVar3,(uint)bVar2), uVar7 != 0)) {
-      *(undefined *)(DAT_803de1d8 + 0x19) = 1;
+    if ((*(char *)(lbl_803DD560 + 0x19) == '\0') &&
+       (uVar7 = fn_8010AEA8(psVar3,(uint)bVar2), uVar7 != 0)) {
+      *(undefined *)(lbl_803DD560 + 0x19) = 1;
     }
     dVar17 = (double)(*(float *)(psVar3 + 0xc) - *(float *)(iVar13 + 0x18));
     dVar14 = (double)(*(float *)(psVar3 + 0x10) - *(float *)(iVar13 + 0x20));
@@ -230,12 +231,12 @@ void FUN_8010b424(undefined8 param_1,undefined8 param_2,undefined8 param_3,undef
     if ((bVar2 & 2) != 0) {
       FUN_80293900((double)(float)(dVar17 * dVar17 + (double)(float)(dVar14 * dVar14)));
       uVar7 = FUN_80017730();
-      dVar14 = FUN_80006a28(dVar16,afStack_e8,(float *)0x0);
+      dVar14 = fn_80010C64(dVar16,afStack_e8,(float *)0x0);
       local_78 = (double)CONCAT44(0x43300000,uVar7 & 0xffff ^ 0x80000000);
       uStack_6c = (ushort)psVar3[1] ^ 0x80000000;
       local_70 = 0x43300000;
-      iVar6 = (int)((float)((double)(float)(local_78 - DOUBLE_803e2520) - dVar14) -
-                   (float)((double)CONCAT44(0x43300000,uStack_6c) - DOUBLE_803e2520));
+      iVar6 = (int)((float)((double)(float)(local_78 - lbl_803E18A0) - dVar14) -
+                   (float)((double)CONCAT44(0x43300000,uStack_6c) - lbl_803E18A0));
       local_68 = (longlong)iVar6;
       if (0x8000 < iVar6) {
         iVar6 = iVar6 + -0xffff;
@@ -243,7 +244,7 @@ void FUN_8010b424(undefined8 param_1,undefined8 param_2,undefined8 param_3,undef
       if (iVar6 < -0x8000) {
         iVar6 = iVar6 + 0xffff;
       }
-      psVar3[1] = psVar3[1] + (short)((int)(iVar6 * (uint)DAT_803dc070) >> 3);
+      psVar3[1] = psVar3[1] + (short)((int)(iVar6 * (uint)framesThisStep) >> 3);
     }
     if ((bVar2 & 4) != 0) {
       iVar13 = (int)psVar3[2] - (uint)*(ushort *)(iVar13 + 4);
@@ -253,25 +254,25 @@ void FUN_8010b424(undefined8 param_1,undefined8 param_2,undefined8 param_3,undef
       if (iVar13 < -0x8000) {
         iVar13 = iVar13 + 0xffff;
       }
-      psVar3[2] = psVar3[2] + (short)((int)(iVar13 * (uint)DAT_803dc070) >> 3);
+      psVar3[2] = psVar3[2] + (short)((int)(iVar13 * (uint)framesThisStep) >> 3);
     }
-    if (*DAT_803de1d8 != 0) {
+    if (*lbl_803DD560 != 0) {
       uVar4 = *(undefined4 *)(psVar3 + 0xc);
-      *(undefined4 *)(*DAT_803de1d8 + 0x18) = uVar4;
-      *(undefined4 *)(*DAT_803de1d8 + 0xc) = uVar4;
+      *(undefined4 *)(*lbl_803DD560 + 0x18) = uVar4;
+      *(undefined4 *)(*lbl_803DD560 + 0xc) = uVar4;
       uVar4 = *(undefined4 *)(psVar3 + 0xe);
-      *(undefined4 *)(*DAT_803de1d8 + 0x1c) = uVar4;
-      *(undefined4 *)(*DAT_803de1d8 + 0x10) = uVar4;
+      *(undefined4 *)(*lbl_803DD560 + 0x1c) = uVar4;
+      *(undefined4 *)(*lbl_803DD560 + 0x10) = uVar4;
       uVar4 = *(undefined4 *)(psVar3 + 0x10);
-      *(undefined4 *)(*DAT_803de1d8 + 0x20) = uVar4;
-      *(undefined4 *)(*DAT_803de1d8 + 0x14) = uVar4;
+      *(undefined4 *)(*lbl_803DD560 + 0x20) = uVar4;
+      *(undefined4 *)(*lbl_803DD560 + 0x14) = uVar4;
     }
-    FUN_800068f4((double)*(float *)(psVar3 + 0xc),(double)*(float *)(psVar3 + 0xe),
+    Obj_TransformWorldPointToLocal((double)*(float *)(psVar3 + 0xc),(double)*(float *)(psVar3 + 0xe),
                  (double)*(float *)(psVar3 + 0x10),(float *)(psVar3 + 6),(float *)(psVar3 + 8),
                  (float *)(psVar3 + 10),*(int *)(psVar3 + 0x18));
   }
   else {
-    (**(code **)(*DAT_803dd6d0 + 0x1c))(0x42,0,1,0,0,0,0xff);
+    (**(code **)(*lbl_803DCA50 + 0x1c))(0x42,0,1,0,0,0,0xff);
   }
   FUN_80286888();
   return;
@@ -280,7 +281,7 @@ void FUN_8010b424(undefined8 param_1,undefined8 param_2,undefined8 param_3,undef
 /*
  * --INFO--
  *
- * Function: FUN_8010bd7c
+ * Function: fn_8010BA98
  * EN v1.0 Address: 0x8010BD7C
  * EN v1.0 Size: 4b
  * EN v1.1 Address: 0x8010BD34
@@ -290,7 +291,7 @@ void FUN_8010b424(undefined8 param_1,undefined8 param_2,undefined8 param_3,undef
  * PAL Address: TODO
  * PAL Size: TODO
  */
-void FUN_8010bd7c(undefined4 param_1,undefined4 param_2,undefined4 *param_3,undefined4 param_4,
+void fn_8010BA98(undefined4 param_1,undefined4 param_2,undefined4 *param_3,undefined4 param_4,
                  undefined4 param_5,undefined4 param_6,undefined4 param_7,undefined4 param_8)
 {
 }
