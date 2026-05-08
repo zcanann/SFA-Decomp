b1634f7fe617c84c806beea5480da2883cd1da67
Zachary Canann <zacharycanann@gmail.com>
2026-05-07 22:56:56 -0700

    Improve objanim event step frame match

--- src/main/objanim.c
+++ src/main/objanim.c
@@ -527,11 +527,13 @@ void ObjAnim_WriteStateWord(ObjAnimComponent *objAnim,int stateIndex,short wordI
 void ObjAnim_SetCurrentEventStepFrames(ObjAnimComponent *objAnim,uint frameCount)
 {
   ObjAnimBank *bank;
+  float eventStepFrames;
 
   bank = ObjAnim_GetActiveBank(objAnim);
   if (bank != (ObjAnimBank *)0x0) {
-    bank->currentState->eventStep =
-        (int)(lbl_803DE8F4 / (ObjAnim_U32AsDouble(frameCount ^ 0x80000000) - lbl_803DE900));
+    eventStepFrames = lbl_803DE8F4 /
+                      (ObjAnim_U32AsDouble(frameCount ^ 0x80000000) - lbl_803DE900);
+    bank->currentState->eventStep = (int)eventStepFrames;
   }
 }
 #pragma peephole reset
