572a2de53ba1a4981f951298e712d9bb232d1b29
Zachary Canann <zacharycanann@gmail.com>
2026-05-08 11:59:26 -0700

    Recover synth priority table initialization

--- src/main/unknown/autos/placeholder_80279EC0.c
+++ src/main/unknown/autos/placeholder_80279EC0.c
@@ -8,16 +8,198 @@ extern u32 get_vidlist(u32 id);
 extern void fn_802737EC(u8 voice);
 
 extern u8 *lbl_803DE268;
+extern u8 lbl_803BD150[];
 extern u8 gSynthInitialized;
+extern u8 lbl_803CA2D0[];
 extern u8 lbl_803CAB50[];
 extern u8 lbl_803CAAD0[][16];
+extern u16 lbl_803DE2FC;
+extern u8 lbl_803DE2FE;
+extern u8 lbl_803DE2FF;
+extern u8 lbl_803DE300;
+extern u8 lbl_803DE301;
 
 /*
- * fn_80279C7C - large voice/slot table init (~204 instructions). Stubbed.
+ * Initialize the voice priority and group linked-list tables.
  */
-#pragma dont_inline on
-void fn_80279C7C(void) {}
-#pragma dont_inline reset
+void fn_80279C7C(void)
+{
+    s8 value;
+    u8 lastVoice;
+    int remaining;
+    u8 *groupHead;
+    u32 progress;
+    u8 *activeSlot;
+    s8 *freeSlot;
+    u32 count;
+    u32 batches;
+
+    progress = 0;
+    count = lbl_803BD150[0x210];
+    if (count != 0) {
+        if (count > 8) {
+            batches = (count - 1) >> 3;
+            freeSlot = (s8 *)(lbl_803CA2D0 + 0xec0);
+            if (count != 8) {
+                do {
+                    value = progress;
+                    freeSlot[0] = value - 1;
+                    freeSlot[1] = value + 1;
+                    *(u16 *)(freeSlot + 2) = 1;
+                    freeSlot[4] = value;
+                    freeSlot[5] = value + 2;
+                    progress += 8;
+                    *(u16 *)(freeSlot + 6) = 1;
+                    freeSlot[8] = value + 1;
+                    freeSlot[9] = value + 3;
+                    *(u16 *)(freeSlot + 10) = 1;
+                    freeSlot[12] = value + 2;
+                    freeSlot[13] = value + 4;
+                    *(u16 *)(freeSlot + 14) = 1;
+                    freeSlot[16] = value + 3;
+                    freeSlot[17] = value + 5;
+                    *(u16 *)(freeSlot + 18) = 1;
+                    freeSlot[20] = value + 4;
+                    freeSlot[21] = value + 6;
+                    *(u16 *)(freeSlot + 22) = 1;
+                    freeSlot[24] = value + 5;
+                    freeSlot[25] = value + 7;
+                    *(u16 *)(freeSlot + 26) = 1;
+                    freeSlot[28] = value + 6;
+                    freeSlot[29] = value + 8;
+                    *(u16 *)(freeSlot + 30) = 1;
+                    freeSlot += 0x20;
+                    batches--;
+                } while (batches != 0);
+            }
+        }
+        freeSlot = (s8 *)(lbl_803CA2D0 + 0xec0 + progress * 4);
+        remaining = lbl_803BD150[0x210] - progress;
+        if (progress < lbl_803BD150[0x210]) {
+            do {
+                value = progress;
+                freeSlot[0] = value - 1;
+                progress++;
+                freeSlot[1] = value + 1;
+                *(u16 *)(freeSlot + 2) = 1;
+                freeSlot += 4;
+                remaining--;
+            } while (remaining != 0);
+        }
+    }
+
+    lastVoice = lbl_803BD150[0x210];
+    *(u8 *)(lbl_803CA2D0 + 0xec0) = 0xff;
+    progress = 0;
+    count = lbl_803BD150[0x210];
+    *(u8 *)(lbl_803CA2D0 + 0xec0 - 3 + count * 4) = 0xff;
+    lbl_803DE300 = lastVoice - 1;
+    lbl_803DE301 = 0;
+    if (count != 0) {
+        if (count > 8) {
+            batches = (count - 1) >> 3;
+            activeSlot = lbl_803CA2D0 + 0x8c0;
+            if (count != 8) {
+                do {
+                    *(u16 *)(activeSlot + 2) = 0;
+                    progress += 8;
+                    *(u16 *)(activeSlot + 6) = 0;
+                    *(u16 *)(activeSlot + 10) = 0;
+                    *(u16 *)(activeSlot + 14) = 0;
+                    *(u16 *)(activeSlot + 18) = 0;
+                    *(u16 *)(activeSlot + 22) = 0;
+                    *(u16 *)(activeSlot + 26) = 0;
+                    *(u16 *)(activeSlot + 30) = 0;
+                    activeSlot += 0x20;
+                    batches--;
+                } while (batches != 0);
+            }
+        }
+        activeSlot = lbl_803CA2D0 + 0x8c0 + progress * 4;
+        remaining = lbl_803BD150[0x210] - progress;
+        if (progress < lbl_803BD150[0x210]) {
+            do {
+                *(u16 *)(activeSlot + 2) = 0;
+                activeSlot += 4;
+                remaining--;
+            } while (remaining != 0);
+        }
+    }
+
+    remaining = 4;
+    groupHead = lbl_803CA2D0 + 0x9c0;
+    do {
+        groupHead[0] = 0xff;
+        groupHead[1] = 0xff;
+        groupHead[2] = 0xff;
+        groupHead[3] = 0xff;
+        groupHead[4] = 0xff;
+        groupHead[5] = 0xff;
+        groupHead[6] = 0xff;
+        groupHead[7] = 0xff;
+        groupHead[8] = 0xff;
+        groupHead[9] = 0xff;
+        groupHead[10] = 0xff;
+        groupHead[11] = 0xff;
+        groupHead[12] = 0xff;
+        groupHead[13] = 0xff;
+        groupHead[14] = 0xff;
+        groupHead[15] = 0xff;
+        groupHead[16] = 0xff;
+        groupHead[17] = 0xff;
+        groupHead[18] = 0xff;
+        groupHead[19] = 0xff;
+        groupHead[20] = 0xff;
+        groupHead[21] = 0xff;
+        groupHead[22] = 0xff;
+        groupHead[23] = 0xff;
+        groupHead[24] = 0xff;
+        groupHead[25] = 0xff;
+        groupHead[26] = 0xff;
+        groupHead[27] = 0xff;
+        groupHead[28] = 0xff;
+        groupHead[29] = 0xff;
+        groupHead[30] = 0xff;
+        groupHead[31] = 0xff;
+        groupHead[32] = 0xff;
+        groupHead[33] = 0xff;
+        groupHead[34] = 0xff;
+        groupHead[35] = 0xff;
+        groupHead[36] = 0xff;
+        groupHead[37] = 0xff;
+        groupHead[38] = 0xff;
+        groupHead[39] = 0xff;
+        groupHead[40] = 0xff;
+        groupHead[41] = 0xff;
+        groupHead[42] = 0xff;
+        groupHead[43] = 0xff;
+        groupHead[44] = 0xff;
+        groupHead[45] = 0xff;
+        groupHead[46] = 0xff;
+        groupHead[47] = 0xff;
+        groupHead[48] = 0xff;
+        groupHead[49] = 0xff;
+        groupHead[50] = 0xff;
+        groupHead[51] = 0xff;
+        groupHead[52] = 0xff;
+        groupHead[53] = 0xff;
+        groupHead[54] = 0xff;
+        groupHead[55] = 0xff;
+        groupHead[56] = 0xff;
+        groupHead[57] = 0xff;
+        groupHead[58] = 0xff;
+        groupHead[59] = 0xff;
+        groupHead[60] = 0xff;
+        groupHead[61] = 0xff;
+        groupHead[62] = 0xff;
+        groupHead[63] = 0xff;
+        groupHead += 0x40;
+        remaining--;
+    } while (remaining != 0);
+    lbl_803DE2FC = 0xffff;
+    lbl_803DE2FE = 0;
+    lbl_803DE2FF = 0;
+}
 
 /*
  * Voice cleanup: if voice handle is valid, break the active voice and
