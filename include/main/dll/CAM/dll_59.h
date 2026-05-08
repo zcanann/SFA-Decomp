ccc57b81540380d44372ab85df4e24a8f31216ff
Zachary Canann <zacharycanann@gmail.com>
2026-05-07 20:39:47 -0700

    Recover staff camera state helper

--- include/main/dll/CAM/dll_59.h
+++ include/main/dll/CAM/dll_59.h
@@ -3,6 +3,7 @@
 
 #include "ghidra_import.h"
 
-void FUN_8010747c(undefined4 param_1,undefined4 param_2,short *param_3);
+void fn_8010747C(undefined4 param_1,undefined4 param_2,short *param_3);
+void fn_80107AEC(f32 *param_1);
 
 #endif /* MAIN_DLL_CAM_DLL_59_H_ */
--- src/main/dll/CAM/dll_59.c
+++ src/main/dll/CAM/dll_59.c
@@ -3,36 +3,37 @@
 
 extern undefined4 FUN_800033a8();
 extern undefined4 FUN_80006824();
-extern undefined4 FUN_800068f4();
+extern undefined4 Obj_TransformWorldPointToLocal();
 extern undefined4 FUN_80006a1c();
 extern undefined4 FUN_80006a38();
-extern int FUN_80017730();
+extern int getAngle();
 extern undefined4 FUN_80017830();
 extern undefined4 camcontrol_getTargetPosition();
 extern undefined8 camcontrol_buildPathPoints();
-extern int FUN_80286840();
+extern int Camera_GetCurrentViewSlot();
 extern undefined4 FUN_8028688c();
-extern double FUN_80293900();
-extern undefined4 FUN_80293f90();
-extern undefined4 FUN_80294964();
+extern double sqrtf();
+extern undefined4 fn_80293E80();
+extern undefined4 sin();
 
-extern undefined4* DAT_803dd6d0;
-extern undefined4* gCamcontrolPathState;
-extern f64 DOUBLE_803e23d0;
-extern f32 lbl_803E23C0;
-extern f32 lbl_803E23C4;
-extern f32 lbl_803E23D8;
-extern f32 lbl_803E23DC;
-extern f32 lbl_803E23E8;
-extern f32 lbl_803E23EC;
-extern f32 lbl_803E23F0;
-extern f32 lbl_803E23F4;
-extern f32 lbl_803E23F8;
+extern undefined4* lbl_803DCA50;
+extern undefined4* lbl_803DD538;
+extern f32* lbl_803DD540;
+extern f64 lbl_803E1750;
+extern f32 lbl_803E1740;
+extern f32 lbl_803E1744;
+extern f32 lbl_803E1758;
+extern f32 lbl_803E175C;
+extern f32 lbl_803E1768;
+extern f32 lbl_803E176C;
+extern f32 lbl_803E1770;
+extern f32 lbl_803E1774;
+extern f32 lbl_803E1778;
 
 /*
  * --INFO--
  *
- * Function: FUN_8010747c
+ * Function: fn_8010747C
  * EN v1.0 Address: 0x8010747C
  * EN v1.0 Size: 4b
  * EN v1.1 Address: 0x80107718
@@ -42,8 +43,16 @@ extern f32 lbl_803E23F8;
  * PAL Address: TODO
  * PAL Size: TODO
  */
-void FUN_8010747c(undefined4 param_1,undefined4 param_2,short *param_3)
+void fn_8010747C(undefined4 param_1,undefined4 param_2,short *param_3)
 {
+}
+
+void fn_80107AEC(f32 *param_1)
+{
+  lbl_803DD540[7] = param_1[0];
+  lbl_803DD540[9] = param_1[1];
+  lbl_803DD540[0xb] = param_1[2];
+  lbl_803DD540[0xc] = param_1[3];
 }
 
 
@@ -52,8 +61,7 @@ void CameraModeStaffAnim_release(void) {}
 void CameraModeStaffAnim_initialise(void) {}
 
 /* fn_X(lbl); lbl = 0; */
-extern u32 lbl_803DD540;
-extern void fn_80023800(u32);
+extern void fn_80023800(void *);
 #pragma scheduling off
 #pragma peephole off
 void CameraModeBike_free(void) { fn_80023800(lbl_803DD540); lbl_803DD540 = 0; }
