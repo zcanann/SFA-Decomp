f5f5ce62eb1cca59fb9d714ce611a883fe022391
Jack Price-Burns <jackpriceburns@outlook.com>
2026-05-12 22:34:16 +0100

    Match ediblemushroom_free in NWsfx.c

--- include/main/dll/NW/NWsfx.h
+++ include/main/dll/NW/NWsfx.h
@@ -3,10 +3,7 @@
 
 #include "ghidra_import.h"
 
-void FUN_801d083c(undefined8 param_1,undefined8 param_2,double param_3,undefined8 param_4,
-                 undefined8 param_5,undefined8 param_6,undefined8 param_7,undefined8 param_8,
-                 short *param_9,float *param_10,int param_11,float *param_12,undefined4 param_13,
-                 undefined4 param_14,int param_15,undefined4 param_16);
 int ediblemushroom_getExtraSize(void);
+void ediblemushroom_free(int obj);
 
 #endif /* MAIN_DLL_NW_NWSFX_H_ */
