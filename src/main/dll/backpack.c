2e694982983c19c545a0311d82ef292ea016aae0
Jack Price-Burns <jackpriceburns@outlook.com>
2026-05-12 21:59:33 +0100

    Decompile fn_80164C44 in backpack.c (98.75% match)

--- src/main/dll/backpack.c
+++ src/main/dll/backpack.c
@@ -5,9 +5,16 @@ extern void fn_801641B0(int obj);
 extern void fn_80164940(int obj);
 extern void fn_80164C44(int obj);
 extern int GameBit_Set(int eventId, int value);
+extern void Sfx_PlayFromObject(int obj, int sfxId);
+extern void Sfx_KeepAliveLoopedObjectSound(int obj, int sfxId);
+extern void ObjHits_DisableObject(int obj);
+extern void ObjHits_SetHitVolumeSlot(int obj, int a, int b, int c);
+extern void fn_80098B18(int obj, float f, int a, int b, int c, int d);
 
 extern void* lbl_803DCAB8;
 extern void* lbl_803DCA8C;
+extern void* pDll_expgfx;
+extern f32 lbl_803E2FC8;
 
 /*
  * --INFO--
@@ -31,6 +38,81 @@ void tumbleweed_update(int obj) {
 
 /* 8b "li r3, N; blr" returners. */
 int fn_801650D0(void) { return 0x0; }
+
+/*
+ * --INFO--
+ *
+ * Function: fn_80164C44
+ * EN v1.0 Address: 0x80164C44
+ * EN v1.0 Size: 672b
+ */
+#pragma push
+#pragma scheduling off
+#pragma peephole off
+void fn_80164C44(int obj) {
+    int aux = *(int*)(obj + 0xb8);
+    int i;
+    s16 type;
+
+    if ((*(u8*)(aux + 0x27a) & 1) != 0) {
+        switch (*(s16*)(obj + 0x46)) {
+        case 0x4ba:
+        case 0x39d:
+        case 0x4c1:
+            i = 0x14;
+            do {
+                (*(void(**)(int, int, int, int, int, int))(*(int*)pDll_expgfx + 0x8))(obj, 0x34d, 0, 2, -1, 0);
+                i = i - 1;
+            } while (i != 0);
+            break;
+        default:
+            i = 0x14;
+            do {
+                (*(void(**)(int, int, int, int, int, int))(*(int*)pDll_expgfx + 0x8))(obj, 0x32e, 0, 2, -1, 0);
+                i = i - 1;
+            } while (i != 0);
+            break;
+        }
+        Sfx_PlayFromObject(obj, 0x27d);
+        *(u8*)(aux + 0x27a) = (u8)(*(u8*)(aux + 0x27a) & ~1);
+    }
+
+    if ((*(u8*)(aux + 0x27a) & 2) != 0) {
+        switch (*(s16*)(obj + 0x46)) {
+        case 0x4ba:
+        case 0x39d:
+        case 0x4c1:
+            (*(void(**)(int, int, int, int, int, int))(*(int*)pDll_expgfx + 0x8))(obj, 0x34c, 0, 2, -1, 0);
+            break;
+        default:
+            (*(void(**)(int, int, int, int, int, int))(*(int*)pDll_expgfx + 0x8))(obj, 0x32d, 0, 2, -1, 0);
+            break;
+        }
+        *(u8*)(aux + 0x27a) = (u8)(*(u8*)(aux + 0x27a) & ~2);
+    }
+
+    if ((*(u8*)(aux + 0x27a) & 4) != 0) {
+        *(u8*)(obj + 0x36) = 0;
+        *(u8*)(aux + 0x278) = 5;
+        *(f32*)(aux + 0x270) = lbl_803E2FC8;
+        ObjHits_DisableObject(obj);
+        *(u8*)(aux + 0x27a) = (u8)(*(u8*)(aux + 0x27a) & ~4);
+    }
+
+    if ((*(u8*)(aux + 0x27a) & 0x10) != 0 && (*(u16*)(obj + 0xb0) & 0x800) != 0) {
+        u8 bVar2;
+        ObjHits_SetHitVolumeSlot(obj, 0x1f, 1, 0);
+        bVar2 = (u8)(*(s8*)(aux + 0x27b) + 1);
+        *(u8*)(aux + 0x27b) = bVar2;
+        if ((int)(uint)bVar2 % 6 == 0) {
+            fn_80098B18(obj, *(f32*)(obj + 0x8), 1, 0, 0, 0);
+        } else {
+            fn_80098B18(obj, *(f32*)(obj + 0x8), 1, 3, 0, 0);
+        }
+        Sfx_KeepAliveLoopedObjectSound(obj, 0x451);
+    }
+}
+#pragma pop
 
 /*
  * --INFO--
