c4287e718dcba10baf6483ce5a8f694b2ccf92a8
Zachary Canann <zacharycanann@gmail.com>
2026-05-15 20:10:45 -0700

    Update TREX Lazerwall header prototypes

--- include/main/dll/TREX/TREX_Lazerwall.h
+++ include/main/dll/TREX/TREX_Lazerwall.h
@@ -3,14 +3,8 @@
 
 #include "ghidra_import.h"
 
-undefined4 FUN_801e67bc(undefined4 param_1,int param_2);
-undefined4 FUN_801e6818(undefined4 param_1,int param_2);
-void FUN_801e6874(int param_1,int param_2,undefined4 param_3);
-void FUN_801e68c8(int param_1);
-void FUN_801e6920(int param_1);
-void FUN_801e6948(undefined8 param_1,double param_2,double param_3,undefined8 param_4,
-                 undefined8 param_5,undefined8 param_6,undefined8 param_7,undefined8 param_8,
-                 int param_9);
-void FUN_801e6bec(int param_1);
+int fn_801E67BC(int obj, int param2);
+int fn_801E6990(void);
+int fn_801E69C8(int obj);
 
 #endif /* MAIN_DLL_TREX_TREX_LAZERWALL_H_ */
