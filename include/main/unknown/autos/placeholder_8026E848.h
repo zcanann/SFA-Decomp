2937e97f212604947f625199a1a330ee40edd18e
Zachary Canann <zacharycanann@gmail.com>
2026-05-15 21:47:05 -0700

    Update synth placeholder headers

--- include/main/unknown/autos/placeholder_8026E848.h
+++ include/main/unknown/autos/placeholder_8026E848.h
@@ -3,6 +3,9 @@
 
 #include "ghidra_import.h"
 
-int FUN_8026e0e4(int param_1,byte param_2,uint *param_3);
+void fn_8026E0E4(void);
+void fn_8026E864(void);
+void fn_8026E90C(u8 voice);
+int fn_8026E9D0(u8 voice, int param);
 
 #endif /* MAIN_UNKNOWN_AUTOS_PLACEHOLDER_8026E848_H_ */
--- include/main/unknown/autos/placeholder_8026F134.h
+++ include/main/unknown/autos/placeholder_8026F134.h
@@ -3,7 +3,11 @@
 
 #include "ghidra_import.h"
 
-undefined4 FUN_8026ec44(uint param_1,undefined4 param_2);
-void FUN_8026ec4c(int param_1);
+void fn_8026EC44(void);
+void fn_8026F30C(void);
+void synthSetStudioChannelScale(int value, u8 bank, u32 key);
+int synthGetVoiceSlotChannelScale(int state);
+void fn_8026F5B8(int state);
+void audioFn_8026f630(void);
 
 #endif /* MAIN_UNKNOWN_AUTOS_PLACEHOLDER_8026F134_H_ */
