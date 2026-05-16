27ddd3e48d06d52117c5477501f003c61f531c13
Zachary Canann <zacharycanann@gmail.com>
2026-05-15 20:15:38 -0700

    Propagate WM Galleon update prototype

--- include/main/dll/WC/WClaser.h
+++ include/main/dll/WC/WClaser.h
@@ -4,9 +4,9 @@
 #include "ghidra_import.h"
 
 undefined4
-FUN_801f02f0(undefined8 param_1,double param_2,double param_3,undefined8 param_4,undefined8 param_5,
-            undefined8 param_6,undefined8 param_7,undefined8 param_8,int param_9,undefined4 param_10
-            ,int param_11);
+WM_Galleon_update(undefined8 param_1,double param_2,double param_3,undefined8 param_4,
+                 undefined8 param_5,undefined8 param_6,undefined8 param_7,undefined8 param_8,
+                 int param_9,undefined4 param_10,int param_11);
 void FUN_801f0674(int param_1,int param_2);
 void FUN_801f0678(void);
 void FUN_801f0718(short *param_1);
