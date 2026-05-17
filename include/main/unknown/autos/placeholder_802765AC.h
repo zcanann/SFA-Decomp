8656098d2257704a35364d2e27f4d5946abf067c
Zachary Canann <zacharycanann@gmail.com>
2026-05-17 11:55:35 -0700

    Clean macro command header aliases

--- include/main/unknown/autos/placeholder_802765AC.h
+++ include/main/unknown/autos/placeholder_802765AC.h
@@ -3,26 +3,6 @@
 
 #include "ghidra_import.h"
 
-void FUN_8027656c(int param_1,uint *param_2);
-void FUN_80276570(int param_1,undefined4 *param_2);
-void FUN_80276574(int param_1,uint *param_2,uint param_3);
-uint FUN_80276578(uint param_1,short param_2);
-void FUN_80276580(int param_1,uint *param_2,int param_3);
-void FUN_80276584(int param_1,uint *param_2);
-void FUN_80276588(int param_1,int param_2,uint *param_3,undefined4 param_4,uint param_5,uint param_6
-                 ,uint param_7);
-void FUN_8027658c(int param_1,uint *param_2);
-uint FUN_80276590(int param_1,int param_2,uint param_3);
-int FUN_80276598(int param_1,int param_2,uint param_3);
-void FUN_802765a0(int param_1,int param_2,uint param_3,undefined4 param_4);
-void FUN_802765a4(int param_1,uint *param_2,byte param_3);
-void FUN_802765a8(int param_1,uint *param_2);
-void FUN_802765ac(int param_1,uint *param_2);
-void FUN_802765b0(int *param_1);
-void FUN_802765b4(uint param_1);
-void FUN_802765b8(int *param_1);
-uint FUN_802765bc(int *param_1);
-void FUN_802765c4(int *param_1,int param_2);
 void mcmdRandomKey(int state, u32 *args);
 void SelectSource(int state, int ctrlObj, u32 *args, int unused, u32 stateFlag,
                   u32 activeFlag, u32 dirtyFlag);
