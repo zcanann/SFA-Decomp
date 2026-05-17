bc3d993657fc2bbb21ad9efc14c42df6c944d800
Zachary Canann <zacharycanann@gmail.com>
2026-05-17 11:40:28 -0700

    Expose virtual sample update API

--- include/main/unknown/autos/placeholder_8027B41C.h
+++ include/main/unknown/autos/placeholder_8027B41C.h
@@ -4,4 +4,7 @@
 #include "ghidra_import.h"
 #include "main/audio/synth_virtual_sample.h"
 
+void synthUpdateVirtualSamples(void);
+void synthResetLoadedGroupCount(void);
+
 #endif /* MAIN_UNKNOWN_AUTOS_PLACEHOLDER_8027B41C_H_ */
