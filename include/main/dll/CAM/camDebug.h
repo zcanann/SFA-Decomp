fad2b2fa9333a3957bf8dfe91dff10459c4debca
Zachary Canann <zacharycanann@gmail.com>
2026-05-17 14:19:11 -0700

    Name climb camera init declaration

--- include/main/dll/CAM/camDebug.h
+++ include/main/dll/CAM/camDebug.h
@@ -3,7 +3,7 @@
 
 #include "ghidra_import.h"
 
-void FUN_8010d810(undefined4 param_1,int param_2,int param_3);
+void CameraModeClimb_init(undefined4 param_1,int param_2,s8 *param_3);
 void FUN_8010daf8(undefined2 *param_1,undefined4 param_2,undefined2 *param_3);
 
 #endif /* MAIN_DLL_CAM_CAMDEBUG_H_ */
