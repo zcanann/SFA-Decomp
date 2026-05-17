ad9d30c8123dfaf8730c22fd67e1432faaeb7dbc
Zachary Canann <zacharycanann@gmail.com>
2026-05-17 14:05:31 -0700

    Propagate rachnop callback declarations

--- include/main/dll/duster.h
+++ include/main/dll/duster.h
@@ -6,7 +6,7 @@
 uint FUN_8015536c(double param_1,short *param_2,int param_3,uint param_4);
 void FUN_801556d4(double param_1,double param_2,float *param_3,float *param_4);
 void FUN_80155830(int *param_1,int param_2);
-void FUN_80155b08(uint param_1,int param_2,undefined4 param_3,int param_4);
+void rachnopUpdateWhileFrozen(uint param_1,int param_2,undefined4 param_3,int param_4);
 void FUN_80155b6c(undefined8 param_1,double param_2,double param_3,undefined8 param_4,
                  undefined8 param_5,undefined8 param_6,undefined8 param_7,undefined8 param_8,
                  int *param_9,int param_10);
@@ -16,7 +16,7 @@ void FUN_80155cac(undefined8 param_1,double param_2,double param_3,undefined8 pa
 void FUN_80155e00(undefined8 param_1,double param_2,double param_3,undefined8 param_4,
                  undefined8 param_5,undefined8 param_6,undefined8 param_7,undefined8 param_8,
                  int *param_9,int param_10);
-void FUN_8015603c(undefined4 param_1,int param_2);
+void rachnopInit(undefined4 param_1,int param_2);
 void FUN_801560a0(undefined8 param_1,undefined8 param_2,double param_3,undefined8 param_4,
                  undefined8 param_5,undefined8 param_6,undefined8 param_7,undefined8 param_8,
                  uint param_9,int param_10);
