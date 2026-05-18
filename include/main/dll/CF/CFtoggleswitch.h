c7aa892c6fabe6bd021549d46dd71e737581ca4d
Zachary Canann <zacharycanann@gmail.com>
2026-05-18 12:36:28 -0700

    Expose CF descriptor callbacks

--- include/main/dll/CF/CFtoggleswitch.h
+++ include/main/dll/CF/CFtoggleswitch.h
@@ -3,6 +3,14 @@
 
 #include "ghidra_import.h"
 #include "main/objanim_update.h"
+#include "main/object_descriptor.h"
+
+extern ObjectDescriptor gMagicCaveBottomObjDescriptor;
+extern ObjectDescriptor gMagicCaveTopObjDescriptor;
+extern ObjectDescriptor gTrickyGuardSpotObjDescriptor;
+extern ObjectDescriptor gInfoTextObjDescriptor;
+extern ObjectDescriptor gCCTestInfotObjDescriptor;
+extern ObjectDescriptor gDeathGasObjDescriptor;
 
 void magiccavebottom_update(undefined4 param_1,undefined4 param_2,ObjAnimUpdateState *animUpdate);
 void FUN_8018aee4(void);
@@ -23,5 +31,12 @@ void FUN_8018b5a0(undefined8 param_1,undefined8 param_2,undefined8 param_3,undef
                  int param_9);
 void FUN_8018b6ac(undefined8 param_1,double param_2,double param_3,undefined8 param_4,
                  undefined8 param_5,undefined8 param_6,undefined8 param_7,undefined8 param_8);
+int magiccavetop_getExtraSize(void);
+int trickyguardspot_getExtraSize(void);
+int infotext_getExtraSize(void);
+int cctestinfot_getExtraSize(void);
+int deathgas_getExtraSize(void);
+void trickyguardspot_free(int x);
+void trickyguardspot_render(void);
 
 #endif /* MAIN_DLL_CF_CFTOGGLESWITCH_H_ */
--- include/main/dll/CF/treasureRelated0177.h
+++ include/main/dll/CF/treasureRelated0177.h
@@ -2,6 +2,11 @@
 #define MAIN_DLL_CF_TREASURERELATED0177_H_
 
 #include "ghidra_import.h"
+#include "main/object_descriptor.h"
+
+extern ObjectDescriptor gCampFireObjDescriptor;
+extern ObjectDescriptor gKT_TorchObjDescriptor;
+extern ObjectDescriptor gCFCrateObjDescriptor;
 
 void dll_127_update(int obj);
 void FUN_8018cdb0(undefined8 param_1,double param_2,double param_3,undefined8 param_4,
@@ -13,9 +18,20 @@ void dll_127_init(short *param_1,int param_2);
 void FUN_8018d064(int param_1);
 void FUN_8018d0b4(int param_1);
 void FUN_8018d110(void);
+int campfire_getExtraSize(void);
+int campfire_func08(void);
 void campfire_free(int obj);
 void campfire_render(int obj, int param_2, int param_3, int param_4, int param_5, s8 visible);
+int kt_torch_getExtraSize(void);
+int kt_torch_func08(void);
+void kt_torch_free(void);
+void kt_torch_render(int p1,int p2,int p3,int p4,int p5,s8 visible);
+void kt_torch_hitDetect(void);
 void kt_torch_update(int obj);
+void kt_torch_release(void);
+void kt_torch_initialise(void);
+int cfccrate_getExtraSize(void);
+int cfccrate_func08(void);
 void cfccrate_free(int obj);
 
 #endif /* MAIN_DLL_CF_TREASURERELATED0177_H_ */
