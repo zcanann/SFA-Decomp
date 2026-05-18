ed88c0ba14efd9bf5c357915847fb05f8e3fd80d
Zachary Canann <zacharycanann@gmail.com>
2026-05-17 19:53:13 -0700

    Expose post animation control layouts

--- include/main/dll/FRONT/POST.h
+++ include/main/dll/FRONT/POST.h
@@ -3,9 +3,38 @@
 
 #include "ghidra_import.h"
 
-typedef struct PostObjAnimComponent PostObjAnimComponent;
-typedef struct PostObject PostObject;
-typedef struct PostControl PostControl;
+typedef struct PostMotionTarget {
+  u8 pad0[0x5a];
+  s16 yawA;
+  u8 pad5c[2];
+  s16 yawB;
+  u8 pad60[2];
+  u8 flags;
+} PostMotionTarget;
+
+typedef struct PostObject {
+  u8 pad0[0x54];
+  PostMotionTarget *motion;
+} PostObject;
+
+typedef struct PostObjAnimComponent {
+  s16 yaw;
+  u8 pad2[0x9e];
+  s16 currentMove;
+} PostObjAnimComponent;
+
+typedef struct PostControl {
+  u8 pad0[0x10];
+  u8 primary[0xc];
+  u8 secondary[0x5a0];
+  s16 events[0x1e];
+  int blocked;
+  u8 pad5fc[0x10];
+  s16 eventState;
+  s16 yawLimit;
+  u8 contactAnim;
+  u8 flags;
+} PostControl;
 
 int objAnimFn_80115650(PostObjAnimComponent *objAnim,PostObject *obj,int *turning,
                 PostControl *control,float *turnSpeed,short *moves);
--- src/main/dll/FRONT/POST.c
+++ src/main/dll/FRONT/POST.c
@@ -3,39 +3,6 @@
 #include "main/objanim.h"
 #include "main/objlib.h"
 
-typedef struct PostMotionTarget {
-  u8 pad0[0x5a];
-  s16 yawA;
-  u8 pad5c[2];
-  s16 yawB;
-  u8 pad60[2];
-  u8 flags;
-} PostMotionTarget;
-
-typedef struct PostObject {
-  u8 pad0[0x54];
-  PostMotionTarget *motion;
-} PostObject;
-
-typedef struct PostObjAnimComponent {
-  s16 yaw;
-  u8 pad2[0x9e];
-  s16 currentMove;
-} PostObjAnimComponent;
-
-typedef struct PostControl {
-  u8 pad0[0x10];
-  u8 primary[0xc];
-  u8 secondary[0x5a0];
-  s16 events[0x1e];
-  int blocked;
-  u8 pad5fc[0x10];
-  s16 eventState;
-  s16 yawLimit;
-  u8 contactAnim;
-  u8 flags;
-} PostControl;
-
 extern PostMotionTarget *seqFn_800394a0(void);
 extern void fn_80038F1C(int a, int b);
 extern s16 objMathFn_8003a380(double distance, PostObjAnimComponent *objAnim, PostObject *obj,
