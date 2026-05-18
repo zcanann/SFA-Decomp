9b3ae6457f145229d58afa2421ca20ff18f8bc6a
Zachary Canann <zacharycanann@gmail.com>
2026-05-18 13:28:40 -0700

    Expose SnowBike descriptor accessors

--- include/main/dll/DR/DRcradle.h
+++ include/main/dll/DR/DRcradle.h
@@ -2,9 +2,21 @@
 #define MAIN_DLL_DR_DRCRADLE_H_
 
 #include "ghidra_import.h"
+#include "main/object_descriptor.h"
+
+extern ObjectDescriptor24 gSnowBikeObjDescriptor;
 
 void FUN_801ec7a0(uint param_1,int param_2);
 void FUN_801ecd30(undefined4 param_1,int param_2);
 void FUN_801ecdec(undefined4 param_1,int param_2);
+void SnowBike_func17(void);
+void SnowBike_func16(void);
+int SnowBike_func0E(void);
+int SnowBike_render2(void);
+int SnowBike_getExtraSize(void);
+int SnowBike_func08(void);
+u8 SnowBike_func0B(int *obj);
+s32 SnowBike_func14(int *obj);
+s32 SnowBike_getType(int *obj);
 
 #endif /* MAIN_DLL_DR_DRCRADLE_H_ */
