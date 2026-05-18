b92fcce8770e271ac48320b8d61dead6476086af
Zachary Canann <zacharycanann@gmail.com>
2026-05-17 23:13:41 -0700

    Name remaining exact callback stubs

--- src/main/unknown/autos/placeholder_80220608.c
+++ src/main/unknown/autos/placeholder_80220608.c
@@ -13,15 +13,15 @@ void fn_80228CDC(void) {}
 void fn_80228CE0(void) {}
 void fn_80228ED4(void) {}
 void fn_80228ED8(void) {}
-int fn_8022F52C(void) { return 0x0; }
-int fn_8022F534(void) { return 0x0; }
-void fn_8022F53C(void) {}
-void fn_8022F540(void) {}
-void fn_8022F544(void) {}
-void fn_8022F548(void) {}
-void fn_8022F54C(void) {}
-void fn_8022F550(void) {}
-void fn_8022F554(void) {}
+int Dummy29E_getExtraSize(void) { return 0x0; }
+int Dummy29E_func08_ret_0(void) { return 0x0; }
+void Dummy29E_free(void) {}
+void Dummy29E_render(void) {}
+void Dummy29E_hitDetect(void) {}
+void Dummy29E_update(void) {}
+void Dummy29E_init(void) {}
+void Dummy29E_release(void) {}
+void Dummy29E_initialise(void) {}
 int fn_80231074(void) { return 0xc; }
 int fn_8023107C(void) { return 0x0; }
 void fn_80231344(void) {}
