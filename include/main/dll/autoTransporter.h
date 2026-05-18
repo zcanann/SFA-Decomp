5e26412a9329c1239b499b65d163603a9077477f
Zachary Canann <zacharycanann@gmail.com>
2026-05-18 11:17:53 -0700

    Expose DoorF4 and SidekickBall callbacks

--- include/main/dll/autoTransporter.h
+++ include/main/dll/autoTransporter.h
@@ -2,6 +2,10 @@
 #define MAIN_DLL_AUTOTRANSPORTER_H_
 
 #include "ghidra_import.h"
+#include "main/object_descriptor.h"
+
+extern ObjectDescriptor gDoorF4ObjDescriptor;
+extern ObjectDescriptor gSidekickBallObjDescriptor;
 
 void FUN_80178338(undefined4 param_1);
 void FUN_80178370(undefined8 param_1,undefined8 param_2,undefined8 param_3,undefined8 param_4,
@@ -19,5 +23,19 @@ void FUN_80179820(int param_1);
 void FUN_80179848(undefined2 *param_1);
 void FUN_801799bc(undefined2 *param_1,int param_2);
 uint FUN_801799c0(int param_1);
+
+int doorf4_getExtraSize(void);
+int doorf4_func08(void);
+void doorf4_free(int obj);
+void doorf4_render(int p1, int p2, int p3, int p4, int p5, s8 visible);
+void doorf4_hitDetect(void);
+void doorf4_update(void);
+void doorf4_init(void);
+void doorf4_release(void);
+void doorf4_initialise(void);
+
+int sidekickball_getExtraSize(void);
+void sidekickball_free(int obj);
+void sidekickball_render(int p1, int p2, int p3, int p4, int p5, s8 visible);
 
 #endif /* MAIN_DLL_AUTOTRANSPORTER_H_ */
