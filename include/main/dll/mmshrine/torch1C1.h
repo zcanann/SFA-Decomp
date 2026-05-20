c444eaf5963c7ba799f019627b3005c6c06522d5
Jack Price-Burns <jackpriceburns@outlook.com>
2026-05-21 00:16:45 +0200

    torch1C1: ecsh_shrine_func0B + setScale 0 -> 100% (104b drift fix)

--- include/main/dll/mmshrine/torch1C1.h
+++ include/main/dll/mmshrine/torch1C1.h
@@ -3,7 +3,7 @@
 
 #include "ghidra_import.h"
 
-void ecsh_shrine_func0B(int param_1,int param_2);
+void ecsh_shrine_func0B(u8 idx, f32 *out1, f32 *out2);
 void FUN_801c5f28(ushort *param_1);
 void FUN_801c61f4(undefined4 param_1,undefined4 param_2,int param_3);
 void FUN_801c6358(int param_1);
