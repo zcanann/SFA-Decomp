3b9e59defb015c7ce74d9acaff5554a7a623d74c
Zachary Canann <zacharycanann@gmail.com>
2026-05-20 10:57:24 -0700

    objects: name 28B 299 2A3 2A4 callbacks

--- src/main/unknown/autos/placeholder_80220608.c
+++ src/main/unknown/autos/placeholder_80220608.c
@@ -3,16 +3,16 @@
 /* Pattern wrappers. */
 int fn_80223BBC(void) { return 0x2; }
 int fn_80223D10(void) { return 0x2; }
-int fn_80223D18(void) { return 0xac4; }
-int fn_80223D20(void) { return 0x0; }
-void fn_80223DA4(void) {}
-void fn_80224014(void) {}
-int fn_80228C68(void) { return 0x2; }
-int fn_80228C70(void) { return 0x0; }
-void fn_80228CDC(void) {}
-void fn_80228CE0(void) {}
-void fn_80228ED4(void) {}
-void fn_80228ED8(void) {}
+int dll_28B_getExtraSize_ret_2756(void) { return 0xac4; }
+int dll_28B_func08_ret_0(void) { return 0x0; }
+void dll_28B_hitDetect_nop(void) {}
+void dll_28B_release_nop(void) {}
+int dll_299_getExtraSize_ret_2(void) { return 0x2; }
+int dll_299_func08_ret_0(void) { return 0x0; }
+void dll_299_render_nop(void) {}
+void dll_299_hitDetect_nop(void) {}
+void dll_299_release_nop(void) {}
+void dll_299_initialise_nop(void) {}
 int Dummy29E_getExtraSize(void) { return 0x0; }
 int Dummy29E_func08_ret_0(void) { return 0x0; }
 void Dummy29E_free(void) {}
@@ -22,13 +22,13 @@ void Dummy29E_update(void) {}
 void Dummy29E_init(void) {}
 void Dummy29E_release(void) {}
 void Dummy29E_initialise(void) {}
-int fn_80231074(void) { return 0xc; }
-int fn_8023107C(void) { return 0x0; }
-void fn_80231344(void) {}
-void fn_80231348(void) {}
-int fn_80231398(void) { return 0xc; }
-int fn_802313A0(void) { return 0x0; }
-void fn_802313A8(void) {}
-void fn_802313D0(void) {}
-void fn_802315E4(void) {}
-void fn_802315E8(void) {}
+int dll_2A3_getExtraSize_ret_12(void) { return 0xc; }
+int dll_2A3_func08_ret_0(void) { return 0x0; }
+void dll_2A3_release_nop(void) {}
+void dll_2A3_initialise_nop(void) {}
+int dll_2A4_getExtraSize_ret_12(void) { return 0xc; }
+int dll_2A4_func08_ret_0(void) { return 0x0; }
+void dll_2A4_free_nop(void) {}
+void dll_2A4_hitDetect_nop(void) {}
+void dll_2A4_release_nop(void) {}
+void dll_2A4_initialise_nop(void) {}
