eb03a6867de9ed8c91e5ae8f1a9e6fdd30ba43f8
Jack Price-Burns <jackpriceburns@outlook.com>
2026-05-21 03:23:15 +0200

    screenOverlay: InvisibleHitSwitch_update 71.8 -> 88.2% (peephole+scheduling off)

--- src/main/dll/screenOverlay.c
+++ src/main/dll/screenOverlay.c
@@ -227,6 +227,8 @@ int InvisibleHitSwitch_getExtraSize(void) { return 0xc; }
  * EN v1.0 Address: 0x8017A8F4
  * EN v1.0 Size: 556b
  */
+#pragma peephole off
+#pragma scheduling off
 void InvisibleHitSwitch_update(int obj)
 {
   int state2;
@@ -292,3 +294,5 @@ void InvisibleHitSwitch_update(int obj)
     }
   }
 }
+#pragma peephole reset
+#pragma scheduling reset
