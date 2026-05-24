54bea20f35a6c1aa54ca7fce59f324c5d7a3498a
Zachary Canann <zacharycanann@gmail.com>
2026-05-24 15:05:47 -0700

    Remove stale dimsnowball drift bodies

--- include/main/dll/DIM/DIMlavasmash.h
+++ include/main/dll/DIM/DIMlavasmash.h
@@ -9,8 +9,6 @@ void FUN_801b0ae8(undefined8 param_1,undefined8 param_2,undefined8 param_3,undef
                  undefined8 param_5,undefined8 param_6,undefined8 param_7,undefined8 param_8,
                  int param_9,int param_10);
 void dimlogfire_init(int obj,int def);
-void FUN_801b0c9c(uint param_1);
-void FUN_801b0fd4(int param_1,int param_2);
 int dimsnowball_getExtraSize(void);
 int dimsnowball_getObjectTypeId(void);
 void dimsnowball_free(void);
