ea10860f54c1778817dc9ed88b4a823a97ab4f3a
Zachary Canann <zacharycanann@gmail.com>
2026-05-24 13:15:42 -0700

    Propagate enemy mushroom update name

--- include/main/dll/NW/NWtricky.h
+++ include/main/dll/NW/NWtricky.h
@@ -3,7 +3,7 @@
 
 #include "ghidra_import.h"
 
-void FUN_801d1e24(undefined2 *param_1,undefined4 *param_2,int param_3);
+void enemymushroom_update(undefined2 *param_1,undefined4 *param_2,int param_3);
 void FUN_801d1fb8(int param_1);
 
 #endif /* MAIN_DLL_NW_NWTRICKY_H_ */
