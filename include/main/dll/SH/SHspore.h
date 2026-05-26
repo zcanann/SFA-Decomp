d1ec628cb2fc2ea66f1bf0b5a41a65b39ea0ab39
Zachary Canann <zacharycanann@gmail.com>
2026-05-25 20:09:50 -0700

    Share queen earthwalker callback types

--- include/main/dll/SH/SHspore.h
+++ include/main/dll/SH/SHspore.h
@@ -3,6 +3,11 @@
 
 #include "ghidra_import.h"
 
+typedef struct QueenEarthWalkerMapData {
+  u8 pad00[0x18];
+  s8 yawByte;
+} QueenEarthWalkerMapData;
+
 void FUN_801d4364(int param_1);
 void FUN_801d4524(undefined2 *param_1);
 undefined4 FUN_801d468c(short *param_1,undefined4 param_2,int param_3);
@@ -12,6 +17,6 @@ int sh_queenearthwalker_getExtraSize(void);
 void sh_queenearthwalker_update(void *obj);
 void queenFeedFn_801d44a4(void *obj, void *state);
 void openPortalFn_801d4364(void *obj, void *state);
-void sh_queenearthwalker_init(void *obj, void *param2);
+void sh_queenearthwalker_init(void *obj, QueenEarthWalkerMapData *mapData);
 
 #endif /* MAIN_DLL_SH_SHSPORE_H_ */
