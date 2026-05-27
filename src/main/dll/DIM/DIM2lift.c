5ac689aa1f62c588e2aa5de8f07184b4be613a23
Jack Price-Burns <jackpriceburns@outlook.com>
2026-05-27 03:04:24 +0200

    Recover DIM2lift: fn_801BACB8 (boss-seq float-band flags + dispatch, 97%)

--- src/main/dll/DIM/DIM2lift.c
+++ src/main/dll/DIM/DIM2lift.c
@@ -1349,3 +1349,40 @@ int fn_801BAB88(int obj, int param2, f32 arg)
 }
 #pragma peephole reset
 #pragma scheduling reset
+
+extern f32 lbl_803E4C14;
+extern f32 lbl_803E4C18;
+extern f32 lbl_803E4C1C;
+
+#pragma peephole off
+#pragma scheduling off
+int fn_801BACB8(int obj, int param2, f32 arg)
+{
+    f32 h;
+    f32 v;
+    if (*(s8 *)(param2 + 0x27a) != 0) {
+        *(f32 *)(param2 + 0x2a0) = lbl_803E4C14;
+        if (*(s8 *)(param2 + 0x27a) != 0) {
+            ObjAnim_SetCurrentMove((int *)obj, 0x11, lbl_803E4BD8, 0);
+            *(u8 *)(param2 + 0x346) = 0;
+        }
+        *(s16 *)(obj + 0xa2) = -1;
+        v = lbl_803E4BD8;
+        *(f32 *)(param2 + 0x280) = v;
+        *(f32 *)(param2 + 0x284) = v;
+    }
+    h = *(f32 *)(obj + 0x98);
+    if (h > lbl_803E4C18) {
+        gDIMbossSequenceFlags &= 0xffffffbf;
+    } else if (h > lbl_803E4C1C) {
+        gDIMbossSequenceFlags |= 0x40;
+    }
+    if (*(int *)(param2 + 0x314) & 1) {
+        gDIMbossSequenceFlags |= 0x10000;
+    }
+    (*(int (**)(int, int, int, int, void *))(*(int *)gPlayerInterface + 0x34))(obj, param2, 0, 3, lbl_80325AA0);
+    (*(int (**)(int, int, f32, int))(*(int *)gPlayerInterface + 0x30))(obj, param2, arg, 0xf0);
+    return 0;
+}
+#pragma peephole reset
+#pragma scheduling reset
