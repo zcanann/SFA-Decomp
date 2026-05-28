a5dfd1b935f30201785e4535f987877c68e013fa
Jack Price-Burns <jackpriceburns@outlook.com>
2026-05-28 14:51:14 +0000

    Recover dll_299 cluster (free/update/init all 100%) in placeholder_80220608

--- src/main/unknown/autos/placeholder_80220608.c
+++ src/main/unknown/autos/placeholder_80220608.c
@@ -4285,3 +4285,41 @@ void dll_28B_initialise(void)
 }
 #pragma scheduling on
 #pragma peephole on
+
+extern int *gModgfxInterface;
+extern void Resource_Release(int handle);
+extern int Resource_Acquire(int id, int p2);
+extern int lbl_803DDD80;
+
+#pragma peephole off
+#pragma scheduling off
+void dll_299_free(int obj)
+{
+    (*(void (**)(int))(*gExpgfxInterface + 0x18))(obj);
+    (*(void (**)(int))(*gModgfxInterface + 0x14))(obj);
+    Resource_Release(lbl_803DDD80);
+    lbl_803DDD80 = 0;
+}
+
+void dll_299_update(int obj)
+{
+    if (randomGetRange(0, 2) == 0) {
+        (*(void (**)(int, int, int, int, int, int))(*(int *)lbl_803DDD80 + 0x4))(obj, 1, 0, 4, -1, 0);
+    }
+    (*(void (**)(int, int, int, int, int, int))(*gPartfxInterface + 0x8))(obj, 0x547, 0, 4, -1, 0);
+    (*(void (**)(int, int, int, int, int, int))(*gPartfxInterface + 0x8))(obj, 0x547, 0, 4, -1, 0);
+    (*(void (**)(int, int, int, int, int, int))(*gPartfxInterface + 0x8))(obj, 0x547, 0, 4, -1, 0);
+}
+
+void dll_299_init(int obj, int setup)
+{
+    *(s16 *)*(int *)(obj + 0xb8) = *(s16 *)(setup + 0x1e);
+    *(u16 *)(obj + 0xb0) |= 0x2000;
+    lbl_803DDD80 = Resource_Acquire(0xa6, 1);
+    (*(void (**)(int, int, int, int, int, int))(*gPartfxInterface + 0x8))(obj, 0x545, 0, 0x802, -1, 0);
+    (*(void (**)(int, int, int, int, int, int))(*gPartfxInterface + 0x8))(obj, 0x545, 0, 0x802, -1, 0);
+    (*(void (**)(int, int, int, int, int, int))(*gPartfxInterface + 0x8))(obj, 0x545, 0, 0x802, -1, 0);
+    (*(void (**)(int, int, int, int, int, int))(*gPartfxInterface + 0x8))(obj, 0x546, 0, 0x802, -1, 0);
+}
+#pragma scheduling on
+#pragma peephole on
