f862d033856aa567e92418a585421b4142f93534
Jack Price-Burns <jackpriceburns@outlook.com>
2026-05-28 15:59:09 +0000

    Recover placeholder_80220608: drmusiccont_update 99.1% (GameBit edge-detect + FP timer + map dispatch; u32 cmplwi + FP CSE)

--- src/main/unknown/autos/placeholder_80220608.c
+++ src/main/unknown/autos/placeholder_80220608.c
@@ -4706,7 +4706,8 @@ void fn_80221E94(int obj, f32 *p2)
 }
 
 typedef struct DrMusicContFlags {
-    u8 pad8 : 2;
+    u8 b_state : 1;
+    u8 pad8_lo : 1;
     u8 b_e30 : 1;
     u8 b_e31 : 1;
     u8 b_e32 : 1;
@@ -4746,6 +4747,121 @@ void drmusiccont_init(int obj)
 }
 #pragma peephole on
 #pragma scheduling on
+
+extern void fn_80094378(int obj, f32 a, f32 b, f32 c);
+extern void getEnvfxActImmediately(int a, int b, int c, int d);
+extern void skyFn_80088e54(int a, f32 b);
+extern void SCGameBitLatch_Update(int state, int a, int b, int c, int d, int e);
+extern void SCGameBitLatch_UpdateInverted(int state, int a, int b, int c, int d, int e);
+extern f32 lbl_803E6BCC;
+extern f32 lbl_803E6BD0;
+extern f32 lbl_803E6BD4;
+extern f32 lbl_803E6BD8;
+extern f32 lbl_803E6BDC;
+extern f32 lbl_803E6BE0;
+extern f32 lbl_803E6BE4;
+extern f32 lbl_803E6BE8;
+
+#pragma peephole off
+#pragma scheduling off
+void drmusiccont_update(int obj)
+{
+    int state = *(int *)(obj + 0xb8);
+    DrMusicContFlags *f = (DrMusicContFlags *)(state + 0x8);
+    u32 a;
+    u32 b;
+    u32 c;
+    u32 d;
+
+    fn_80094378(obj, lbl_803E6BCC, lbl_803E6BD0, lbl_803E6BD4);
+    if (*(int *)(obj + 0xf4) == 0) {
+        if ((u32)GameBit_Get(0xe7b) == 0) {
+            getEnvfxActImmediately(obj, obj, 0x210, 0);
+            getEnvfxActImmediately(obj, obj, 0x20f, 0);
+            getEnvfxActImmediately(obj, obj, 0x212, 0);
+            getEnvfxActImmediately(obj, obj, 0x1ea, 0);
+            skyFn_80088e54(0, lbl_803E6BD8);
+            GameBit_Set(0xe7b, 1);
+        }
+        *(int *)(obj + 0xf4) = 1;
+    }
+
+    SCGameBitLatch_Update(state, 2, 0x1a7, 0x64b, 0xf0e, 0xe5);
+    SCGameBitLatch_UpdateInverted(state, 1, -1, -1, 0xe26, 0xb8);
+    SCGameBitLatch_Update(state, 4, -1, -1, 0xcbb, 0xc4);
+
+    a = (u8)GameBit_Get(0xe30);
+    b = (u8)GameBit_Get(0xe31);
+    c = (u8)GameBit_Get(0xe32);
+    d = (u8)GameBit_Get(0xe33);
+    if (f->b_e9c == 0 && a && b && c && d) {
+        f->b_e9c = 1;
+        GameBit_Set(0xe9c, 1);
+        Sfx_PlayFromObject(0, 0x7e);
+    } else if (a != f->b_e30 || b != f->b_e31 || c != f->b_e32 || d != f->b_e33) {
+        Sfx_PlayFromObject(0, 0x109);
+    }
+    f->b_e30 = a;
+    f->b_e31 = b;
+    f->b_e32 = c;
+    f->b_e33 = d;
+
+    a = (u8)GameBit_Get(0xe38);
+    b = (u8)GameBit_Get(0xe3c);
+    c = (u8)GameBit_Get(0xe3d);
+    d = (u8)GameBit_Get(0xe3e);
+    if (f->b_e39 == 0 && a && b && c && d) {
+        f->b_e39 = 1;
+        Sfx_PlayFromObject(0, 0x7e);
+    } else if (a != f->b_e38 || b != f->b_e3c || c != f->b_e3d || d != f->b_e3e) {
+        Sfx_PlayFromObject(0, 0x109);
+    }
+    f->b_e38 = a;
+    f->b_e3c = b;
+    f->b_e3d = c;
+    f->b_e3e = d;
+
+    a = (u8)GameBit_Get(0x9e0);
+    b = (u8)GameBit_Get(0x9e1);
+    c = (u8)GameBit_Get(0x9e2);
+    d = (u8)GameBit_Get(0x9e7);
+    if (!(a && b && c && d)) {
+        if (a != f->b_9e0 || b != f->b_9e1 || c != f->b_9e2 || d != f->b_9e7) {
+            *(f32 *)(state + 4) = lbl_803E6BDC;
+        }
+    }
+    {
+        f32 zero = lbl_803E6BD8;
+        if (*(f32 *)(state + 4) > zero) {
+            *(f32 *)(state + 4) = *(f32 *)(state + 4) - timeDelta;
+            if (*(f32 *)(state + 4) <= zero) {
+                Sfx_PlayFromObject(0, 0x4bd);
+            }
+        }
+    }
+    f->b_9e0 = a;
+    f->b_9e1 = b;
+    f->b_9e2 = c;
+    f->b_9e7 = d;
+
+    if (f->b_state != 0) {
+        if ((u32)GameBit_Get(0x9f0) == 0 || (u32)GameBit_Get(0x632) != 0) {
+            (*(void (**)(void))(*gMapEventInterface + 0x2c))();
+            f->b_state = 0;
+        }
+    } else {
+        if ((u32)GameBit_Get(0x9f0) != 0 && (u32)GameBit_Get(0x632) == 0) {
+            f32 vec[3];
+            vec[0] = lbl_803E6BE0;
+            vec[1] = lbl_803E6BE4;
+            vec[2] = lbl_803E6BE8;
+            (*(void (**)(f32 *, int, int, int))(*gMapEventInterface + 0x24))(vec, 0x7fff, 0, 0);
+            f->b_state = 1;
+        }
+    }
+}
+#pragma scheduling on
+#pragma peephole on
 
 typedef struct DrBarrelGrFlags {
     u8 bit80 : 1;
