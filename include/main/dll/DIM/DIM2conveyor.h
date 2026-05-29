9ee84c5e306858b344896cdc228ad3fa6c18e819
Zachary Canann <zacharycanann@gmail.com>
2026-05-29 15:42:04 -0700

    Name DIM object header prototypes

--- include/main/dll/DIM/DIM2conveyor.h
+++ include/main/dll/DIM/DIM2conveyor.h
@@ -7,20 +7,20 @@
 extern ObjectDescriptor gDIMBridgeCogMaiObjDescriptor;
 extern ObjectDescriptor12 gDIMDismountPointObjDescriptor;
 
-void FUN_801b3658(undefined2 *param_1,int param_2);
+void dimlavasmash_init(s16 *obj,s8 *def);
 void FUN_801b365c(undefined4 param_1,undefined4 param_2,uint param_3);
 uint FUN_801b376c(uint param_1,undefined4 param_2,int param_3);
 void FUN_801b3a28(int param_1,int p1,int p2,int p3,int p4,s8 visible);
 void FUN_dropped_dimbridgecogmai_release(int param_1);
 void FUN_801b3af0(undefined2 *param_1,int param_2);
 undefined4 FUN_801b3af4(int param_1,undefined4 param_2,int param_3);
-void FUN_dropped_dimdismountpoint_func08(int param_1);
+void FUN_dropped_dimdismountpoint_getObjectTypeId(int param_1);
 void FUN_801b3b7c(int param_1,int p1,int p2,int p3,int p4,s8 visible);
 void FUN_801b3ba4(int param_1);
 void FUN_801b3d1c(short *param_1,int param_2);
 
 int dimbridgecogmai_getExtraSize(void);
-int dimbridgecogmai_func08(void);
+int dimbridgecogmai_getObjectTypeId(void);
 void dimbridgecogmai_free(int obj);
 void dimbridgecogmai_render(int p1, int p2, int p3, int p4, int p5, s8 visible);
 void dimbridgecogmai_hitDetect(void);
