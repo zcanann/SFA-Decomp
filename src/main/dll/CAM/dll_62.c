a237aa25506b16648403201baab2ede6fb0cfce8
Zachary Canann <zacharycanann@gmail.com>
2026-05-29 21:27:24 -0700

    Recover climb camera float vectors

--- src/main/dll/CAM/dll_62.c
+++ src/main/dll/CAM/dll_62.c
@@ -5,10 +5,10 @@
 #pragma peephole off
 #pragma scheduling off
 extern void Obj_TransformWorldPointToLocal(f32 x,f32 y,f32 z,float *outX,float *outY,float *outZ,int obj);
-extern uint getAngle();
-extern undefined4 camcontrol_traceMove();
-extern f32 fn_80293E80();
-extern f32 sin();
+extern uint getAngle(f32 dx, f32 dz);
+extern void camcontrol_traceMove(f32 radius, f32 *from, void *to, f32 *out, void *work, int a, int b, int c);
+extern f32 fn_80293E80(f32 x);
+extern f32 sin(f32 x);
 
 extern u8 framesThisStep;
 extern undefined4* gCameraInterface;
@@ -43,17 +43,13 @@ void CameraModeClimb_update(short *param_1)
   uint uVar3;
   int iVar4;
   short *psVar5;
-  f64 dVar6;
+  f32 trigValue;
   f32 local_d8;
   f32 local_d4;
   f32 local_d0;
   f32 local_cc;
-  undefined4 local_c8;
-  undefined4 local_c4;
-  undefined4 local_c0;
-  f32 local_bc;
-  undefined4 local_b8;
-  f32 local_b4;
+  f32 traceFrom[3];
+  f32 traceOut[3];
   undefined auStack176 [112];
 
   psVar5 = *(short **)(param_1 + 0x52);
@@ -74,38 +70,36 @@ void CameraModeClimb_update(short *param_1)
   local_d0 = *(f32 *)(psVar5 + 0xe) + lbl_803DD578[4];
   fVar2 = *(f32 *)(psVar5 + 0xe) + lbl_803DD578[3];
   fVar1 = *(f32 *)(param_1 + 0xe);
-  if (fVar2 <= fVar1) {
-    if (fVar1 <= local_d0) {
-      local_d0 = lbl_803E19A0;
-    }
-    else {
-      local_d0 = local_d0 - fVar1;
-    }
+  if (fVar1 < fVar2) {
+    local_d0 = fVar2 - fVar1;
+  }
+  else if (fVar1 > local_d0) {
+    local_d0 = local_d0 - fVar1;
   }
   else {
-    local_d0 = fVar2 - fVar1;
+    local_d0 = lbl_803E19A0;
   }
   local_d0 = local_d0 * lbl_803DD578[2] * timeDelta;
   *(f32 *)(param_1 + 0xe) = *(f32 *)(param_1 + 0xe) + local_d0;
   local_d8 = (*lbl_803DD578 - lbl_803DD578[1]) * lbl_803E19A4 * timeDelta;
   lbl_803DD578[1] = lbl_803DD578[1] + local_d8;
-  dVar6 = (f64)fn_80293E80((f64)((lbl_803E19AC * (f32)(s32)*psVar5) / lbl_803E19B0));
-  local_bc = (f32)((f64)lbl_803E19A8 * dVar6 + (f64)*(f32 *)(psVar5 + 0xc));
-  local_b8 = *(undefined4 *)(psVar5 + 0xe);
-  dVar6 = (f64)sin((f64)((lbl_803E19AC * (f32)(s32)*psVar5) / lbl_803E19B0));
-  local_b4 = (f32)((f64)lbl_803E19A8 * dVar6 + (f64)*(f32 *)(psVar5 + 0x10));
-  dVar6 = (f64)fn_80293E80((f64)((lbl_803E19AC * (f32)(s32)*psVar5) / lbl_803E19B0));
-  *(f32 *)(param_1 + 0xc) = (f32)((f64)lbl_803DD578[1] * dVar6 + (f64)local_bc);
-  dVar6 = (f64)sin((f64)((lbl_803E19AC * (f32)(s32)*psVar5) / lbl_803E19B0));
-  *(f32 *)(param_1 + 0x10) = (f32)((f64)lbl_803DD578[1] * dVar6 + (f64)local_b4);
-  camcontrol_traceMove((f64)lbl_803E19B4,&local_bc,param_1 + 0xc,&local_c8,auStack176,3,1,1);
-  *(undefined4 *)(param_1 + 0xc) = local_c8;
-  *(undefined4 *)(param_1 + 0xe) = local_c4;
-  *(undefined4 *)(param_1 + 0x10) = local_c0;
+  trigValue = fn_80293E80((lbl_803E19AC * (f32)(s32)*psVar5) / lbl_803E19B0);
+  traceFrom[0] = lbl_803E19A8 * trigValue + *(f32 *)(psVar5 + 0xc);
+  traceFrom[1] = *(f32 *)(psVar5 + 0xe);
+  trigValue = sin((lbl_803E19AC * (f32)(s32)*psVar5) / lbl_803E19B0);
+  traceFrom[2] = lbl_803E19A8 * trigValue + *(f32 *)(psVar5 + 0x10);
+  trigValue = fn_80293E80((lbl_803E19AC * (f32)(s32)*psVar5) / lbl_803E19B0);
+  *(f32 *)(param_1 + 0xc) = lbl_803DD578[1] * trigValue + traceFrom[0];
+  trigValue = sin((lbl_803E19AC * (f32)(s32)*psVar5) / lbl_803E19B0);
+  *(f32 *)(param_1 + 0x10) = lbl_803DD578[1] * trigValue + traceFrom[2];
+  camcontrol_traceMove(lbl_803E19B4,traceFrom,param_1 + 0xc,traceOut,auStack176,3,1,1);
+  *(f32 *)(param_1 + 0xc) = traceOut[0];
+  *(f32 *)(param_1 + 0xe) = traceOut[1];
+  *(f32 *)(param_1 + 0x10) = traceOut[2];
   (**(code **)(*gCameraInterface + 0x38))
             ((f64)(f32)(u32)*(u16 *)(lbl_803DD578 + 0xc),param_1,
              &local_cc,&local_d0,&local_d4,&local_d8,0);
-  uVar3 = getAngle((f64)local_cc,(f64)local_d4);
+  uVar3 = getAngle(local_cc,local_d4);
   iVar4 = (0x8000 - (uVar3 & 0xffff)) - ((int)*param_1 & 0xffffU);
   if (0x8000 < iVar4) {
     iVar4 = iVar4 + -0xffff;
@@ -116,7 +110,7 @@ void CameraModeClimb_update(short *param_1)
   *param_1 = *param_1 + (short)iVar4;
   local_d0 = *(f32 *)(param_1 + 0xe) -
              (*(f32 *)(psVar5 + 0xe) + (f32)(u32)*(u16 *)(lbl_803DD578 + 0xc));
-  uVar3 = getAngle((f64)local_d0,(f64)local_d8);
+  uVar3 = getAngle(local_d0,local_d8);
   iVar4 = (uVar3 & 0xffff) - ((int)param_1[1] & 0xffffU);
   if (0x8000 < iVar4) {
     iVar4 = iVar4 + -0xffff;
@@ -128,8 +122,8 @@ void CameraModeClimb_update(short *param_1)
   param_1[1] = param_1[1] +
                ((short)((ulonglong)((longlong)iVar4 * 0x2aaaaaab) >> 0x20) -
                ((short)((short)(iVar4 / 0x60000) + (short)(iVar4 >> 0x1f)) >> 0xf));
-  Obj_TransformWorldPointToLocal((f64)*(f32 *)(param_1 + 0xc),(f64)*(f32 *)(param_1 + 0xe),
-               (f64)*(f32 *)(param_1 + 0x10),(f32 *)(param_1 + 6),(f32 *)(param_1 + 8),
+  Obj_TransformWorldPointToLocal(*(f32 *)(param_1 + 0xc),*(f32 *)(param_1 + 0xe),
+               *(f32 *)(param_1 + 0x10),(f32 *)(param_1 + 6),(f32 *)(param_1 + 8),
                (f32 *)(param_1 + 10),
                *(int *)(param_1 + 0x18));
 }
