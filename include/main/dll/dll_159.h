1297a0eb571ab41c14a7cfb2eef3e2473ccb83c0
Zachary Canann <zacharycanann@gmail.com>
2026-06-01 11:51:09 -0700

    Prune empty dll_159 split

--- include/main/dll/dll_159.h
+++ include/main/dll/dll_159.h
@@ -3,8 +3,4 @@
 
 #include "ghidra_import.h"
 
-void FUN_801833e4(undefined2 *param_1,int param_2);
-double FUN_80183544(int param_1);
-void FUN_801835c4(uint param_1,int param_2);
-
 #endif /* MAIN_DLL_DLL_159_H_ */
