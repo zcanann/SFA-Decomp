897da9e76609930c657fbdfb0aa0a4408cb77334
Zachary Canann <zacharycanann@gmail.com>
2026-06-01 21:18:25 -0700

    Name foodbag function prototypes

--- include/main/dll/foodbag.h
+++ include/main/dll/foodbag.h
@@ -3,29 +3,29 @@
 
 #include "ghidra_import.h"
 
-void FUN_800f472c(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
-void FUN_800f4c70(undefined4 param_1,undefined4 param_2,int param_3,uint param_4,undefined4 param_5,
+void dll_7C_func03(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
+void dll_7D_func03(undefined4 param_1,undefined4 param_2,int param_3,uint param_4,undefined4 param_5,
                  float *param_6);
-void FUN_800f4fa4(int param_1,int param_2,int param_3,uint param_4,undefined4 param_5,float *param_6
+void dll_7E_func03(int param_1,int param_2,int param_3,uint param_4,undefined4 param_5,float *param_6
                  );
-void FUN_800f52e0(int param_1,int param_2,int param_3,uint param_4);
-void FUN_800f57d8(int param_1,int param_2,int param_3,uint param_4);
-void FUN_800f5a8c(int param_1,int param_2,int param_3,uint param_4);
-void FUN_800f6150(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
-void FUN_800f6534(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
-void FUN_800f6988(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
-void FUN_800f6ddc(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
-void FUN_800f7434(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
-void FUN_800f77bc(int param_1,undefined2 param_2,int param_3,uint param_4);
-void FUN_800f7ac0(int param_1,undefined2 param_2,int param_3,uint param_4);
-void FUN_800f7d90(int param_1,undefined2 param_2,int param_3,uint param_4);
-void FUN_800f8094(int param_1,undefined2 param_2,int param_3,uint param_4);
-void FUN_800f8250(undefined4 param_1,undefined4 param_2,int param_3,uint param_4,undefined4 param_5,
+void dll_7F_func03(int param_1,int param_2,int param_3,uint param_4);
+void dll_80_func03(int param_1,int param_2,int param_3,uint param_4);
+void dll_81_func03(int param_1,int param_2,int param_3,uint param_4);
+void dll_82_func03(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
+void dll_83_func03(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
+void dll_84_func03(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
+void dll_85_func03(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
+void dll_86_func03(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
+void dll_87_func03(int param_1,int param_2,int param_3,uint param_4);
+void dll_88_func03(int param_1,int param_2,int param_3,uint param_4);
+void dll_89_func03(int param_1,int param_2,int param_3,uint param_4);
+void dll_8A_func03(int param_1,undefined2 param_2,int param_3,uint param_4);
+void dll_8B_func03(undefined4 param_1,undefined4 param_2,int param_3,uint param_4,undefined4 param_5,
                  float *param_6);
-void FUN_800f87e8(int param_1,undefined2 param_2,short *param_3,uint param_4);
-void FUN_800f8d68(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
-void FUN_800f977c(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
-void FUN_800f9e78(int param_1,undefined2 param_2,int param_3,uint param_4);
-void FUN_800fa16c(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
+void dll_8C_func03(int param_1,undefined2 param_2,short *param_3,uint param_4);
+void dll_8D_func03(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
+void dll_8E_func03(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
+void dll_8F_func03(int param_1,int param_2,int param_3,uint param_4);
+void dll_90_func03(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
 
 #endif /* MAIN_DLL_FOODBAG_H_ */
