4f906aaf08d52e3d24ea3f74de8510340648135b
Jack Price-Burns <jackpriceburns@outlook.com>
2026-06-03 17:07:29 +0000

    Recover wmwallcrawler: reconstruct fn_801F8008 to 100%

--- src/main/dll/WM/dll_0211_wmwallcrawler.c
+++ src/main/dll/WM/dll_0211_wmwallcrawler.c
@@ -43,3 +43,33 @@ void wmwallcrawler_render(int p1, int p2, int p3, int p4, int p5, s8 vis) {
 }
 #pragma scheduling reset
 #pragma peephole reset
+
+extern void mathFn_80021ac8(void* mtx, f32* vec);
+extern f32 lbl_803E5FB0;
+typedef struct { s16 r0, r1, r2; f32 m8, mc, m10, m14; } WcXf;
+
+#pragma peephole off
+#pragma scheduling off
+void fn_801F8008(int a, f32* b)
+{
+    WcXf mtx;
+    f32 in[3];
+    u16 ang, ang2;
+    in[0] = b[1];
+    in[1] = b[2];
+    in[2] = b[3];
+    mtx.mc = lbl_803E5FB0;
+    mtx.m10 = lbl_803E5FB0;
+    mtx.m14 = lbl_803E5FB0;
+    mtx.m8 = lbl_803E5FB4;
+    mtx.r2 = 0;
+    mtx.r1 = 0;
+    mtx.r0 = *(s16*)a;
+    mathFn_80021ac8(&mtx, in);
+    ang = getAngle(in[0], in[1]);
+    ang2 = getAngle(in[2], in[1]);
+    *(s16*)(a + 2) = (s16)ang2;
+    *(s16*)(a + 4) = (s16)ang;
+}
+#pragma scheduling reset
+#pragma peephole reset
