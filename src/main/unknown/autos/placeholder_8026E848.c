4e022f74b27a0204027bfa9a7ee786b98fc84b9f
Jack Price-Burns <jackpriceburns@outlook.com>
2026-06-03 23:40:39 +0000

    placeholder_8026E848: fn_8026E9D0 77%

--- src/main/unknown/autos/placeholder_8026E848.c
+++ src/main/unknown/autos/placeholder_8026E848.c
@@ -9,7 +9,10 @@ extern int gSynthCurrentVoice;
  * fn_8026E0E4 - large voice/MIDI dispatch (~1920 instructions). Stubbed.
  */
 #pragma dont_inline on
-void fn_8026E0E4(void) {}
+int fn_8026E0E4(int event, u8 voice, u32 *flag)
+{
+    return 0;
+}
 #pragma dont_inline reset
 
 /*
@@ -68,13 +71,76 @@ void fn_8026E90C(u8 voice)
     }
 }
 
-/*
- * fn_8026E9D0 - large 628-instr voice update with FP math. Stubbed.
- */
+extern int fn_8026CF78(u8 voice);
+
+extern void fn_8026E90C(u8 voice);
+extern f32 floorf(f32 x);
+extern f32 lbl_803E7780;
+extern f32 lbl_803E7784;
+extern f32 lbl_803E7788;
+
 #pragma dont_inline on
-int fn_8026E9D0(u8 voice, int param)
+#pragma fp_contract off
+int fn_8026E9D0(u8 voice, u32 param)
 {
-    (void)voice; (void)param;
-    return 0;
+    u8 *vp;
+    u8 *vp2;
+    u8 *event;
+    u32 v;
+    int res;
+    f32 k80;
+    f32 k84;
+    f32 k88;
+    f64 k88abs;
+    f32 ftotal;
+    f32 fm;
+    u32 flag;
+
+    flag = 0;
+    k88 = lbl_803E7788;
+    k88abs = __fabs(k88);
+    k80 = lbl_803E7780;
+    k84 = lbl_803E7784;
+    vp = (u8 *)(gSynthCurrentVoice + voice * 56 + 0x14e8);
+    while (((event = *(u8 **)(vp + 0x1c)) == NULL ? 0 : *(u32 *)(event + 8))
+           <= *(u32 *)(vp + *(u8 *)(vp + 0x30) * 8 + 0x24)) {
+        if (event != NULL) {
+            *(u8 **)(vp + 0x1c) = *(u8 **)event;
+            if (*(int *)event != 0) {
+                *(int *)(*(int *)(vp + 0x1c) + 4) = 0;
+            }
+        }
+        if (event != NULL) {
+            res = fn_8026E0E4((int)event, voice, &flag);
+            if (res != 0) {
+                synthInsertChannelEvent((int)vp, res);
+            }
+        } else {
+            if (flag == 0) {
+                return 0;
+            }
+            flag = 0;
+            *(u8 *)(vp + 0x30) ^= 1;
+            *(u32 *)(vp + *(u8 *)(vp + 0x30) * 8 + 0x24) = *(u32 *)(*(int *)(gSynthCurrentVoice + 0x118) + voice * 4 + 0x14);
+            *(u32 *)(vp + *(u8 *)(vp + 0x30) * 8 + 0x20) = *(u32 *)(vp + (*(u8 *)(vp + 0x30) ^ 1) * 8 + 0x20);
+            if (*(void **)(gSynthCurrentVoice + voice * 56 + 0x14e8) != NULL) {
+                *(int *)(gSynthCurrentVoice + voice * 56 + 0x14ec) = *(int *)(gSynthCurrentVoice + voice * 56 + 0x14e8);
+                fn_8026CF78(voice);
+                vp2 = (u8 *)(gSynthCurrentVoice + voice * 56 + 0x14e8);
+                fm = k80 * ((f32)*(u32 *)(vp2 + 8) * (f32)param) * (k84 * (f32)*(u16 *)(vp2 + 0x32));
+                ftotal = k88 * fm;
+                if (k88abs > __fabs(ftotal)) {
+                } else {
+                    ftotal -= k88 * (f32)(s64)(u64)(ftotal / k88);
+                }
+                *(u32 *)(vp2 + *(u8 *)(vp2 + 0x30) * 8 + 0xc) = (u32)ftotal;
+                *(u32 *)(vp2 + *(u8 *)(vp2 + 0x30) * 8 + 0x10) = (int)floorf(fm);
+            }
+            *(u16 *)(vp + 0x34) += 1;
+            fn_8026E90C(voice);
+        }
+    }
+    return 1;
 }
+#pragma fp_contract reset
 #pragma dont_inline reset
