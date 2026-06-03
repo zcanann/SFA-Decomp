7783a3e07531882704d54ad8bb2e61d513c9c843
Jack Price-Burns <jackpriceburns@outlook.com>
2026-06-03 22:13:03 +0000

    Recover platform1: platform1_control v1.0 rewrite (72.3%), unit 19.6->50.3%

--- include/main/dll/VF/platform1.h
+++ include/main/dll/VF/platform1.h
@@ -7,12 +7,12 @@
 typedef struct Platform1State {
   int linkedObject;
   int motionValue0;
-  int offsetVelocity;
+  f32 offsetVelocity;
   int savedPosXBits;
   int savedPosYBits;
   int savedPosZBits;
-  int playerSfxTimer;
-  int platformSfxTimer;
+  f32 playerSfxTimer;
+  f32 platformSfxTimer;
   int currentTrackOffset;
   int loopSfxHandle;
   int prevTrackOffset;
@@ -42,9 +42,6 @@ void platform1_controlUnsupported(void);
 void platform1_init(void);
 void platform1_release(void);
 void platform1_initialise(void);
-void platform1_control(undefined8 param_1,double param_2,double param_3,undefined8 param_4,
-                       undefined8 param_5,undefined8 param_6,undefined8 param_7,undefined8 param_8,
-                       undefined4 param_9,undefined4 param_10,int param_11,int param_12,
-                       int *param_13,undefined4 param_14,undefined4 param_15,int param_16);
+int platform1_control(int obj, int p2, u8 *data);
 
 #endif /* MAIN_DLL_VF_PLATFORM1_H_ */
