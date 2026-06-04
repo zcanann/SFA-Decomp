01fce9785989a5a1117929faf7c5b09c18dc3f6f
Jack Price-Burns <jackpriceburns@outlook.com>
2026-06-04 14:48:12 +0000

    Recover DBprotection: fn_801DFA28 30.4->97.7% v1.0 rewrite (unit 43.0->98.0%)

--- include/main/dll/DB/DBprotection.h
+++ include/main/dll/DB/DBprotection.h
@@ -3,7 +3,7 @@
 
 #include "ghidra_import.h"
 
-void fn_801DFA28(void);
+void fn_801DFA28(u8 *obj);
 void DBprotection_updateEnvfxGameBits(u8 *state);
 int DBprotection_getCameraState(int *obj);
 void DBprotection_updateShield(int *obj);
