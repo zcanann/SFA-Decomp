61045c9e4a409fa6dd155e75e596cc88f42792d7
Jack Price-Burns <jackpriceburns@outlook.com>
2026-06-04 07:33:40 +0000

    Recover placeholder_802736D4: MusyX streamHandle 75.4% + dataInsertKeymap 83.6% (31.5%->81.1%)

--- src/main/unknown/autos/placeholder_802736D4.c
+++ src/main/unknown/autos/placeholder_802736D4.c
@@ -47,8 +47,213 @@ extern f32 lbl_803E77D8;
  * PAL Address: TODO
  * PAL Size: TODO
  */
+typedef u32 (*SynthStreamUpdateFn)(u8* buffer, u32 length, u8* buffer2, u32 length2, u32 user);
+
+typedef struct SynthSampleInfo {
+    u32 info;
+    void* addr;
+    void* extraData;
+    u32 offset;
+    u32 length;
+    u32 loop;
+    u32 loopLength;
+    u8 compType;
+} SynthSampleInfo;
+
+extern u8 synthJobTableCountdown;
+extern u8 synthJobTablePeriod;
+extern u8* synthVoice;
+extern void DCInvalidateRange(void* addr, u32 size);
+extern void* hwFlushStream(u8 handle);                /* gets the stream play buffer */
+extern u32 hwChangeStudio(u32 voice);                 /* gets the stream playback position */
+extern void hwGetPos(u8* buffer, u32 offset, u32 length, u8 handle, u32 callback, u32 user); /* flushes stream data */
+extern void hwInitSamplePlayback(u32 voice, u32 keyInfo, SynthSampleInfo* sample, u32 a, s32 b, u32 voiceId, u32 c, u32 d);
+extern void hwSetPitch(u32 voice, s32 pitch);
+extern void hwStart(u32 voice, u8 studio);
+extern void hwSetStreamLoopPS(u32 voice, u32 ps);
+extern f32 lbl_803E77E8;
+
 void synthUpdateJobTable(void)
 {
+    u32 cpos;
+    u32 len;
+    u32 off;
+    SynthSampleInfo newsmp;
+    u32 i;
+    SynthJob* si;
+    f32 volScale;
+    f32 freqScale;
+
+    if (synthJobTableCountdown != 0) {
+        --synthJobTableCountdown;
+        return;
+    }
+    synthJobTableCountdown = synthJobTablePeriod;
+    freqScale = lbl_803E77E8;
+    volScale = lbl_803E77D8;
+    si = synthJobTable;
+    for (i = 0; i < lbl_803BD150[0x210]; ++i, ++si) {
+        switch (si->state) {
+        case 1:
+            newsmp.info = si->frq | 0x40000000;
+            newsmp.addr = hwFlushStream(si->streamHandle);
+            newsmp.offset = 0;
+            newsmp.length = si->size;
+            newsmp.loop = 0;
+            newsmp.loopLength = si->size;
+            si->adpcm.loopPS = si->adpcm.initialPS = *si->buffer;
+            DCInvalidateRange(si->buffer, 1);
+            switch (si->format) {
+            case 0:
+                newsmp.compType = 2;
+                break;
+            case 1:
+                newsmp.extraData = &si->adpcm;
+                newsmp.compType = 4;
+                break;
+            }
+            hwInitSamplePlayback(si->voice, 0xFFFF, &newsmp, 1, -1,
+                                 *(u32*)(synthVoice + si->voice * 0x404 + 0xF4), 1, 1);
+            hwSetPitch(si->voice, freqScale * ((f32)si->frq / (f32)*(u32*)lbl_803BD150));
+            hwSetVolume(si->voice, 0, volScale * si->volume, volScale * si->leftVolume,
+                        volScale * si->rightVolume, si->pan << 16, si->surroundPan << 16);
+            hwStart(si->voice, si->studio);
+            si->state = 2;
+            if (!(si->flags & 0x20000)) {
+                hwGetPos(si->buffer, 0, si->bytes, si->streamHandle, 0, 0);
+            }
+            break;
+        case 2:
+            cpos = hwChangeStudio(si->voice);
+
+            if (si->format == 1) {
+                cpos = (cpos / 14) * 14;
+            }
+
+            if (si->last != cpos) {
+                if (si->last < cpos) {
+                    if (si->format == 1) {
+                        off = (si->last / 14) * 8;
+                        if ((len = ((SynthStreamUpdateFn)si->callback)(si->buffer + off, cpos - si->last,
+                                                                       0, 0, si->callbackUser)) != 0 &&
+                            si->state == 2) {
+                            cpos = (si->last + len) % si->size;
+                            if (!(si->flags & 0x20000)) {
+                                if (cpos == 0) {
+                                    hwGetPos(si->buffer, off, si->bytes - off, si->streamHandle, 0, 0);
+                                } else {
+                                    hwGetPos(si->buffer, off, ((cpos + 13) / 14) * 8 - off,
+                                             si->streamHandle, 0, 0);
+                                }
+                            }
+                            si->last = cpos;
+                        }
+                    } else if (si->format == 0) {
+                        if ((len = ((SynthStreamUpdateFn)si->callback)(si->buffer + si->last * 2,
+                                                                       cpos - si->last, 0, 0,
+                                                                       si->callbackUser)) != 0 &&
+                            si->state == 2) {
+                            cpos = (si->last + len) % si->size;
+                            if (!(si->flags & 0x20000)) {
+                                if (cpos == 0) {
+                                    hwGetPos(si->buffer, si->last * 2, (si->size - si->last) * 2,
+                                             si->streamHandle, 0, 0);
+                                } else {
+                                    hwGetPos(si->buffer, si->last * 2, (cpos - si->last) * 2,
+                                             si->streamHandle, 0, 0);
+                                }
+                            }
+                            si->last = cpos;
+                        }
+                    }
+                } else if (cpos == 0) {
+                    if (si->format == 1) {
+                        off = (si->last / 14) * 8;
+                        if ((len = ((SynthStreamUpdateFn)si->callback)(si->buffer + off,
+                                                                       si->size - si->last, 0, 0,
+                                                                       si->callbackUser)) != 0 &&
+                            si->state == 2) {
+                            cpos = (si->last + len) % si->size;
+                            if (!(si->flags & 0x20000)) {
+                                if (cpos == 0) {
+                                    hwGetPos(si->buffer, off, si->bytes - off, si->streamHandle, 0, 0);
+                                } else {
+                                    hwGetPos(si->buffer, off, ((cpos + 13) / 14) * 8 - off,
+                                             si->streamHandle, 0, 0);
+                                }
+                            }
+                            si->last = cpos;
+                        }
+                    } else if (si->format == 0) {
+                        if ((len = ((SynthStreamUpdateFn)si->callback)(si->buffer + si->last * 2,
+                                                                       si->size - si->last, 0, 0,
+                                                                       si->callbackUser)) != 0 &&
+                            si->state == 2) {
+                            cpos = (si->last + len) % si->size;
+                            if (!(si->flags & 0x20000)) {
+                                if (cpos == 0) {
+                                    hwGetPos(si->buffer, si->last * 2, si->bytes - si->last * 2,
+                                             si->streamHandle, 0, 0);
+                                } else {
+                                    hwGetPos(si->buffer, si->last * 2, (cpos - si->last) * 2,
+                                             si->streamHandle, 0, 0);
+                                }
+                            }
+                            si->last = cpos;
+                        }
+                    }
+                } else {
+                    if (si->format == 1) {
+                        off = (si->last / 14) * 8;
+                        if ((len = ((SynthStreamUpdateFn)si->callback)(si->buffer + off,
+                                                                       si->size - si->last, si->buffer,
+                                                                       cpos, si->callbackUser)) != 0 &&
+                            si->state == 2) {
+                            cpos = (si->last + len) % si->size;
+                            if (!(si->flags & 0x20000)) {
+                                if (si->size - si->last < len) {
+                                    hwGetPos(si->buffer, off, si->bytes - off, si->streamHandle, 0, 0);
+                                    hwGetPos(si->buffer, 0, (cpos / 14) * 8, si->streamHandle, 0, 0);
+                                } else if (cpos == 0) {
+                                    hwGetPos(si->buffer, off, si->bytes - off, si->streamHandle, 0, 0);
+                                } else {
+                                    hwGetPos(si->buffer, off, ((cpos + 13) / 14) * 8 - off,
+                                             si->streamHandle, 0, 0);
+                                }
+                            }
+                            si->last = cpos;
+                        }
+                    } else if (si->format == 0) {
+                        if ((len = ((SynthStreamUpdateFn)si->callback)(si->buffer + si->last * 2,
+                                                                       si->size - si->last, si->buffer,
+                                                                       cpos, si->callbackUser)) != 0 &&
+                            si->state == 2) {
+                            cpos = (si->last + len) % si->size;
+                            if (!(si->flags & 0x20000)) {
+                                if (si->size - si->last < len) {
+                                    hwGetPos(si->buffer, si->last * 2, si->bytes - si->last * 2,
+                                             si->streamHandle, 0, 0);
+                                    hwGetPos(si->buffer, 0, cpos * 2, si->streamHandle, 0, 0);
+                                } else if (cpos == 0) {
+                                    hwGetPos(si->buffer, si->last * 2, si->bytes - si->last * 2,
+                                             si->streamHandle, 0, 0);
+                                } else {
+                                    hwGetPos(si->buffer, si->last * 2, (cpos - si->last) * 2,
+                                             si->streamHandle, 0, 0);
+                                }
+                            }
+                            si->last = cpos;
+                        }
+                    }
+                }
+
+                if (si->state == 2 && !(si->flags & 0x20000) && si->format == 1) {
+                    hwSetStreamLoopPS(si->voice, *(u32*)((u32)si->buffer + 0x40000000) >> 24);
+                }
+            }
+            break;
+        }
+    }
 }
 
 /* Pattern wrappers. */
@@ -105,88 +310,41 @@ void synthRefreshJobVolumes(void)
     sndEnd();
 }
 
-int dataInsertKeymap(u16 keymapId, void *data)
-{
-    DataRefEntry *entry;
-    u8 *tableBase;
-    u16 count;
-    u32 moveCount;
-    u32 batches;
-    u32 *move;
-    int index;
-    u16 key;
+#define DATA_KEYMAP_TAB ((DataRefEntry*)(dataSmpSDirTable + 0x4600))
 
-    tableBase = dataSmpSDirTable;
+int dataInsertKeymap(u16 cid, void *keymapdata)
+{
+    long i;
+    long j;
+
     sndBegin();
-    count = dataKeymapNum;
-    key = keymapId;
-    entry = (DataRefEntry *)(tableBase + 0x4600);
-    index = 0;
-    while (index < count && entry->key < key) {
-        entry++;
-        index++;
-    }
-    if (index < count) {
-        entry = (DataRefEntry *)(tableBase + 0x4600 + index * sizeof(DataRefEntry));
-        if (key == entry->key) {
-            entry->refCount++;
-            sndEnd();
-            return 0;
-        }
-        if (count >= 0x100) {
-            sndEnd();
-            return 0;
-        }
-        moveCount = count - index;
-        move = (u32 *)(tableBase + 0x4600 + (count - 1) * sizeof(DataRefEntry));
-        if (index <= (int)(count - 1)) {
-            batches = moveCount >> 3;
-            if (batches != 0) {
-                do {
-                    move[2] = move[0];
-                    move[3] = move[1];
-                    move[0] = move[-2];
-                    move[1] = move[-1];
-                    move[-2] = move[-4];
-                    move[-1] = move[-3];
-                    move[-4] = move[-6];
-                    move[-3] = move[-5];
-                    move[-6] = move[-8];
-                    move[-5] = move[-7];
-                    move[-8] = move[-10];
-                    move[-7] = move[-9];
-                    move[-10] = move[-12];
-                    move[-9] = move[-11];
-                    move[-12] = move[-14];
-                    move[-11] = move[-13];
-                    move -= 0x10;
-                    batches--;
-                } while (batches != 0);
-                moveCount &= 7;
-                if (moveCount == 0) {
-                    goto insert;
-                }
+    for (i = 0; i < dataKeymapNum && DATA_KEYMAP_TAB[i].key < cid; ++i)
+        ;
+    if (i < dataKeymapNum) {
+        if (cid != DATA_KEYMAP_TAB[i].key) {
+            if (dataKeymapNum < 256) {
+                for (j = dataKeymapNum - 1; j >= i; --j)
+                    DATA_KEYMAP_TAB[j + 1] = DATA_KEYMAP_TAB[j];
+                ++dataKeymapNum;
+            } else {
+                sndEnd();
+                return 0;
             }
-            do {
-                move[2] = move[0];
-                move[3] = move[1];
-                move -= 2;
-                moveCount--;
-            } while (moveCount != 0);
-        }
-insert:
-        dataKeymapNum++;
-    } else {
-        if (count >= 0x100) {
+        } else {
+            DATA_KEYMAP_TAB[i].refCount++;
             sndEnd();
             return 0;
         }
-        dataKeymapNum++;
+    } else if (dataKeymapNum < 256) {
+        ++dataKeymapNum;
+    } else {
+        sndEnd();
+        return 0;
     }
-    entry = (DataRefEntry *)(tableBase + 0x4600 + index * sizeof(DataRefEntry));
-    entry->key = keymapId;
-    entry->data = data;
-    entry->refCount = 1;
+
+    DATA_KEYMAP_TAB[i].key = cid;
+    DATA_KEYMAP_TAB[i].data = keymapdata;
+    DATA_KEYMAP_TAB[i].refCount = 1;
     sndEnd();
     return 1;
 }
