b2524f38cde3d7a5238bb9fed7c260a071c873ad
Jack Price-Burns <jackpriceburns@outlook.com>
2026-06-04 05:58:14 +0000

    Recover foodbag: dll_82_func03 17.2->97.9% from v1.0 asm

--- include/main/dll/foodbag.h
+++ include/main/dll/foodbag.h
@@ -11,7 +11,7 @@ void dll_7E_func03(int param_1,int param_2,int param_3,uint param_4,undefined4 p
 void dll_7F_func03(int param_1,int param_2,int param_3,uint param_4);
 void dll_80_func03(int param_1,int param_2,int param_3,uint param_4);
 void dll_81_func03(int param_1,int param_2,int param_3,uint param_4);
-void dll_82_func03(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
+void dll_82_func03(int param_1,int param_2,int param_3,uint param_4);
 void dll_83_func03(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
 void dll_84_func03(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
 void dll_85_func03(undefined4 param_1,undefined4 param_2,int param_3,uint param_4);
