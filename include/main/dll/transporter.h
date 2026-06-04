c3820d84ad85350f50b3f0e3550a9c85c5136b26
Zachary Canann <zacharycanann@gmail.com>
2026-06-03 18:24:07 -0700

    Resolve transporter hit detect duplicate

--- include/main/dll/transporter.h
+++ include/main/dll/transporter.h
@@ -18,7 +18,7 @@ void FUN_801757ac(undefined8 param_1,double param_2,undefined4 param_3,undefined
                  int param_5);
 void FUN_80175ed4(int param_1);
 void pushable_render(int param_1,int param_2,int param_3,int param_4,int param_5,s8 visible);
-void pushable_hitDetect(void);
+void pushable_hitDetect(int *obj);
 void FUN_801765c8(undefined8 param_1,undefined8 param_2,undefined8 param_3,undefined8 param_4,
                  undefined8 param_5,undefined8 param_6,undefined8 param_7,undefined8 param_8,
                  uint param_9);
