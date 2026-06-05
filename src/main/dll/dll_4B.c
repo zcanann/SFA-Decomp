f6d3ebd5abc86199d87c1809d469b01cfa9da99a
Zachary Canann <zacharycanann@gmail.com>
2026-06-04 21:40:09 -0700

    Remove stale options callback duplicates

--- src/main/dll/dll_4B.c
+++ src/main/dll/dll_4B.c
@@ -1,23 +1,2 @@
 #include "ghidra_import.h"
 #include "main/dll/dll_4B.h"
-
-extern undefined4* DAT_803dd720;
-
-/*
- * --INFO--
- *
- * Function: FUN_8011bfc8
- * EN v1.0 Address: 0x8011BFC8
- * EN v1.0 Size: 52b
- * EN v1.1 Address: 0x8011C150
- * EN v1.1 Size: 48b
- * JP Address: TODO
- * JP Size: TODO
- * PAL Address: TODO
- * PAL Size: TODO
- */
-void FUN_8011bfc8(void)
-{
-  (**(code **)(*DAT_803dd720 + 8))();
-  return;
-}
