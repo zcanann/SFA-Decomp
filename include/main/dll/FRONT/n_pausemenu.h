fae4e4c8f3f8f21d8cd536f5ecbfc7d72dea832d
Zachary Canann <zacharycanann@gmail.com>
2026-06-04 21:43:06 -0700

    Remove stale pause menu overlay bodies

--- include/main/dll/FRONT/n_pausemenu.h
+++ include/main/dll/FRONT/n_pausemenu.h
@@ -3,8 +3,4 @@
 
 #include "ghidra_import.h"
 
-void FUN_8011846c(void);
-undefined4 FUN_801184b8(undefined4 param_1,int *param_2,int param_3);
-undefined4 FUN_80118540(void);
-
 #endif /* MAIN_DLL_FRONT_N_PAUSEMENU_H_ */
