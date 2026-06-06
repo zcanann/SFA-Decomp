b883e560545e1921d98a99a8457a60dbe890ac28
Zachary Canann <zacharycanann@gmail.com>
2026-06-06 15:43:00 -0700

    Trim stale KaldaChom drift functions

--- include/main/dll/texscroll2.h
+++ include/main/dll/texscroll2.h
@@ -1,41 +1,12 @@
 #ifndef MAIN_DLL_DLL_134_H_
 #define MAIN_DLL_DLL_134_H_
 
-#include "ghidra_import.h"
-
-undefined4 FUN_80167d10(undefined4 param_1,int param_2);
 int kaldachom_stateHandlerA06(int obj, int state);
-undefined4 FUN_80167d58(int param_1,int param_2);
 int kaldachom_stateHandlerA05(int obj, int state);
-undefined4
-FUN_80167db0(undefined8 param_1,undefined8 param_2,undefined8 param_3,undefined8 param_4,
-            undefined8 param_5,undefined8 param_6,undefined8 param_7,undefined8 param_8,int param_9,
-            int param_10);
 int kaldachom_stateHandlerA04(int obj, int state);
-undefined4 FUN_80167ec8(undefined4 param_1,int param_2);
 int kaldachom_stateHandlerA03(int obj, int state);
-undefined4
-FUN_80167f3c(undefined8 param_1,double param_2,double param_3,undefined8 param_4,undefined8 param_5,
-            undefined8 param_6,undefined8 param_7,undefined8 param_8,uint param_9,int param_10,
-            undefined4 param_11,undefined4 param_12,undefined4 param_13,undefined4 param_14,
-            undefined4 param_15,undefined4 param_16);
 int kaldachom_stateHandlerA02(int obj, int state);
 int kaldachom_stateHandlerA01(int obj, int state);
 int kaldachom_stateHandlerA00(int obj, int state);
-undefined4
-FUN_8016814c(undefined8 param_1,double param_2,double param_3,undefined8 param_4,undefined8 param_5,
-            undefined8 param_6,undefined8 param_7,undefined8 param_8,short *param_9,int param_10,
-            undefined4 param_11,undefined4 param_12,undefined4 param_13,undefined4 param_14,
-            undefined4 param_15,undefined4 param_16);
-undefined4
-FUN_8016825c(undefined8 param_1,double param_2,double param_3,undefined8 param_4,undefined8 param_5,
-            undefined8 param_6,undefined8 param_7,undefined8 param_8,int param_9,int param_10,
-            undefined4 param_11,undefined4 param_12,undefined4 param_13,undefined4 param_14,
-            undefined4 param_15,undefined4 param_16);
-undefined4
-FUN_80168368(undefined8 param_1,double param_2,double param_3,undefined8 param_4,undefined8 param_5,
-            undefined8 param_6,undefined8 param_7,undefined8 param_8,uint param_9,int param_10,
-            undefined4 param_11,undefined4 param_12,undefined4 param_13,undefined4 param_14,
-            undefined4 param_15,undefined4 param_16);
 
 #endif /* MAIN_DLL_DLL_134_H_ */
