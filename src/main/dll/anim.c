be79133dffe138748f7815614422745df3323feb
Zachary Canann <zacharycanann@gmail.com>
2026-06-08 12:24:28 -0700

    Remove final raw effect interface alias

--- src/main/dll/anim.c
+++ src/main/dll/anim.c
@@ -330,7 +330,6 @@ extern undefined4 DAT_803dcde8;
 extern undefined4 DAT_803dcdea;
 extern undefined4 DAT_803dcdeb;
 extern ObjectTriggerInterface **gObjectTriggerInterface;
-extern undefined4* DAT_803dd6f8;
 extern ModgfxInterface **gModgfxInterface;
 extern EffectInterface **gPartfxInterface;
 extern undefined4* DAT_803dd70c;
@@ -3352,17 +3351,10 @@ void FUN_80204078(undefined8 param_1,double param_2,double param_3,undefined8 pa
  * PAL Address: TODO
  * PAL Size: TODO
  */
-void FUN_80204238(undefined8 param_1,double param_2,double param_3,undefined8 param_4,
-                 undefined8 param_5,undefined8 param_6,undefined8 param_7,undefined8 param_8,
-                 undefined4 param_9)
+void FUN_80204238(int obj)
 {
-  undefined4 in_r9;
-  undefined4 in_r10;
-  undefined8 uVar1;
-  
-  uVar1 = (**(code **)(*DAT_803dd6f8 + 0x18))();
-  getLActions(uVar1,param_2,param_3,param_4,param_5,param_6,param_7,param_8,param_9,param_9,0,0,0,0
-               ,in_r9,in_r10);
+  (*gExpgfxInterface)->freeSource2((u32)obj);
+  getLActions(obj,obj,0,0,0,0);
   return;
 }
 
