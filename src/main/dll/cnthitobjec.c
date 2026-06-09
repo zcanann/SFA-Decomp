766c7f77a351a4577f66156d340b3fabd8cdc32d
Zachary Canann <zacharycanann@gmail.com>
2026-06-08 21:26:15 -0700

    Promote CNThitObjec setup state

--- src/main/dll/cnthitobjec.c
+++ src/main/dll/cnthitobjec.c
@@ -1,44 +1,8 @@
 #include "main/dll/dll_80220608_shared.h"
 #include "main/game_object.h"
 #include "main/dll/cnthitobjec_state.h"
-#include "main/obj_placement.h"
 
 #include "main/audio/sfx_ids.h"
-
-#define CNTHIT_MODE_VISIBLE_OBJECT 2
-#define CNTHIT_PROFILE_COUNT 3
-#define CNTHIT_DEFAULT_VISIBLE_EXPLOSION_SIZE 80
-
-#define CNTHIT_MODEL_NO_EXPLOSION_A 0x470EA
-#define CNTHIT_MODEL_NO_EXPLOSION_B 0x480F5
-#define CNTHIT_MODEL_NO_EXPLOSION_C 0x46710
-#define CNTHIT_MODEL_NO_EXPLOSION_D 0x49B43
-
-typedef struct CntHitObjectSetup {
-    ObjPlacement base;
-    s8 hitSourceProfile;
-    u8 mode;
-    s16 startHealth;
-    s16 explosionSize;
-    s16 doneGameBit;
-    s16 startGameBit;
-} CntHitObjectSetup;
-
-typedef struct CntHitObjectAnimEvent {
-    u8 pad0[0x81];
-    u8 explosionIds[10];
-    u8 explosionCount;
-} CntHitObjectAnimEvent;
-
-STATIC_ASSERT(offsetof(CntHitObjectSetup, hitSourceProfile) == 0x18);
-STATIC_ASSERT(offsetof(CntHitObjectSetup, mode) == 0x19);
-STATIC_ASSERT(offsetof(CntHitObjectSetup, startHealth) == 0x1A);
-STATIC_ASSERT(offsetof(CntHitObjectSetup, explosionSize) == 0x1C);
-STATIC_ASSERT(offsetof(CntHitObjectSetup, doneGameBit) == 0x1E);
-STATIC_ASSERT(offsetof(CntHitObjectSetup, startGameBit) == 0x20);
-STATIC_ASSERT(sizeof(CntHitObjectSetup) == 0x24);
-STATIC_ASSERT(offsetof(CntHitObjectAnimEvent, explosionIds) == 0x81);
-STATIC_ASSERT(offsetof(CntHitObjectAnimEvent, explosionCount) == 0x8B);
 
 int cnthitobjec_getExtraSize(void) { return 0xc; }
 
