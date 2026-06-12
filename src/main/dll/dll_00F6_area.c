47e9f7d83d0b80edc5b79107a934d4f78e94a6fa
Jack Price-Burns <jackpriceburns@outlook.com>
2026-06-12 10:50:54 +0000

    decl_trim: strip donor decls from tFrameAnimator.c carve outputs

--- src/main/dll/dll_00F6_area.c
+++ src/main/dll/dll_00F6_area.c
@@ -2,22 +2,8 @@
 #include "main/game_object.h"
 #include "main/dll/tframeanimator_state.h"
 
-typedef struct LevelnameState
-{
-    u8 pad0[0x8 - 0x0];
-    s32 unk8;
-    u8 padC[0xE - 0xC];
-    s16 unkE;
-    s16 unk10;
-    s16 unk12;
-    u8 pad14[0x18 - 0x14];
-} LevelnameState;
 
 
-extern int* Obj_GetPlayerObject(void);
-extern void GameBit_Set(int gameBit, int value);
-extern u32 GameBit_Get(int gameBit);
-extern int* gameTextGet(int textId);
 
 
 
@@ -75,34 +61,17 @@ void area_initialise(void)
 /* Trivial 4b 0-arg blr leaves. */
 void levelname_free(void);
 
-void levelname_render(void);
 
-void levelname_hitDetect(void);
 
-void levelname_release(void);
 
-void levelname_initialise(void);
 
-extern u8 framesThisStep;
-extern f32 Vec_distance(f32 * a, f32 * b);
-extern f32 mathSinf(f32 v);
-extern f32 lbl_803E36E0;
-extern f32 lbl_803E36E4;
-extern f32 lbl_803E36E8;
 
-void levelname_update(int* obj);
 
-void levelname_init(int obj, int objDef);
 
-void ProjectileSwitch_free(void);
 
 /* 8b "li r3, N; blr" returners. */
-int levelname_getExtraSize(void);
-int levelname_getObjectTypeId(void);
-int ProjectileSwitch_getExtraSize(void);
 
 
-int levelname_SeqFn(int obj, int unused, ObjAnimUpdateState* animUpdate);
 
 ObjectDescriptor gAreaObjDescriptor = {
     0, 0, 0, OBJECT_DESCRIPTOR_FLAGS_10_SLOTS,
