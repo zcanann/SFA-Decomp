0df6241baef07cefc61c669f756d8c58c0abb128
Zachary Canann <zacharycanann@gmail.com>
2026-06-12 17:40:15 -0700

    Share effect source parameter layout

--- include/main/dll/effectsrcparams_struct.h
+++ include/main/dll/effectsrcparams_struct.h
@@ -5,13 +5,50 @@
 
 typedef struct EffectSrcParams
 {
-  s16 rot0;
-  s16 rot1;
-  s16 rot2;
-  f32 w;
-  f32 x;
-  f32 y;
-  f32 z;
+  union
+  {
+    struct
+    {
+      s16 rot0;
+      s16 rot1;
+      s16 rot2;
+      s16 pad06;
+    };
+    struct
+    {
+      s16 rotX;
+      s16 rotY;
+      s16 rotZ;
+      s16 padRot;
+    };
+    struct
+    {
+      s16 arg0;
+      s16 arg1;
+      s16 arg2;
+      s16 arg3;
+    };
+  };
+  union
+  {
+    f32 w;
+    f32 scale;
+  };
+  union
+  {
+    f32 x;
+    f32 posX;
+  };
+  union
+  {
+    f32 y;
+    f32 posY;
+  };
+  union
+  {
+    f32 z;
+    f32 posZ;
+  };
 } EffectSrcParams;
 
 #endif
---
+++ include/main/effect_src_params.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_EFFECT_SRC_PARAMS_H_
+#define MAIN_EFFECT_SRC_PARAMS_H_
+
+#include "main/dll/effectsrcparams_struct.h"
+
+typedef EffectSrcParams PartFxSpawnArgs;
+
+#endif /* MAIN_EFFECT_SRC_PARAMS_H_ */
