52f3dc21fded9a3ba9e976fc9d4be0ec001e79d9
Zachary Canann <zacharycanann@gmail.com>
2026-06-12 20:16:30 -0700

    Remove duplicate vfpspellplace drift bodies

--- include/main/main.h
+++ include/main/main.h
@@ -22,8 +22,6 @@ void FUN_801fe080(int param_1);
 void FUN_801fe084(int param_1,int param_2);
 void FUN_801fe194(void);
 void FUN_801fe1c0(void);
-void FUN_801fe1c4(LaserObject *obj);
-void FUN_801fe324(LaserObject *obj,LaserObjectMapData *mapData);
 void FUN_801fe3b0(int param_1);
 void FUN_801fe540(short *param_1,undefined4 *param_2);
 undefined4 FUN_801fe750(double param_1,double param_2,int param_3,float *param_4,int param_5);
