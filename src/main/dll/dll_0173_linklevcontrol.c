6ae0d0bad859bb1f86b629be3149dd0f4725a8e0
Zachary Canann <zacharycanann@gmail.com>
2026-06-14 19:18:33 -0700

    Name linklevcontrol render temps

--- src/main/dll/dll_0173_linklevcontrol.c
+++ src/main/dll/dll_0173_linklevcontrol.c
@@ -170,57 +170,57 @@ void FUN_801ae184(undefined4 param_1, undefined4 param_2, undefined4 param_3, un
 {
     extern undefined4 FUN_801adca0(); /* #57 */
     extern undefined4 ObjPath_GetPointWorldPosition(); /* #57 */
-    u8 uVar1;
-    bool bVar2;
-    undefined2* puVar3;
-    uint uVar4;
-    int iVar5;
-    undefined4 uVar6;
-    undefined2* puVar7;
-    undefined4* puVar8;
-    undefined8 uVar9;
+    u8 savedByte;
+    bool active;
+    undefined2* obj;
+    uint bit;
+    int status;
+    undefined4 flag;
+    undefined2* subObj;
+    undefined4* data;
+    undefined8 ret;
 
-    uVar9 = FUN_80286830();
-    puVar3 = (undefined2*)((ulonglong)uVar9 >> 0x20);
-    if (puVar3[0x23] == 0x373)
+    ret = FUN_80286830();
+    obj = (undefined2*)((ulonglong)ret >> 0x20);
+    if (obj[0x23] == 0x373)
     {
-        FUN_8003b818((int)puVar3);
+        FUN_8003b818((int)obj);
     }
     else
     {
-        uVar4 = GameBit_Get(0x6e);
-        if ((uVar4 == 0) || (uVar4 = GameBit_Get(0x382), uVar4 != 0))
+        bit = GameBit_Get(0x6e);
+        if ((bit == 0) || (bit = GameBit_Get(0x382), bit != 0))
         {
-            puVar8 = *(undefined4**)(puVar3 + 0x5c);
-            puVar7 = (undefined2*)*puVar8;
-            bVar2 = false;
-            if ((puVar7 != (undefined2*)0x0) &&
-                (iVar5 = (**(code**)(**(int**)(puVar7 + 0x34) + 0x38))(puVar7), iVar5 == 2))
+            data = *(undefined4**)(obj + 0x5c);
+            subObj = (undefined2*)*data;
+            active = false;
+            if ((subObj != (undefined2*)0x0) &&
+                (status = (**(code**)(**(int**)(subObj + 0x34) + 0x38))(subObj), status == 2))
             {
-                bVar2 = true;
+                active = true;
             }
-            if (bVar2)
+            if (active)
             {
-                puVar3[3] = puVar3[3] | 8;
-                uVar6 = FUN_80057690((int)puVar7);
-                param_6 = (char)uVar6;
-                FUN_801adca0(puVar3, puVar7, (int)uVar9, param_3, param_4, param_5, param_6,
-                             (uint) * (byte*)(puVar8 + 8), 1);
+                obj[3] = obj[3] | 8;
+                flag = FUN_80057690((int)subObj);
+                param_6 = (char)flag;
+                FUN_801adca0(obj, subObj, (int)ret, param_3, param_4, param_5, param_6,
+                             (uint) * (byte*)(data + 8), 1);
             }
             else
             {
-                puVar3[3] = puVar3[3] & ~0x8;
+                obj[3] = obj[3] & ~0x8;
             }
-            if ((param_6 != '\0') && (*(char*)(puVar8 + 8) != '\0'))
+            if ((param_6 != '\0') && (*(char*)(data + 8) != '\0'))
             {
-                uVar1 = *(u8*)((int)puVar3 + 0x37);
-                if (bVar2)
+                savedByte = *(u8*)((int)obj + 0x37);
+                if (active)
                 {
-                    *(char*)((int)puVar3 + 0x37) = *(char*)(puVar8 + 8);
+                    *(char*)((int)obj + 0x37) = *(char*)(data + 8);
                 }
-                FUN_8003b818((int)puVar3);
-                ObjPath_GetPointWorldPosition(puVar3, 1, (float*)(puVar8 + 5), puVar8 + 6, (float*)(puVar8 + 7), 0);
-                *(u8*)((int)puVar3 + 0x37) = uVar1;
+                FUN_8003b818((int)obj);
+                ObjPath_GetPointWorldPosition(obj, 1, (float*)(data + 5), data + 6, (float*)(data + 7), 0);
+                *(u8*)((int)obj + 0x37) = savedByte;
             }
         }
     }
