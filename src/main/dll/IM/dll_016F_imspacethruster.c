e610628ea1f7f5e0b8932dddc8580ef35bb18f8f
Zachary Canann <zacharycanann@gmail.com>
2026-06-14 20:29:15 -0700

    Remove unused includes in IM/dll_016F_imspacethruster

--- src/main/dll/IM/dll_016F_imspacethruster.c
+++ src/main/dll/IM/dll_016F_imspacethruster.c
@@ -1,4 +1,3 @@
-#include "main/effect_interfaces.h"
 #include "main/dll/linklevcontrolstate_struct.h"
 #include "main/dll/lavaball1bfstate_struct.h"
 #include "main/dll/imspacethrusterstate_struct.h"
@@ -8,7 +7,6 @@
 #include "main/dll/magiclightstate_struct.h"
 #include "main/dll/crrockfall_types.h"
 #include "main/objtexture.h"
-#include "main/objseq.h"
 
 /*
  * Per-object extra state for the IM ice-mountain event controller
@@ -104,15 +102,8 @@ extern u8 framesThisStep;
  * height/distance, trigger the fall when the player is in range, integrate the
  * fall, then shatter (sfx + explosion) on impact. */
 
-#include "main/audio/sfx_ids.h"
-#include "main/obj_placement.h"
-#include "main/effect_interfaces.h"
-#include "main/dll_000A_expgfx.h"
 #include "main/game_object.h"
-#include "main/mapEvent.h"
 #include "main/dll/DIM/DIMcannon.h"
-#include "main/objanim_internal.h"
-#include "main/objseq.h"
 
 STATIC_ASSERT(sizeof(ImAnimSpacecraftState) == 0x4);
 
