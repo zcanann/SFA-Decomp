c0cbedbfab0006b80b524e83f7b6eb44524069b6
Zachary Canann <zacharycanann@gmail.com>
2026-06-14 20:54:59 -0700

    Remove unused includes in seqobj11d

--- src/main/dll/seqobj11d.c
+++ src/main/dll/seqobj11d.c
@@ -6,8 +6,6 @@
 #include "main/game_object.h"
 #include "main/model.h"
 #include "main/dll/baddie_state.h"
-#include "main/dll/seqObj11D.h"
-#include "main/objhits.h"
 #include "main/object_transform.h"
 #include "main/objseq.h"
 #include "main/dll/player_target.h"
