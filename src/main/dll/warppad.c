87163ac4a4d21e52e9ececaaa2444e42665644e1
Zachary Canann <zacharycanann@gmail.com>
2026-06-14 20:56:10 -0700

    Remove unused includes in warppad

--- src/main/dll/warppad.c
+++ src/main/dll/warppad.c
@@ -1,12 +1,8 @@
-#include "main/asset_load.h"
 #include "main/dll/CF/CFchuckobj.h"
-#include "main/dll/rom_curve_interface.h"
-#include "main/dll_000A_expgfx.h"
 #include "main/effect_interfaces.h"
 #include "main/game_object.h"
 #include "main/dll/CF/warp_pad.h"
 #include "main/objseq.h"
-#include "main/resource.h"
 
 typedef struct WarpPadPlayerStandingOnPlacement
 {
