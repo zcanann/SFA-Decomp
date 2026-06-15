6ba43cf2050cfd10ee25005b4e43cca11f183139
Zachary Canann <zacharycanann@gmail.com>
2026-06-14 19:56:30 -0700

    Remove unused externs in light

--- src/main/light.c
+++ src/main/light.c
@@ -21,8 +21,6 @@
  * this DLL live in the adjacent unit main/light.c (next .text range).
  */
 
-extern undefined4 DAT_803de940;
-extern f64 DOUBLE_803e6d90;
 extern f32 lbl_803E6100;
 extern f32 lbl_803E6144;
 extern f32 lbl_803E6148;
