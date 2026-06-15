c49d93b3062516d102bf2485f1b6242ee28ccc8d
Zachary Canann <zacharycanann@gmail.com>
2026-06-14 20:32:22 -0700

    Remove unused includes in SH/dll_01A6_shtricky

--- src/main/dll/SH/dll_01A6_shtricky.c
+++ src/main/dll/SH/dll_01A6_shtricky.c
@@ -1,9 +1,4 @@
-#include "main/audio/sfx_ids.h"
 #include "main/game_object.h"
-#include "main/mapEvent.h"
-#include "main/dll/flybaddie1D7.h"
-#include "main/dll/projball1D8.h"
-#include "main/objseq.h"
 
 extern uint GameBit_Get();
 extern undefined4 GameBit_Set();
