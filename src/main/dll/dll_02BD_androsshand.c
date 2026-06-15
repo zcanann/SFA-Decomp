4bcffcab0bbc2178a69b890fcb0b6a150f155bb6
Zachary Canann <zacharycanann@gmail.com>
2026-06-14 20:44:19 -0700

    Remove unused includes in dll_02BD_androsshand

--- src/main/dll/dll_02BD_androsshand.c
+++ src/main/dll/dll_02BD_androsshand.c
@@ -1,7 +1,5 @@
 #include "main/dll/dll_80220608_shared.h"
 #include "main/game_object.h"
-#include "main/obj_placement.h"
-#include "main/objhits.h"
 
 /*
  * Per-object extra state for an Andross hand
