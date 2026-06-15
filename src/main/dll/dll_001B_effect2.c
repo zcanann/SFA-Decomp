92d476a336dcfdcced8854dc203f6cc5d66eb9d4
Zachary Canann <zacharycanann@gmail.com>
2026-06-14 20:29:27 -0700

    Remove unused includes in dll_001B_effect2

--- src/main/dll/dll_001B_effect2.c
+++ src/main/dll/dll_001B_effect2.c
@@ -1,11 +1,9 @@
-#include "main/audio/sfx_ids.h"
 #include "main/dll/fxnode9_struct.h"
 #include "main/dll/partfxspawn_struct.h"
 #include "main/dll/modgfx_types.h"
 #include "main/dll_000A_expgfx.h"
 #include "main/game_object.h"
 #include "main/dll/modgfx.h"
-#include "main/resource.h"
 
 STATIC_ASSERT(offsetof(ModgfxState, vertexBuffers) == 0x78);
 STATIC_ASSERT(offsetof(ModgfxState, alphaChannels) == 0xAC);
