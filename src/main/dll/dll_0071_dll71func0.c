207dec59b1a2475967fd07a61a95a3923fb5280e
Zachary Canann <zacharycanann@gmail.com>
2026-06-14 19:47:32 -0700

    Remove unused externs in dll_0071_dll71func0

--- src/main/dll/dll_0071_dll71func0.c
+++ src/main/dll/dll_0071_dll71func0.c
@@ -118,7 +118,6 @@ extern undefined4 DAT_803de10c;
 extern undefined4* DAT_803de110;
 extern f32 lbl_803E1348;
 extern undefined4 uRam803de108;
-extern int maybeTryLoadSave(int a);
 extern u8 lbl_80314060[];
 extern f32 lbl_803E0B38;
 extern f32 lbl_803E0B3C;
