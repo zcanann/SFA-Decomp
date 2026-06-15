53f66264f56e52666dbb1470d5b3688682eb4a24
Zachary Canann <zacharycanann@gmail.com>
2026-06-14 19:49:44 -0700

    Remove unused externs in dll_00EA_sideload

--- src/main/dll/dll_00EA_sideload.c
+++ src/main/dll/dll_00EA_sideload.c
@@ -310,7 +310,6 @@ extern u8 Obj_IsLoadingLocked(void);
 extern u32 GameBit_Get(int eventId);
 extern void* Obj_AllocObjectSetup(int size, int type);
 extern int* Obj_SetupObject(void* setup, int a, int b, int c, void* d);
-extern f32 lbl_803E31D8;
 extern void gcbaddieshield_update(int* obj);
 extern void animatedobj_free();
 extern void animatedobj_render(int* obj, int p2, int p3, int p4, int p5, s8 visible);
@@ -346,7 +345,6 @@ extern void dll_F7_free();
 extern void dll_F7_render(int* obj, int p2, int p3, int p4, int p5, s8 visible);
 extern void dll_F7_update();
 extern void dll_F7_init();
-extern void selectTexture(void* tex, int x);
 
 void staticCamera_free(int param_1)
 {
