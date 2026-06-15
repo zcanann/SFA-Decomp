fd4e78124f0ff42bdd905fc7f0acf57a036f85ae
Zachary Canann <zacharycanann@gmail.com>
2026-06-14 20:36:30 -0700

    Remove unused includes in WM/dll_01F9_wmobjcreator

--- src/main/dll/WM/dll_01F9_wmobjcreator.c
+++ src/main/dll/WM/dll_01F9_wmobjcreator.c
@@ -9,11 +9,8 @@
  * spawnTimer from spawnPeriod + randomGetRange(0, spawnJitter); most
  * modes gate on the placement game bit (-1 = always).
  */
-#include "main/dll_000A_expgfx.h"
 #include "main/effect_interfaces.h"
-#include "main/dll/WC/dll_01F9_wmobjcreator.h"
 #include "main/dll/CF/CFchuckobj.h"
-#include "main/obj_placement.h"
 #include "main/game_object.h"
 
 extern uint GameBit_Get(int eventId);
