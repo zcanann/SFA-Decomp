c720136c828b5ecd069ce4ee961ffa353372115c
Zachary Canann <zacharycanann@gmail.com>
2026-06-14 20:40:40 -0700

    Remove unused includes in dll_00E7_flammablevine

--- src/main/dll/dll_00E7_flammablevine.c
+++ src/main/dll/dll_00E7_flammablevine.c
@@ -1,10 +1,6 @@
-#include "main/dll_000A_expgfx.h"
-#include "main/obj_placement.h"
 #include "main/game_object.h"
-#include "main/objanim_internal.h"
 #include "main/audio/sfx_ids.h"
 #include "main/mapEvent.h"
-#include "main/objseq.h"
 #include "main/dll/CF/CFBaby.h"
 
 typedef struct FlammablevineObjectDef
