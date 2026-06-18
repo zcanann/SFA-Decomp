4f0e2dfa414de949b75f2cd44a25d93b76f33a43
Zachary Canann <zacharycanann@gmail.com>
2026-06-18 16:13:50 -0700

    Improve skyFn_800895e0 97.6->100% (decl-order coloring)

--- src/main/sky.c
+++ src/main/sky.c
@@ -346,16 +346,16 @@ void fn_80089578(int flags, u8 red, u8 green, u8 blue)
 
 void skyFn_800895e0(int flags, u8 red, u8 green, u8 blue, u8 m1, u8 m2)
 {
-    int r1, g1, b1, r2, g2, b2;
     int base;
+    int r1, g1, b1, r2, g2, b2;
     int bit;
 
     if (lbl_803DD12C == NULL)
     {
         return;
     }
-    base = 0;
     bit = 0;
+    base = 0;
     r1 = red * m1 >> 8;
     g1 = green * m1 >> 8;
     b1 = blue * m1 >> 8;
