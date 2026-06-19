be5fcaea2ab24f78731808b85d36d7cfa0eaef5e
Zachary Canann <zacharycanann@gmail.com>
2026-06-19 13:01:28 -0700

    dll_0014_unk: Objfsa_FindRomCurveById loop guard hi>=lo (8 fns +%)

--- src/main/dll/dll_0014_unk.c
+++ src/main/dll/dll_0014_unk.c
@@ -319,7 +319,7 @@ static inline int Objfsa_FindRomCurveById(int curveId)
     lo = 0;
     hi = nRomCurves - 1;
     id = curveId;
-    while (lo <= hi)
+    while (hi >= lo)
     {
         mid = (hi + lo) >> 1;
         curve = (int)romCurves[mid];
