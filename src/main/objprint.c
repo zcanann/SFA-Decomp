ec615981eb75394c3248330b6f0eadfb75cf998c
Zachary Canann <zacharycanann@gmail.com>
2026-06-19 14:32:33 -0700

    Improve fn_8003B0D0 94.8->95.9% (bank deref form)

--- src/main/objprint.c
+++ src/main/objprint.c
@@ -2105,7 +2105,6 @@ void fn_8003B0D0(int obj, int p2, int p3, int p4)
     int n;
     int angle;
     s16 limit;
-    u8* bank;
 
     found = NULL;
     table = (void*)((GameObject*)obj)->anim.modelInstance;
@@ -2116,9 +2115,9 @@ void fn_8003B0D0(int obj, int p2, int p3, int p4)
         n = (s32)(u32)((ObjDef*)table)->jointCount;
         for (k = 0; k < n; k++)
         {
-            bank = *(u8**)((int)table + 0x10);
-            if ((int)bank[OBJPRINT_ACTIVE_BANK_INDEX(obj) + i + 1] != 0xff &&
-                (int)bank[i] == 0)
+            int bankI = *(int*)((int)table + 0x10);
+            if ((int)*(u8*)(bankI + OBJPRINT_ACTIVE_BANK_INDEX(obj) + i + 1) != 0xff &&
+                (int)*(u8*)(bankI + i) == 0)
             {
                 found = (s16*)((char*)((GameObject*)obj)->anim.jointPoseData + j);
             }
