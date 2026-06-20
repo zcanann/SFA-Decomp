ac8e2e2abe7832876c0498b2a8bf4dcccde01e91
Zachary Canann <zacharycanann@gmail.com>
2026-06-20 14:52:25 -0700

    Remove phantom lowercase header index duplicates (Windows case-collision)

--- include/main/dll/maybeTemplate.h
+++ include/main/dll/maybeTemplate.h
@@ -1,17 +1,18 @@
 #ifndef MAIN_DLL_MAYBETEMPLATE_H_
 #define MAIN_DLL_MAYBETEMPLATE_H_
 
-#include "ghidra_import.h"
+#include "main/audio/sfx.h"
+#include "main/dll/maybeTemplate.h"
+#include "main/camera_interface.h"
+#include "main/gamebits.h"
+#include "main/mapEventTypes.h"
+#include "main/screen_transition.h"
+#include "main/dll/player_status.h"
+#include "main/gameplay_runtime.h"
+#include "dolphin/gx/GXCull.h"
 
-void fn_80121C4C(u32 param_1,u32 param_2,u32 param_3);
-void fn_801225F4(u64 param_1,u64 param_2,u64 param_3,u64 param_4,
-                 u64 param_5,u64 param_6,u64 param_7,u64 param_8,
-                 u32 param_9,u32 param_10,short param_11,u32 param_12,u32 param_13,
-                 int *param_14,u32 param_15,u32 param_16);
-void fn_801228FC(void);
-void fn_8012310C(void);
-void hudDrawButtons(int param1, int param2, int param3);
-void fn_80124068(void);
-int fn_801241CC(short* arr, s8 flag);
+void pauseMenuDrawStatus(void);
+void cMenuUpdateAnims(void);
+void minimapFn_8012310c(void);
 
-#endif /* MAIN_DLL_MAYBETEMPLATE_H_ */
+#endif
--- include/main/dll/seqObj11D.h
+++ include/main/dll/seqObj11D.h
@@ -1,16 +1,19 @@
 #ifndef MAIN_DLL_SEQOBJ11D_H_
 #define MAIN_DLL_SEQOBJ11D_H_
 
-#include "ghidra_import.h"
+#include "main/audio/sfx_ids.h"
+#include "main/audio/sfx.h"
+#include "main/gamebits.h"
+#include "main/obj_placement.h"
+#include "main/game_ui_interface.h"
+#include "main/game_object.h"
+#include "main/model.h"
+#include "main/dll/baddie_state.h"
+#include "main/object_transform.h"
+#include "main/objseq.h"
+#include "main/dll/player_target.h"
+#include "main/dll/VF/vf_shared.h"
 
-void FUN_801511e8(u64 param_1,u64 param_2,double param_3,u64 param_4,
-                 u64 param_5,u64 param_6,u64 param_7,u64 param_8);
-void FUN_80151640(int param_1,int param_2);
-void FUN_80151844(u64 param_1,u64 param_2,double param_3,u64 param_4,
-                 u64 param_5,u64 param_6,u64 param_7,u64 param_8,
-                 short *param_9,int param_10);
-void FUN_80151b1c(u64 param_1,u64 param_2,double param_3,u64 param_4,
-                 u64 param_5,u64 param_6,u64 param_7,u64 param_8);
-void FUN_8015209c(int param_1,int param_2);
+void fn_80151954(int obj, u8* state);
 
-#endif /* MAIN_DLL_SEQOBJ11D_H_ */
+#endif
