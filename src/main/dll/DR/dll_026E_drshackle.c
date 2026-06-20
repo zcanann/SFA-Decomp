e28c52cb4a1af88c1dc598db158267e2578ec0d5
Zachary Canann <zacharycanann@gmail.com>
2026-06-20 00:10:39 -0700

    Improve drshackle_setScale 97.84->98.03 (swap p/q decl order, #108)

--- src/main/dll/DR/dll_026E_drshackle.c
+++ src/main/dll/DR/dll_026E_drshackle.c
@@ -75,8 +75,8 @@ void drshackle_initialise(void)
 
 int drshackle_setScale(int obj, int a, int b, int c, int d, int e, int f)
 {
-    u8* p = ((GameObject*)obj)->extra;
     int* q = *(int**)&((GameObject*)obj)->anim.placementData;
+    u8* p = ((GameObject*)obj)->extra;
     int* model;
     int* modelData;
     int joint1;
