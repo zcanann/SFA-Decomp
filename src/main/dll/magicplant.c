1223ff6f9e0ef3bf7f6f4b219ea8e7a61d9fff14
Zachary Canann <zacharycanann@gmail.com>
2026-06-20 05:28:06 -0700

    Improve fn_80153640 95.0->97.38% (inline trackedObj deref, #107)

--- src/main/dll/magicplant.c
+++ src/main/dll/magicplant.c
@@ -343,7 +343,6 @@ void fn_80153640(int obj, int state)
     u8* fx;
     int newObj;
     u32 rnd;
-    int p29c;
 
     if ((u8)Obj_IsLoadingLocked() != 0)
     {
@@ -358,18 +357,19 @@ void fn_80153640(int obj, int state)
         newObj = Obj_SetupObject((int)fx, 5, -1, -1, 0);
         if ((void*)newObj != NULL)
         {
-            p29c = *(int*)&((BaddieState*)state)->trackedObj;
-            ((GameObject*)newObj)->anim.velocityX = 0.02f * (((GameObject*)p29c)->anim.localPosX - ((GameObject*)
-                fx)->anim.rootMotionScale);
+            ((GameObject*)newObj)->anim.velocityX = 0.02f *
+                (((GameObject*)*(int*)&((BaddieState*)state)->trackedObj)->anim.localPosX -
+                 ((GameObject*)fx)->anim.rootMotionScale);
             rnd = randomGetRange(-10, 10);
             {
-                p29c = *(int*)&((BaddieState*)state)->trackedObj;
                 ((GameObject*)newObj)->anim.velocityY = 0.02f *
-                    ((lbl_803E28F0 + ((GameObject*)p29c)->anim.localPosY + (f32)(s32)rnd) -
+                    ((lbl_803E28F0 +
+                      ((GameObject*)*(int*)&((BaddieState*)state)->trackedObj)->anim.localPosY +
+                      (f32)(s32)rnd) -
                      ((GameObject*)fx)->anim.localPosX);
-                p29c = *(int*)&((BaddieState*)state)->trackedObj;
-                ((GameObject*)newObj)->anim.velocityZ = 0.02f * (((GameObject*)p29c)->anim.localPosZ -
-                    ((GameObject*)fx)->anim.localPosY);
+                ((GameObject*)newObj)->anim.velocityZ = 0.02f *
+                    (((GameObject*)*(int*)&((BaddieState*)state)->trackedObj)->anim.localPosZ -
+                     ((GameObject*)fx)->anim.localPosY);
             }
             *(int*)&((GameObject*)newObj)->ownerObj = obj;
         }
