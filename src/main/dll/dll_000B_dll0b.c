ae022c405ccfaa5318ff6b595e845aa697b6ff93
Jack Price-Burns <jackpriceburns@outlook.com>
2026-06-22 10:28:12 +0000

    Improve fn_800A02DC (#81 launder lbl_803DF438 flips f1/f2 const swap, 97.23->97.52)

--- src/main/dll/dll_000B_dll0b.c
+++ src/main/dll/dll_000B_dll0b.c
@@ -1580,7 +1580,7 @@ void fn_800A02DC(ModgfxState* state, f32* in)
     int j;
     ModgfxVertexData* slot;
 
-    dx = (s32)(lbl_803DF438 * (in[1] * gModgfxMotionStep));
+    dx = (s32)(*(f32*)&lbl_803DF438 * (in[1] * gModgfxMotionStep));
     dy = (s32)(lbl_803DF438 * (in[2] * gModgfxMotionStep));
 
     cur = state->vertexBuffers[state->activeVertexBufferIndex];
