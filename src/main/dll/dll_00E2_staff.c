264d406ae845e82f29f52cf05570f5aa837fe9ea
Zachary Canann <zacharycanann@gmail.com>
2026-07-03 22:36:33 -0700

    dll_00E2_staff.c: remove dead Ghidra extern decl lbl_803E3420 (byte-neutral)

--- src/main/dll/dll_00E2_staff.c
+++ src/main/dll/dll_00E2_staff.c
@@ -764,7 +764,6 @@ s16 staff_getHitReactValue(int* obj) { return ((StaffState*)(int*)((GameObject*)
 
 s32 staff_func16(int* obj) { return ((StaffState*)(int*)((GameObject*)obj)->extra)->swipeTextureIndex; }
 
-extern f32 lbl_803E3420;
 void objSetAnimField48to0(int* obj)
 {
     s32 v = 0x0;
