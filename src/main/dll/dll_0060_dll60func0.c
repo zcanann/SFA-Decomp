212ac743f8b45d7e11d0486b6ea5037f0591daee
Zachary Canann <zacharycanann@gmail.com>
2026-07-03 22:34:06 -0700

    dll_0060: drop 2 dead externs (lbl_803E1348, uRam803de108) - byte-neutral md5-verified

--- src/main/dll/dll_0060_dll60func0.c
+++ src/main/dll/dll_0060_dll60func0.c
@@ -48,8 +48,6 @@ extern u32 gGameplayPreviewColorBlue;
 extern u32 gGameplayRegisteredDebugOptions;
 extern u32* DAT_803dd6d0;
 extern u32* DAT_803dd6e8;
-extern f32 lbl_803E1348;
-extern u32 uRam803de108;
 extern f32 lbl_803E0830;
 extern f32 lbl_803E0834;
 extern f32 lbl_803E0838;
