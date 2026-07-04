292a660650262fb400cf4af0cace95ca38585176
Zachary Canann <zacharycanann@gmail.com>
2026-07-04 05:28:06 -0700

    lightmap.c: adopt OBJECT_OBJFLAG_* for objectFlags bits (byte-neutral)

--- src/main/lightmap.c
+++ src/main/lightmap.c
@@ -2291,7 +2291,7 @@ void getVisibleObjects(s8* opacity)
             att = *(u8**)(sub + 0xc8);
             if (att != NULL)
             {
-                *(u16*)(att + 0xb0) &= ~0x800;
+                *(u16*)(att + 0xb0) &= ~OBJECT_OBJFLAG_RENDERED;
             }
             sub += 4;
         }
