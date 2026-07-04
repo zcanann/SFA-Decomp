c7e1c79043bda2138c1438d520c2cf4f66b93d62
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-04 17:57:38 +0000

    data: attribute dll_022E_dfpdoorswitch .data descriptor table (0x80329968-0x803299d8)

--- src/main/dll/DF/dll_022E_dfpdoorswitch.c
+++ src/main/dll/DF/dll_022E_dfpdoorswitch.c
@@ -31,3 +31,26 @@ void doorswitch_update(void) { OSReport(sDoorswitchInitNoLongerSupported); }
 void doorswitch_init(void) { OSReport(sDoorswitchInitNoLongerSupported); }
 
 char sDoorswitchInitNoLongerSupported[] = "<doorswitch Init>No Longer supported \n";
+
+/* descriptor/ptr table auto 0x80329968-0x803299d8 */
+extern u8 DFP_Torch_free[];
+extern u8 DFP_Torch_getExtraSize[];
+extern u8 DFP_Torch_getObjectTypeId[];
+extern u8 DFP_Torch_hitDetect[];
+extern u8 DFP_Torch_init[];
+extern u8 DFP_Torch_initialise[];
+extern u8 DFP_Torch_release[];
+extern u8 DFP_Torch_render[];
+extern u8 DFP_Torch_update[];
+extern u8 dfpseqpoint_free[];
+extern u8 dfpseqpoint_getExtraSize[];
+extern u8 dfpseqpoint_getObjectTypeId[];
+extern u8 dfpseqpoint_hitDetect[];
+extern u8 dfpseqpoint_init[];
+extern u8 dfpseqpoint_initialise[];
+extern u8 dfpseqpoint_release[];
+extern u8 dfpseqpoint_render[];
+extern u8 dfpseqpoint_update[];
+
+u32 gDFP_seqpointObjDescriptor[14] = { 0x00000000, 0x00000000, 0x00000000, 0x00090000, (u32)dfpseqpoint_initialise, (u32)dfpseqpoint_release, 0x00000000, (u32)dfpseqpoint_init, (u32)dfpseqpoint_update, (u32)dfpseqpoint_hitDetect, (u32)dfpseqpoint_render, (u32)dfpseqpoint_free, (u32)dfpseqpoint_getObjectTypeId, (u32)dfpseqpoint_getExtraSize };
+u32 gDFP_TorchObjDescriptor[14] = { 0x00000000, 0x00000000, 0x00000000, 0x00090000, (u32)DFP_Torch_initialise, (u32)DFP_Torch_release, 0x00000000, (u32)DFP_Torch_init, (u32)DFP_Torch_update, (u32)DFP_Torch_hitDetect, (u32)DFP_Torch_render, (u32)DFP_Torch_free, (u32)DFP_Torch_getObjectTypeId, (u32)DFP_Torch_getExtraSize };
