d0c6b393f74535a134bd45d6356dae44bf50771c
Zachary Canann <zacharycanann@gmail.com>
2026-07-12 12:44:40 -0700

    dll_02AF_tree 74.36->100: dont_inline pragma around tree_updateAmbientEffects keeps tree_spawnAmbientEffect out-of-line (target bl's it)

--- src/main/dll/dll_02AF_tree.c
+++ src/main/dll/dll_02AF_tree.c
@@ -87,6 +87,7 @@ void tree_spawnAmbientEffect(GameObject* obj, TreeState* state, s8 index)
     }
 }
 
+#pragma dont_inline on
 void tree_updateAmbientEffects(GameObject* obj, TreeState* state)
 {
     int i;
@@ -123,6 +124,7 @@ void tree_updateAmbientEffects(GameObject* obj, TreeState* state)
         }
     }
 }
+#pragma dont_inline off
 
 void tree_render(GameObject* obj, int p2, int p3, int p4, int p5, s8 visible)
 {
