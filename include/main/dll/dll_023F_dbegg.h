1df62563d0f5b6008ab4e750d4b830a0471a8f0d
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-13 21:19:48 +0100

    Move dbegg internal layout to owner header

--- include/main/dll/dll_023F_dbegg.h
+++ include/main/dll/dll_023F_dbegg.h
@@ -6,6 +6,34 @@
 
 extern ObjectDescriptor12 gDB_eggObjDescriptor;
 
+typedef struct AnimBehaviorConfig
+{
+    u8 pad00[0x08];
+    f32 targetPosX;
+    f32 targetPosY;
+    f32 targetPosZ;
+    u8 pad14[0x19 - 0x14];
+    u8 forceRadiusByte;
+    u8 speedScaleByte;
+    u8 facingAngleByte;
+    s16 primaryConditionId;
+    s16 secondaryConditionId;
+    u8 pad20[0x24 - 0x20];
+    s16 readyConditionId;
+    u8 behaviorMode;
+    u8 pad27[0x2C - 0x27];
+    s16 activationEventId;
+} AnimBehaviorConfig;
+
+STATIC_ASSERT(offsetof(AnimBehaviorConfig, forceRadiusByte) == 0x19);
+STATIC_ASSERT(offsetof(AnimBehaviorConfig, speedScaleByte) == 0x1A);
+STATIC_ASSERT(offsetof(AnimBehaviorConfig, facingAngleByte) == 0x1B);
+STATIC_ASSERT(offsetof(AnimBehaviorConfig, primaryConditionId) == 0x1C);
+STATIC_ASSERT(offsetof(AnimBehaviorConfig, secondaryConditionId) == 0x1E);
+STATIC_ASSERT(offsetof(AnimBehaviorConfig, readyConditionId) == 0x24);
+STATIC_ASSERT(offsetof(AnimBehaviorConfig, behaviorMode) == 0x26);
+STATIC_ASSERT(offsetof(AnimBehaviorConfig, activationEventId) == 0x2C);
+
 int dbegg_setLaunchVelocity(GameObject* obj, f32* velocity);
 int dbegg_setScale(GameObject* obj);
 int dbegg_getExtraSize(void);
