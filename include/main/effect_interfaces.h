61e60510d1e9e66fbdce86562eb7b81bd1c38ac8
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-14 00:56:56 +0100

    Remove effect interface compatibility header

--- include/main/effect_interfaces.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_EFFECT_INTERFACES_H_
-#define MAIN_EFFECT_INTERFACES_H_
-
-#include "main/dll/modgfx_interface.h"
-#include "main/dll/partfx_interface.h"
-
-#endif /* MAIN_EFFECT_INTERFACES_H_ */
