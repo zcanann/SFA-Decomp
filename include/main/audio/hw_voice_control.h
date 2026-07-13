7ee95468a1cb948c1e94b5519204ccc9f895115e
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-13 12:27:26 +0100

    Remove duplicate audio API shells

--- include/main/audio/hw_voice_control.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_AUDIO_HW_VOICE_CONTROL_H_
-#define MAIN_AUDIO_HW_VOICE_CONTROL_H_
-
-#include "ghidra_import.h"
-
-void hwBreak(int slot);
-void hwKeyOff(int slot);
-
-#endif /* MAIN_AUDIO_HW_VOICE_CONTROL_H_ */
--- include/main/audio/vsample_events.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_AUDIO_VSAMPLE_EVENTS_H_
-#define MAIN_AUDIO_VSAMPLE_EVENTS_H_
-
-#include "ghidra_import.h"
-
-void synthHandleVirtualSampleDone(u32 packed);
-void synthAdvanceVirtualSampleEntry(void *entry, u32 elapsed);
-
-#endif /* MAIN_AUDIO_VSAMPLE_EVENTS_H_ */
