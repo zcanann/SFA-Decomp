053e35dcf4ece2e3df7a655435d81ebe6cecd3f0
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-13 23:44:45 +0100

    Remove orphaned light declarations

---
+++ include/main/light_internal.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_LIGHT_INTERNAL_H_
+#define MAIN_LIGHT_INTERNAL_H_
+
+typedef struct GameObject GameObject;
+
+void vfpdoorswitch_updateExplodingVariant(GameObject* obj);
+
+#endif /* MAIN_LIGHT_INTERNAL_H_ */
