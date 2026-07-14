0c340dddda40304d72f4f3c079a5655da89494e9
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-14 04:25:05 +0100

    Remove legacy positional sound redeclaration

--- include/main/audio/sfx_legacy.h
+++ include/main/audio/sfx_legacy.h
@@ -4,9 +4,9 @@
 #include "types.h"
 #include "main/audio/sfx_object_query_api.h"
 #include "main/audio/sfx_play_legacy_api.h"
+#include "main/audio/sfx_position_api.h"
 #include "main/audio/sfx_stop_object_api.h"
 
 void Sfx_KeepAliveLoopedObjectSound(int obj, int sfxId);
-void Sfx_PlayAtPositionFromObject(int obj, int sfxId, f32 x, f32 y, f32 z);
 
 #endif /* MAIN_AUDIO_SFX_LEGACY_H_ */
