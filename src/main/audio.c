ddce0a71e03764dd78f9b7e7cae8cb1abd600ded
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-14 04:34:54 +0100

    Use extended sound owner declaration in audio

--- src/main/audio.c
+++ src/main/audio.c
@@ -2225,8 +2225,7 @@ u32 Sfx_PlayFromObjectLimited(u32 obj, int sfxId, int limit)
     }
     if ((int)gSfxObjectChannelMatchCount < limit)
     {
-        extern void Sfx_PlayFromObjectEx(u32 obj, f32 * pos, u32 channel, int sfxId);
-        Sfx_PlayFromObjectEx(obj, NULL, 0, sfxId);
+        Sfx_PlayFromObjectExIntSfxLegacy(obj, NULL, 0, sfxId);
     }
     return gSfxObjectChannelMatchCount;
 }
