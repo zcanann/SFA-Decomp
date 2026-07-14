6ab3b7878c379c7ce2792887120ca5109d469ca3
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-14 03:21:42 +0100

    Add integer sound query legacy wrapper

--- include/main/audio/sfx_channel_query_api.h
+++ include/main/audio/sfx_channel_query_api.h
@@ -8,4 +8,7 @@ s32 Sfx_IsPlayingFromObjectChannel(u32 obj, u32 channel);
 #define Sfx_IsPlayingFromObjectChannelPtrLegacy(obj, channel)                                                  \
     ((s32 (*)(void*, int))Sfx_IsPlayingFromObjectChannel)((void*)(obj), (channel))
 
+#define Sfx_IsPlayingFromObjectChannelIntLegacy(obj, channel)                                                  \
+    ((int (*)(int, int))Sfx_IsPlayingFromObjectChannel)((obj), (channel))
+
 #endif /* MAIN_AUDIO_SFX_CHANNEL_QUERY_API_H_ */
