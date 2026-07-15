c87d5826438438fe9fcd0e2e84d48981490ee5ad
Zachary Canann <zacharycanann@gmail.com>
2026-07-14 21:19:17 -0700

    Recover AppleOnTree update local lifetimes

--- src/main/dll/dll_0117_appleontree.c
+++ src/main/dll/dll_0117_appleontree.c
@@ -618,13 +618,14 @@ void AppleOnTree_update(int objArg)
             if ((val != 0) || ((((AppleontreeObjectDef*)placement)->gameBit != -1 &&
                                 (bitVal = mainGetBit((int)((AppleontreeObjectDef*)placement)->gameBit), bitVal != 0))))
             {
+                int burstIndex;
                 state = *(int*)&((GameObject*)obj)->extra;
-                i = 0;
+                burstIndex = 0;
                 do
                 {
                     (*gPartfxInterface)->spawnObject((void*)obj, APPLEONTREE_PARTFX_BURST, NULL, 2, -1, NULL);
-                    i = i + 1;
-                } while (i < 8);
+                    burstIndex = burstIndex + 1;
+                } while (burstIndex < 8);
                 if (*(void**)((u8*)obj + 0x54) != 0)
                 {
                     ObjHits_DisableObject(obj);
@@ -712,16 +713,17 @@ void AppleOnTree_update(int objArg)
             }
             else
             {
+                f32 fallProgress;
                 val = *(int*)&((GameObject*)obj)->extra;
-                fb = -(*(float*)(val + 4) * *(float*)(val + 0x14) - *(float*)(val + 8)) /
-                     (*(float*)(val + 4) * (*(float*)(val + 0x18) - *(float*)(val + 0x14)));
+                fallProgress = -(*(float*)(val + 4) * *(float*)(val + 0x14) - *(float*)(val + 8)) /
+                               (*(float*)(val + 4) * (*(float*)(val + 0x18) - *(float*)(val + 0x14)));
                 fa = *(float*)(val + 8);
                 fc = fa * fa;
                 fc = fc * fc;
                 state = 0x100 - (int)((fc * fc) / *(float*)(val + 0x54));
                 modelIdxPtr = (int*)objFindTexture((GameObject*)obj, 0, 0);
                 *modelIdxPtr = state;
-                *(float*)(val + 0x24) = lbl_803E37D0 * fb + lbl_803E37CC;
+                *(float*)(val + 0x24) = lbl_803E37D0 * fallProgress + lbl_803E37CC;
                 ((GameObject*)obj)->anim.rootMotionScale =
                     *(float*)(*(int*)&((GameObject*)obj)->anim.modelInstance + 4) * *(float*)(val + 0x24);
                 Obj_SetActiveModelIndex((GameObject*)obj, 1);
@@ -759,7 +761,9 @@ void AppleOnTree_update(int objArg)
             }
             else
             {
-                val = placement = 0;
+                int iteration;
+                placement = 0;
+                iteration = 0;
                 fd = lbl_803E37D4;
                 while (placement == 0)
                 {
@@ -774,8 +778,8 @@ void AppleOnTree_update(int objArg)
                     {
                         placement = fn_8017DCD4((GameObject*)(obj), state, fc);
                     }
-                    val = val + 1;
-                    if (!((val == 100) || (val != 0x66)))
+                    iteration = iteration + 1;
+                    if (!((iteration == 100) || (iteration != 0x66)))
                         break;
                 }
                 if (lbl_803E37D4 != ((AppleOnTreeState*)state)->dropHeight)
