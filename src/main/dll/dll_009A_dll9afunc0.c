ae447c831010e5304513d69f2bc8126c5aa2bdef
Zachary Canann <zacharycanann@gmail.com>
2026-07-18 00:16:07 -0700

    Revert "Match dll_9A_func03 100% (opt_propagation off keeps cur pointer live in r31 for post-randomGetRange float stores instead of folding to frame base like our CSE build)"
    This reverts commit 4e5914193ffb400244be5dd784158d3c373b85ad.

--- src/main/dll/dll_009A_dll9afunc0.c
+++ src/main/dll/dll_009A_dll9afunc0.c
@@ -40,7 +40,6 @@ extern f32 lbl_803E138C;
 extern f32 lbl_803E1390;
 extern f32 lbl_803E1394;
 
-#pragma opt_propagation off
 void dll_9A_func03(int target, int variant, int parent, u32 flags)
 {
     ScreenSeq seq;
@@ -237,7 +236,6 @@ void dll_9A_func03(int target, int variant, int parent, u32 flags)
     }
     (*gModgfxInterface)->spawnEffect(&hdr, 0, 3, lbl_80317B98, 1, lbl_803DB958, DLL9A_EFFECT_ID, 0);
 }
-#pragma opt_propagation reset
 
 void dll_9A_func01_nop(void)
 {
