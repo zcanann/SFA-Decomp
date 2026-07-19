792a699c73f47af5c61476486457a778822c77fb
Zachary Canann <zacharycanann@gmail.com>
2026-07-19 04:15:14 -0700

    modelWalkAnimFn_800248b8: channel[j+0x60] index form + for-clause slot cursors (93.15->93.83, unit 99.4152->99.4546)

--- src/main/model.c
+++ src/main/model.c
@@ -549,19 +549,17 @@ void modelWalkAnimFn_800248b8(u8* dst, u8* model, u8* channel, f32 blend, int fl
             *(u32*)(stk + 0x24) = *(u32*)&((ObjAnimState*)channel)->blendMoveCache[0];
             *(u32*)(stk + 0x28) = *(u32*)&((ObjAnimState*)channel)->blendMoveCache[1];
             {
-                u8* dstFrameType = stk;
-                u8* srcSlotVals = channel;
-                u8* dstSlotVals = stk;
-                for (j = 0; j < slotCount; j++)
+                u8* dstFrameType;
+                u8* srcSlotVals;
+                u8* dstSlotVals;
+                for (j = 0, dstFrameType = stk, srcSlotVals = channel, dstSlotVals = stk; j < slotCount;
+                     dstFrameType += 1, srcSlotVals += 4, dstSlotVals += 4, j++)
                 {
                     *(u16*)(stk + j * 2 + 0x44) = *(u16*)(channel + j * 2 + 0x44);
-                    *(u8*)(dstFrameType + 0x60) = *(u8*)(channel + j + 0x60);
+                    *(u8*)(dstFrameType + 0x60) = channel[j + 0x60];
                     *(f32*)(dstSlotVals + 0x14) = *(f32*)(srcSlotVals + 0x14);
                     *(f32*)(dstSlotVals + 4) = *(f32*)(srcSlotVals + 4);
                     *(u32*)(dstSlotVals + 0x34) = *(u32*)(srcSlotVals + 0x34);
-                    dstFrameType += 1;
-                    srcSlotVals += 4;
-                    dstSlotVals += 4;
                 }
             }
             *(u16*)(stk + 0x58) = ((ObjAnimState*)channel)->eventCountdown;
