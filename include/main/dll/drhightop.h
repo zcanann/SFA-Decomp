52e8ab741a49b3ec075f6b6e1b094565a268d5b7
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-20 01:55:55 +0100

    Consolidate DR Hightop helper ownership

--- include/main/dll/drhightop.h
+++ include/main/dll/drhightop.h
@@ -1,8 +1,11 @@
 #ifndef H_MAIN_DLL_DRHIGHTOP_H
 #define H_MAIN_DLL_DRHIGHTOP_H
 
-#include "main/game_object.h"
+#include "main/dll/dll_0255_snowbike.h"
 
+void fn_801EAE4C(GameObject* obj, SnowBikeState* state);
+void fn_801EB0D4(u32 obj, int stateRaw);
+void fn_801EB634(GameObject* obj, int stateRaw);
 void fn_801EB940(short* obj, int stateRaw);
 void fn_801EBD60(GameObject* obj, int stateRaw);
 
