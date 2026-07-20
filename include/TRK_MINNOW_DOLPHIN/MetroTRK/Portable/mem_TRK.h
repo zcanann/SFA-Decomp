53b2c7eacafe9266bf72dbf63d774efabdd82b5a
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-20 09:21:28 +0100

    Use MetroTRK memory owner header

--- include/TRK_MINNOW_DOLPHIN/MetroTRK/Portable/mem_TRK.h
+++ include/TRK_MINNOW_DOLPHIN/MetroTRK/Portable/mem_TRK.h
@@ -1,6 +1,10 @@
 #ifndef METROTRK_PORTABLE_MEM_TRK_H
 #define METROTRK_PORTABLE_MEM_TRK_H
 
+#include <stddef.h>
 #include "dolphin/types.h"
+
+void TRK_fill_mem(void* dest, int value, unsigned long length);
+void* TRK_memset(void* dest, int value, size_t length);
 
 #endif /* METROTRK_PORTABLE_MEM_TRK_H */
