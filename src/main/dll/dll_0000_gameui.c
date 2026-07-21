3d2ffbeb089d3bb8d36c6ced80f1458a5967b1a3
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-21 18:43:01 +0100

    Match dll_0000_gameui.drawHudBox

--- src/main/dll/dll_0000_gameui.c
+++ src/main/dll/dll_0000_gameui.c
@@ -82,9 +82,7 @@
 #include "main/newshadows_shadow_api.h"
 #include "main/dll/hint_text_api.h"
 #include "main/shader_map_text_api.h"
-#define INTERSECT_HUD_ALPHA_U8
 #include "track/intersect_hud_api.h"
-#undef INTERSECT_HUD_ALPHA_U8
 #include "main/dll/dll_0011_screens.h"
 #include "main/dll/dll_8B.h"
 
