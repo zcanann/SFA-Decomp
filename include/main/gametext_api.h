52516b63f662bc6ed40822df93053502e2d0887d
Jack Price-Burns <jackpriceburns@outlook.com>
2026-08-12 09:57:12 +0100

    Recover NPC dialogue phrase state

--- include/main/gametext_api.h
+++ include/main/gametext_api.h
@@ -2,12 +2,22 @@
 #define MAIN_GAMETEXT_API_H_
 
 #include "types.h"
+#include "main/gametext_internal.h"
 
 typedef struct TextDisplayState TextDisplayState;
 
+typedef struct NpcDialoguePhraseState
+{
+    TextDisplayState display;
+    char* phraseBuffer;
+} NpcDialoguePhraseState;
+
+STATIC_ASSERT(sizeof(NpcDialoguePhraseState) == 0x18);
+STATIC_ASSERT(offsetof(NpcDialoguePhraseState, phraseBuffer) == 0x14);
+
 void gameTextAppendStr(char* str, int box);
-void gameTextQueueReveal(int textId, int arg);
-void gameTextFreePhrase(int* phrase);
+void gameTextQueueReveal(int textId, TextDisplayState* state);
+void gameTextFreePhrase(NpcDialoguePhraseState* phrase);
 void gameTextMeasureStringBoundsAt(char* str, int boxIdx, int x, int y, int* outMinX, int* outMaxX, int* outMinY,
                                    int* outMaxY);
 void gameTextMeasureById(int id, int x, int y, int* outMinX, int* outMaxX, int* outMinY, int* outMaxY);
