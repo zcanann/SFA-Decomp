55d95b97a1e845ad4e05867b36c3baba90f9396f
Zachary Canann <zacharycanann@gmail.com>
2026-08-21 16:58:37 -0700

    Recover signed track hit result types

--- include/main/track_hit_results.h
+++ include/main/track_hit_results.h
@@ -5,8 +5,7 @@
 #include "global.h"
 #include "game/objects/object.h"
 
-typedef struct TrackGroundHit
-{
+typedef struct TrackGroundHit {
     f32 height;
     f32 normalX;
     f32 normalY;
@@ -18,8 +17,7 @@ typedef struct TrackGroundHit
 
 STATIC_ASSERT(sizeof(TrackGroundHit) == 0x18);
 
-typedef struct TrackQueryBounds
-{
+typedef struct TrackQueryBounds {
     s32 minX;
     s32 minY;
     s32 minZ;
@@ -30,12 +28,11 @@ typedef struct TrackQueryBounds
 
 STATIC_ASSERT(sizeof(TrackQueryBounds) == 0x18);
 
-typedef struct TrackHitResults
-{
+typedef struct TrackHitResults {
     f32 planes[4][4];
     f32 radii[4];
-    u8 surfaceTypes[4];
-    u8 queryTypes[4];
+    s8 surfaceTypes[4];
+    s8 queryTypes[4];
     u8 triangleFlags[4];
     GameObject* objects[4];
     s16 hitCount;
