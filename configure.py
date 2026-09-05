3ed9122e00cd1a964dddd61bf88cd11d8c16e6b2
Jack Price-Burns <jackpriceburns@outlook.com>
2026-09-05 22:58:39 +0100

    Match clean collectible calls using GC 1.3

--- configure.py
+++ configure.py
@@ -1410,7 +1410,7 @@ config.libs = [
             Object(MatchingFor("GSAE01"), "dlls/objects/234_Sideload/Sideload.c"),
             Object(MatchingFor("GSAE01"), "dlls/objects/235/235.c"),
             Object(MatchingFor("GSAE01"), "dlls/objects/236_InfoPoint/InfoPoint.c"),
-            Object(MatchingFor("GSAE01"), "dlls/objects/237/237.c", cflags=cflags_dll_noopt, mw_version="GC/2.0"),
+            Object(MatchingFor("GSAE01"), "dlls/objects/237/237.c", cflags=cflags_dll_noopt),
             Object(MatchingFor("GSAE01"), "dlls/objects/238_EffectBox/EffectBox.c"),
             Object(MatchingFor("GSAE01"), "dlls/objects/239/239.c", cflags=cflags_dll_noopt),
             Object(MatchingFor("GSAE01"), "dlls/objects/240_WarpPoint/WarpPoint.c"),
