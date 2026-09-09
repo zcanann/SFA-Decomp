705ab25612ea9a0fb8a3819217349bfd8cf2e301
Zachary Canann <zacharycanann@gmail.com>
2026-09-09 14:53:49 -0700

    Recover SDK frustum source and natural matrix constant pool

--- src/dolphin/mtx/mtx44.c
+++ src/dolphin/mtx/mtx44.c
@@ -1,71 +1,79 @@
 #include <dolphin.h>
 #include "dolphin/math.h"
 
-const f32 lbl_803E7630 = 1.0f;
-const f32 lbl_803E7634 = 2.0f;
-const f32 lbl_803E7638 = 0.0f;
-const f32 lbl_803E763C = -1.0f;
-const f32 lbl_803E7640 = 0.5f;
-const f32 lbl_803E7644 = 0.017453292f;
-
-extern const f32 lbl_803E7630;
-extern const f32 lbl_803E7634;
-extern const f32 lbl_803E7638;
-extern const f32 lbl_803E763C;
-extern const f32 lbl_803E7640;
-extern const f32 lbl_803E7644;
+void C_MTXFrustum(Mtx44 m, f32 t, f32 b, f32 l, f32 r, f32 n, f32 f) {
+    f32 tmp = 1.0f / (r - l);
+    m[0][0] = (2 * n) * tmp;
+    m[0][1] = 0.0f;
+    m[0][2] = (r + l) * tmp;
+    m[0][3] = 0.0f;
+    tmp = 1.0f / (t - b);
+    m[1][0] = 0.0f;
+    m[1][1] = (2 * n) * tmp;
+    m[1][2] = (t + b) * tmp;
+    m[1][3] = 0.0f;
+    m[2][0] = 0.0f;
+    m[2][1] = 0.0f;
+    tmp = 1.0f / (f - n);
+    m[2][2] = -n * tmp;
+    m[2][3] = -(f * n) * tmp;
+    m[3][0] = 0.0f;
+    m[3][1] = 0.0f;
+    m[3][2] = -1.0f;
+    m[3][3] = 0.0f;
+}
 
 void C_MTXPerspective(Mtx44 m, f32 fovY, f32 aspect, f32 n, f32 f) {
     f32 angle;
     f32 cot;
     f32 tmp;
 
-    angle = *(f32*)&lbl_803E7640 * fovY;
-    angle = *(f32*)&lbl_803E7644 * angle;
-    cot = *(f32*)&lbl_803E7630 / tanf(angle);
+    angle = 0.5f * fovY;
+    angle = MTXDegToRad(angle);
+    cot = 1.0f / tanf(angle);
 
     m[0][0] = cot / aspect;
-    m[0][1] = *(f32*)&lbl_803E7638;
-    m[0][2] = *(f32*)&lbl_803E7638;
-    m[0][3] = *(f32*)&lbl_803E7638;
-    m[1][0] = *(f32*)&lbl_803E7638;
+    m[0][1] = 0.0f;
+    m[0][2] = 0.0f;
+    m[0][3] = 0.0f;
+    m[1][0] = 0.0f;
     m[1][1] = cot;
-    m[1][2] = *(f32*)&lbl_803E7638;
-    m[1][3] = *(f32*)&lbl_803E7638;
-    m[2][0] = *(f32*)&lbl_803E7638;
-    m[2][1] = *(f32*)&lbl_803E7638;
+    m[1][2] = 0.0f;
+    m[1][3] = 0.0f;
+    m[2][0] = 0.0f;
+    m[2][1] = 0.0f;
 
-    tmp = *(f32*)&lbl_803E7630 / (f - n);
+    tmp = 1.0f / (f - n);
     m[2][2] = -n * tmp;
     m[2][3] = tmp * -(f * n);
-    m[3][0] = *(f32*)&lbl_803E7638;
-    m[3][1] = *(f32*)&lbl_803E7638;
-    m[3][2] = *(f32*)&lbl_803E763C;
-    m[3][3] = *(f32*)&lbl_803E7638;
+    m[3][0] = 0.0f;
+    m[3][1] = 0.0f;
+    m[3][2] = -1.0f;
+    m[3][3] = 0.0f;
 }
 
 void C_MTXOrtho(Mtx44 m, f32 t, f32 b, f32 l, f32 r, f32 n, f32 f) {
     f32 tmp;
 
-    tmp = *(f32*)&lbl_803E7630 / (r - l);
-    m[0][0] = *(f32*)&lbl_803E7634 * tmp;
-    m[0][1] = *(f32*)&lbl_803E7638;
-    m[0][2] = *(f32*)&lbl_803E7638;
+    tmp = 1.0f / (r - l);
+    m[0][0] = 2.0f * tmp;
+    m[0][1] = 0.0f;
+    m[0][2] = 0.0f;
     m[0][3] = tmp * -(r + l);
 
-    tmp = *(f32*)&lbl_803E7630 / (t - b);
-    m[1][0] = *(f32*)&lbl_803E7638;
-    m[1][1] = *(f32*)&lbl_803E7634 * tmp;
-    m[1][2] = *(f32*)&lbl_803E7638;
+    tmp = 1.0f / (t - b);
+    m[1][0] = 0.0f;
+    m[1][1] = 2.0f * tmp;
+    m[1][2] = 0.0f;
     m[1][3] = tmp * -(t + b);
 
-    tmp = *(f32*)&lbl_803E7630 / (f - n);
-    m[2][0] = *(f32*)&lbl_803E7638;
-    m[2][1] = *(f32*)&lbl_803E7638;
-    m[2][2] = *(f32*)&lbl_803E763C * tmp;
+    tmp = 1.0f / (f - n);
+    m[2][0] = 0.0f;
+    m[2][1] = 0.0f;
+    m[2][2] = -1.0f * tmp;
     m[2][3] = (-f) * tmp;
-    m[3][0] = *(f32*)&lbl_803E7638;
-    m[3][1] = *(f32*)&lbl_803E7638;
-    m[3][2] = *(f32*)&lbl_803E7638;
-    m[3][3] = *(f32*)&lbl_803E7630;
+    m[3][0] = 0.0f;
+    m[3][1] = 0.0f;
+    m[3][2] = 0.0f;
+    m[3][3] = 1.0f;
 }
