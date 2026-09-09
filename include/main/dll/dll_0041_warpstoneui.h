cd1840c392b3c05732ec558556964dbcf767a19a
Zachary Canann <zacharycanann@gmail.com>
2026-09-09 16:49:41 -0700

    Recover shared warpstone menu records and regional placement

--- include/main/dll/dll_0041_warpstoneui.h
+++ include/main/dll/dll_0041_warpstoneui.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_DLL_DLL_0041_WARPSTONEUI_H_
 #define MAIN_DLL_DLL_0041_WARPSTONEUI_H_
 
-#include "types.h"
+#include "global.h"
 
 typedef struct
 {
@@ -10,17 +10,11 @@ typedef struct
     u8 b3; /* unused/padding */
 } WarpstoneEntry;
 
-typedef struct
-{
-    u16 textId;
-    u16 boxId;
-    s16 x;
-    s16 y;
-    u8 pad08[0x12];
-    s8 previousItem;
-    s8 nextItem;
-    u8 pad1C[0x20];
-} WarpstoneMenuItem;
+STATIC_ASSERT(sizeof(WarpstoneEntry) == 4);
+STATIC_ASSERT(offsetof(WarpstoneEntry, bit) == 0);
+STATIC_ASSERT(offsetof(WarpstoneEntry, mapAct) == 2);
+
+
 
 void WarpstoneUI_setState(int val);
 void WarpstoneUI_showUI(int arg);
