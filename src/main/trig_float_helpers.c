8903b203b2740fb4101a9a419b50aa7cdae18e17
Zachary Canann <zacharycanann@gmail.com>
2026-09-08 21:33:04 -0700

    Format angle-vector polynomial accumulators

--- src/main/trig_float_helpers.c
+++ src/main/trig_float_helpers.c
@@ -39,8 +39,7 @@ void angleToVec2(int angle, float* sinOut, float* cosOut) {
     float sine = fastCastS16ToFloat(&scaledAngleBits);
     float cosine = sine * sine;
     sine = sine * (cosine * (6.424445e-26f * cosine + -2.294029e-15f) + 0.00002396833f);
-    cosine =
-        cosine * (cosine * (-2.575884e-31f * cosine + 1.3747608e-20f) + -2.8724248e-10f) + 1.0f;
+    cosine = cosine * (cosine * (-2.575884e-31f * cosine + 1.3747608e-20f) + -2.8724248e-10f) + 1.0f;
 
     STORE_SINCOS(angle, sine, cosine, sinOut, cosOut);
 }
@@ -49,13 +48,9 @@ void angleToVec2Precise(int angle, float* sinOut, float* cosOut) {
     s16 scaledAngleBits = (u16)angle << 1 << 1;
     float sine = fastCastS16ToFloat(&scaledAngleBits);
     float cosine = sine * sine;
-    sine =
-        sine * (cosine * (cosine * (-8.8444e-37f * cosine + 6.590636e-26f) + -2.2949214e-15f) +
-                       0.000023968449f);
+    sine = sine * (cosine * (cosine * (-8.8444e-37f * cosine + 6.590636e-26f) + -2.2949214e-15f) + 0.000023968449f);
     cosine =
-        cosine * (cosine * (cosine * (2.655e-42f * cosine + -2.632911e-31f) + 1.3751435e-20f) +
-                        -2.8724328e-10f) +
-        1.0f;
+        cosine * (cosine * (cosine * (2.655e-42f * cosine + -2.632911e-31f) + 1.3751435e-20f) + -2.8724328e-10f) + 1.0f;
 
     STORE_SINCOS(angle, sine, cosine, sinOut, cosOut);
 }
