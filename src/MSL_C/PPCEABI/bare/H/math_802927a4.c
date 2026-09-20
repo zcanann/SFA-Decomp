8b726e8b3f30097fbd7f8236fc070c616d4ab1c2
Jack Price-Burns <jackpriceburns@outlook.com>
2026-09-20 11:08:53 +0100

    Format matched power approximation expressions

--- src/MSL_C/PPCEABI/bare/H/math_802927a4.c
+++ src/MSL_C/PPCEABI/bare/H/math_802927a4.c
@@ -72,26 +72,29 @@ float powfCoreHighPrecision(float base, float power) {
         resultExponentAsDouble = (double)resultExponent;
         fractionalExponent = logValue - resultExponentAsDouble;
 
-        result.value = fractionalExponent ? (float)(fractionalExponent *
-                            (fractionalExponent *
-                                 (fractionalExponent *
-                                      (fractionalExponent *
-                                           (fractionalExponent *
-                                                (fractionalExponent *
-                                                     (fractionalExponent *
-                                                          (fractionalExponent *
-                                                               (fractionalExponent *
-                                                                    (9.926346441109975e-09 * fractionalExponent +
-                                                                     9.472326685984924e-08) +
-                                                                1.3310673239175234e-06) +
-                                                           1.5244851723158107e-05) +
-                                                      0.00015403947598618592) +
-                                                 0.0013333543997684197) +
-                                            0.00961812940579326) +
-                                       0.055504108628658844) +
-                                  0.24022650696122427) +
-                             0.693147180559909) +
-                        0.9999999999999999) : 1.0f;
+        result.value =
+            fractionalExponent
+                ? (float)(fractionalExponent *
+                              (fractionalExponent *
+                                   (fractionalExponent *
+                                        (fractionalExponent *
+                                             (fractionalExponent *
+                                                  (fractionalExponent *
+                                                       (fractionalExponent *
+                                                            (fractionalExponent *
+                                                                 (fractionalExponent *
+                                                                      (9.926346441109975e-09 * fractionalExponent +
+                                                                       9.472326685984924e-08) +
+                                                                  1.3310673239175234e-06) +
+                                                             1.5244851723158107e-05) +
+                                                        0.00015403947598618592) +
+                                                   0.0013333543997684197) +
+                                              0.00961812940579326) +
+                                         0.055504108628658844) +
+                                    0.24022650696122427) +
+                               0.693147180559909) +
+                          0.9999999999999999)
+                : 1.0f;
 
         if ((int)(baseBits & 0x80000000)) {
             integerPower = power;
@@ -137,7 +140,8 @@ float powfCoreFast(float base, register float power) {
         fastCastFloatToS16(logValue.value, &resultExponent);
         resultExponentAsFloat = fastCastS16ToFloat(&resultExponent);
         logValue.value = logValue.value - resultExponentAsFloat;
-        result.value = logValue.value ? (logValue.value * (0.3431449f * logValue.value + 0.6519048f) + 1.0023681f) : 1.0f;
+        result.value =
+            logValue.value ? (logValue.value * (0.3431449f * logValue.value + 0.6519048f) + 1.0023681f) : 1.0f;
         if ((int)(baseBits & 0x80000000)) {
             integerPower = power;
             if (integerPower & 1) {
